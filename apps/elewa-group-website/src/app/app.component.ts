fa0734f7d384af4458be1536c2be044af23f27c8
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-01-25 15:10:19 +0300

    Initial commit

---
+++ apps/elewa-group-website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'elewa-group-website';
+}
