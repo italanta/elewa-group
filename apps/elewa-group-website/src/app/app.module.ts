9b9ceb910162022e052fcf13266d8c377b5a8cae
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-01-25 15:58:47 +0300

    :package:  :wrench:  setup scully and project

--- apps/elewa-group-website/src/app/app.module.ts
+++ apps/elewa-group-website/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ScullyLibModule } from '@scullyio/ng-lib';
+
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ scully.elewa-group-website.config.ts
@@ -0,0 +1,10 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'elewa-group-website',
+  distFolder: './dist/apps/elewa-group-website', // output directory of your Angular build artifacts
+  outDir: './dist/static', // directory for scully build artifacts
+  defaultPostRenderers: [],
+  routes: {},
+};
