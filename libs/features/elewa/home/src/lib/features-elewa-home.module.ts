38b8e14d236ced3789d1cbe38af4be2faffd08bf
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-02-02 16:14:47 +0300

    :100:added my first comp

--- libs/features/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/features/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HomePageComponent],
 })
 export class FeaturesElewaHomeModule {}
