00943c3870446e34c182908cf879530a11381fd4
Keith <keithwesley187@gmail.com>
2023-02-03 20:26:29 +0300

    :sparkles: feat: generated elewa-group-card

--- libs/features/components/cards/src/lib/features-components-cards.module.ts
+++ libs/features/components/cards/src/lib/features-components-cards.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaGroupCardComponent } from './elewa-group-card/elewa-group-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaGroupCardComponent],
 })
 export class FeaturesComponentsCardsModule {}
