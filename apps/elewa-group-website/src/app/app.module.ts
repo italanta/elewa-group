64766c5f4f53974bd8bd05a1ecbdc42094b5a6ad
Job Karani <jobkarani084j@gmail.com>
2023-02-04 12:05:41 +0300

    feat:created card and card input decorators

--- apps/elewa-group-website/src/app/app.module.ts
+++ apps/elewa-group-website/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppComponent } from './app.component';
 
 import { HomePageModule } from '@elewa-group/pages/elewa/home'
+import {CardsModule} from 'libs/features/components/cards/src/lib/cards/cards.module'
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule, 
     ScullyLibModule,
-    HomePageModule
+    HomePageModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
