2fce7a1a70fe1aeb8a8014c77f49c868039839b3
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-04 12:01:58 +0300

    :lipstick: add mission component for (We care section)

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MissionComponent } from './mission/mission.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MissionComponent],
 })
 export class AboutUsModule {}
---
+++ libs/pages/elewa/about-us/src/lib/mission/mission.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MissionComponent } from './mission.component';
+
+describe('MissionComponent', () => {
+  let component: MissionComponent;
+  let fixture: ComponentFixture<MissionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MissionComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MissionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
