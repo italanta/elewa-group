5124d715bb7d5fb04e0865df34b5f4e71eb0231a
Vincent <makokhavomondi@gmail.com>
2023-02-06 21:27:53 +0300

    :construction: feat: change placeholder image color
    Enable us to see what we have done so far

--- libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -8,6 +8,6 @@ import { Component, Input } from '@angular/core';
 export class ElewaHeroComponent {
   @Input() subtitle = 'How to recognize an elewa member';
   @Input() title = 'Dependable, Creative, Supportive, Open Minded and Fun*';
-  @Input() backgroundImg =  'https://via.placeholder.com/1900x1080';
+  @Input() backgroundImg =  'https://via.placeholder.com/150/FF0000/00000FF';
   @Input() footer = "";
 }
