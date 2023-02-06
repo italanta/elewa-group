dc2cb057ee7c3ccd21be4c2f5b4d3c5a1ac78bc9
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-06 18:43:07 +0300

    ✨feat: added Inputs for the button component

--- libs/features/components/buttons/src/lib/buttons/elewa-group-button-with-animation/elewa-group-button-with-animation.component.ts
+++ libs/features/components/buttons/src/lib/buttons/elewa-group-button-with-animation/elewa-group-button-with-animation.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-button-with-animation',
   templateUrl: './elewa-group-button-with-animation.component.html',
   styleUrls: ['./elewa-group-button-with-animation.component.css']
 })
-export class ElewaGroupButtonWithAnimationComponent {
 
+export class ElewaGroupButtonWithAnimationComponent {
+  @Input() mode="light";
+  @Input() message="Button Text";
+  @Input() action=".";
 
 }
