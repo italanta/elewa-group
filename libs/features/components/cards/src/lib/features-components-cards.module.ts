4795466d4a8fb37027313133c2e093461df7498b
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-06 15:12:53 +0300

    #7:fix:rename the export module from FeaturesComponentsCardsModule to CardsModule

--- libs/features/components/cards/src/lib/features-components-cards.module.ts
+++ libs/features/components/cards/src/lib/features-components-cards.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaGroupCardComponent } from './pages/elewa-group-card/elewa-group-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaGroupCardComponent],
+  exports: [ElewaGroupCardComponent],
 })
-export class FeaturesComponentsCardsModule {}
+export class CardsModule {}
