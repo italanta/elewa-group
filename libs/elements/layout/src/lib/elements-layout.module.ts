73b3cf73ff3b492da29ccd6d280acb962e1941ae
Mildred <mildredkoskei01@gmail.com>
2023-02-06 17:28:30 +0300

    :sparkles: created elewa-hero component in the lib/element/layout

---
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaHeroComponent } from './components/elewa-hero/elewa-hero.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaHeroComponent],
+})
+export class ElementsLayoutModule {}
