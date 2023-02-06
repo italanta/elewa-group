2e0c454482f18e1d7225a55fe6dcd7d365ba1ed2
Machoka Joash Omao <aaronjoash44@gmail.com>
2023-02-06 21:39:17 +0300

    created home section component

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomeSectionComponent } from './pages/home-section/home-section.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomePageComponent],
-  exports: [ HomePageComponent]
+  declarations: [HomePageComponent, HomeSectionComponent],
+  exports: [HomePageComponent, HomeSectionComponent],
 })
 export class HomePageModule {}
---
+++ libs/pages/elewa/home/src/lib/pages/home-section/home-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-section',
+  templateUrl: './home-section.component.html',
+  styleUrls: ['./home-section.component.scss'],
+})
+export class HomeSectionComponent {}
