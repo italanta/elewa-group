a4bd3b858d60b783d906f67ee6288455420ae91f
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-06 11:32:41 +0300

    ✨ generate the elewa button component

---
+++ libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-button',
+  templateUrl: './elewa-group-button.component.html',
+  styleUrls: ['./elewa-group-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ElewaGroupButtonComponent {}
