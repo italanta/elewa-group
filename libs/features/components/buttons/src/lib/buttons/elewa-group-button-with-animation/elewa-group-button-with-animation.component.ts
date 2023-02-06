466a360817d6d6b676792d7ab7d0842e121e3c79
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-06 18:51:40 +0300

    ✨ feat: added navigation function

--- libs/features/components/buttons/src/lib/buttons/elewa-group-button-with-animation/elewa-group-button-with-animation.component.ts
+++ libs/features/components/buttons/src/lib/buttons/elewa-group-button-with-animation/elewa-group-button-with-animation.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'elewa-group-button-with-animation',
   templateUrl: './elewa-group-button-with-animation.component.html',
@@ -7,8 +9,15 @@ import { Component, Input } from '@angular/core';
 })
 
 export class ElewaGroupButtonWithAnimationComponent {
+  constructor(
+    private location: Location
+  ) {}
+
   @Input() mode="light";
   @Input() message="Button Text";
   @Input() action=".";
 
+  nav(): void{
+    this.location.go(this.action);
+  }
 }
