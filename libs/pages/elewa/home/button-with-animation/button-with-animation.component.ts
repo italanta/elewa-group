c70f05012542523af260946e5389faf2e8d3dd6c
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-06 13:33:41 +0300

    📁file: generated the button-with-animation component

---
+++ libs/pages/elewa/home/button-with-animation/button-with-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-animation',
+  templateUrl: './button-with-animation.component.html',
+  styleUrls: ['./button-with-animation.component.css']
+})
+export class ButtonWithAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
