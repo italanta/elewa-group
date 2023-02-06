903f74c9670cf25e8b4e1d35db08db9722b313ce
Rokyt69 <royokite@gmail.com>
2023-02-06 21:39:20 +0300

    :sparkles: feat: add reusable vertical list files
    Add ui-list library.
    Add elewa-group-vertical-list-one component to the library.

---
+++ libs/features/components/ui-lists/src/lib/elewa-group-vertical-list-one/elewa-group-vertical-list-one.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-vertical-list-one',
+  templateUrl: './elewa-group-vertical-list-one.component.html',
+  styleUrls: ['./elewa-group-vertical-list-one.component.scss'],
+})
+export class ElewaGroupVerticalListOneComponent {}
