af9688cbf8a957910138b29de0bbd63512feb15d
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-06 11:42:09 +0300

    #7:art:created the elewa-group-card component

---
+++ libs/features/components/cards/src/lib/pages/elewa-group-card/elewa-group-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-card',
+  templateUrl: './elewa-group-card.component.html',
+  styleUrls: ['./elewa-group-card.component.scss'],
+})
+export class ElewaGroupCardComponent {}
