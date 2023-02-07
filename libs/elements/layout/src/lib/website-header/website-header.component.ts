da05dcda44b2b6f66ac25e6b3e6443bf00e2c835
Martin Mithi <104300169+lilmithi@users.noreply.github.com>
2023-02-07 20:06:55 +0300

    Update website-header.component.ts

--- libs/elements/layout/src/lib/website-header/website-header.component.ts
+++ libs/elements/layout/src/lib/website-header/website-header.component.ts
@@ -7,11 +7,6 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WebsiteHeaderComponent implements OnInit {
   isNavLinks!: boolean;
-  @Input() subtitle = 'How to recognize an elewa member';
-  @Input() mainTitle = 'Dependable, Creative, Supportive, Open Minded and Fun*';
-  @Input() footer = '*Serious when it matters';
-  @Input() imageSource =
-    'https://images.unsplash.com/photo-1674592309639-39067f6a8111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60';
   ngOnInit(): void {
     this.isNavLinks = false;
   }
