592bbc3152bd9b5350d339a98f9f01fcbb085f84
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-07 13:20:23 +0300

    💄 added styling to button: hover effect in place

--- libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
+++ libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
@@ -7,17 +7,19 @@ import { ChangeDetectionStrategy, Component, OnInit, Input, Output, EventEmitter
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ElewaGroupButtonComponent {
-  public buttonText = 'Learn More';
+  public buttonText = 'Get this offer now';
 
   @Input()
 	set text(name: string) {
 		this.buttonText = name;
 	}
+
+	//@Input() set theme (mode: 'light' | 'dark') 
 	get name(): string {
 		return this.buttonText;
 	}
 
-	@Input() color = '0068B4';
+	@Input() color = 'black';
 	@Input() type = 'hover';
   @Output() btnClick = new EventEmitter();
 
