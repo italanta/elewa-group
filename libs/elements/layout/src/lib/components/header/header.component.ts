77cf7a85fa93ca2ca0b96b5cc8d1e1807ccdd44e
Job Karani <jobkarani084j@gmail.com>
2023-02-08 01:38:27 +0300

    feat: creates and styles nav header

--- libs/elements/layout/src/lib/components/header/header.component.ts
+++ libs/elements/layout/src/lib/components/header/header.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit{
+
+    ngOnInit() {
+
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menus");
+      const navLink = document.querySelectorAll(".nav-links");
+    
+      const mobileMenu = () => {
+        hamburger?.classList.toggle("active")
+        navMenu?.classList.toggle("active")
+      }
+    
+      const closeMenu = () => {
+        hamburger?.classList.remove("active")
+        navMenu?.classList.remove("active")
+      }
+    
+      hamburger?.addEventListener("click" , mobileMenu)
+      navLink.forEach((l) => l.addEventListener("click",closeMenu))
+    }
+}
