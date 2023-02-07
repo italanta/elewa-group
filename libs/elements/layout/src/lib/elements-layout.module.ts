54881d8692055e370a56d47681322a0a183c3e3b
martin mithi <gathitumithi9000@gmail.com>
2023-02-07 16:29:28 +0300

    Initial commit

---
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebsiteHeaderComponent } from './website-header/website-header.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [WebsiteHeaderComponent],
+})
+export class ElementsLayoutModule {}
---
+++ libs/elements/layout/src/lib/website-header/website-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-website-header',
+  templateUrl: './website-header.component.html',
+  styleUrls: ['./website-header.component.scss'],
+})
+export class WebsiteHeaderComponent {}
