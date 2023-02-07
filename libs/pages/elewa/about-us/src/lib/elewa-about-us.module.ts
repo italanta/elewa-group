5ab48af2a6ad3b30488315e9d3dc9cf8a468eb34
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-07 08:55:23 +0300

    Revert ":white_check_mark: add we-care component"
    This reverts commit 5a813245fc4eb6cefcb2c7814591f6e97f3b3ba2.

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MissionComponent } from './mission/mission.component';
-import { WeCareComponent } from './we-care/we-care.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MissionComponent, WeCareComponent],
+  declarations: [MissionComponent],
 })
 export class AboutUsModule {}
--- libs/pages/elewa/about-us/src/lib/we-care/we-care.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-we-care',
-  templateUrl: './we-care.component.html',
-  styleUrls: ['./we-care.component.scss'],
-})
-export class WeCareComponent {}
