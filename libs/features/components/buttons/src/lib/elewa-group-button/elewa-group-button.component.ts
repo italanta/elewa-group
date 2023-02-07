ad096cc54494fd4ca6c4119e0494d061742904cb
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-07 23:47:56 +0300

    🎨 added dynamic inputs

--- libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
+++ libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
@@ -1,29 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit, Input, Output, EventEmitter } from '@angular/core'; 
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'elewa-group-elewa-group-button',
+  selector: 'elewa-group-button',
   templateUrl: './elewa-group-button.component.html',
   styleUrls: ['./elewa-group-button.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ElewaGroupButtonComponent {
-  public buttonText = 'Get this offer now';
-
-  @Input()
-	set text(name: string) {
-		this.buttonText = name;
-	}
-
-	//@Input() set theme (mode: 'light' | 'dark') 
-	get name(): string {
-		return this.buttonText;
-	}
-
-	@Input() color = 'black';
-	@Input() type = 'hover';
-  @Output() btnClick = new EventEmitter();
-
-  onClick() {
-		this.btnClick.emit();
-	}
+  @Input() mode = "dark";
+  @Input() message = "hello world";
+  @Input() action = "google.com";
 }
