f37d24d6f03130a36c6424d630e8dd99f526160c
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-07 11:05:23 +0300

    ✨ Added typescript file for jobs-section

---
+++ libs/pages/elewa/home/jobs-section/src/lib/jobs-section/jobs-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'elewa-group-jobs-section',
+    templateUrl: './jobs-section.component.html',
+    styleUrls: ['./jobs-section.component.scss'],
+})
+export class JobsSectionComponent { }
+
