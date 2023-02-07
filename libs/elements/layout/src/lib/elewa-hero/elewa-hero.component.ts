9a965adcda7fbd3b23d020d71a35d7cb96258774
martin mithi <gathitumithi9000@gmail.com>
2023-02-07 15:05:27 +0300

    feat: added input decorator for the lander image

--- libs/elements/layout/src/lib/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/elewa-hero/elewa-hero.component.ts
@@ -10,7 +10,9 @@ export class ElewaHeroComponent implements OnInit {
   @Input() subtitle = 'How to recognize an elewa member';
   @Input() mainTitle = 'Dependable, Creative, Supportive, Open Minded and Fun*';
   @Input() footer = '*Serious when it matters';
-  ngOnInit(): void { 
+  @Input() imageSource =
+    'https://images.unsplash.com/photo-1674592309639-39067f6a8111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60';
+  ngOnInit(): void {
     this.isNavLinks = false;
   }
   setVisibility() {
