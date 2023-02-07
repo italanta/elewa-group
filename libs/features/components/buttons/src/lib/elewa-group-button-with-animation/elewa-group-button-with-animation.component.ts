b0ed5e712c98c48043d659a9a298369d0ab1431b
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-07 11:04:22 +0300

    📁file: created button-with-animation component

---
+++ libs/features/components/buttons/src/lib/elewa-group-button-with-animation/elewa-group-button-with-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elewa-group-button-with-animation',
+  templateUrl: './elewa-group-button-with-animation.component.html',
+  styleUrls: ['./elewa-group-button-with-animation.component.css']
+})
+export class ElewaGroupButtonWithAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
