af0b8052174adb5458cfa312a3a1e6e7cf80e045
Job Karani <jobkarani084j@gmail.com>
2023-02-07 10:13:41 +0300

    fix: removes home-hero-section component from the pages folder

--- libs/pages/elewa/home/src/lib/pages/home-hero-section/home-hero-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-home-hero-section',
-  templateUrl: './home-hero-section.component.html',
-  styleUrls: ['./home-hero-section.component.scss'],
-})
-export class HomeHeroSectionComponent {}
