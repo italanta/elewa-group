5a813245fc4eb6cefcb2c7814591f6e97f3b3ba2
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-07 08:47:58 +0300

    :white_check_mark: add we-care component

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MissionComponent } from './mission/mission.component';
+import { WeCareComponent } from './we-care/we-care.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MissionComponent],
+  declarations: [MissionComponent, WeCareComponent],
 })
 export class AboutUsModule {}
