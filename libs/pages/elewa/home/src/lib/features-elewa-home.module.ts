d946f35afa91d53cb51239f6d9cec7d2fba2f098
awadh <awadh.hassan@student.moringaschool.com>
2023-02-08 00:27:42 +0300

    done with routing and addition of about component

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './pages/home-page/header/header.component';
+import { AboutComponent } from './pages/home-page/About-culture/about.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomePageComponent,HeaderComponent],
-  exports: [ HomePageComponent ,HeaderComponent]
+  declarations: [HomePageComponent,HeaderComponent,AboutComponent],
+  exports: [ HomePageComponent ,HeaderComponent,AboutComponent]
 })
 export class HomePageModule {}
