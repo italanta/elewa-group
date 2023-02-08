dcf26db8b1d424e5311bb1560b30d04cd65db643
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-02-08 16:58:26 +0300

    ✨feat:Imported the cards module in the home package

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ActivitySectionComponent } from './activity-section/activity-section.component';
+import { CardsModule } from '@elewa-group/features/components/cards';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomePageComponent],
-  exports: [ HomePageComponent]
+  imports: [CommonModule, CardsModule],
+  declarations: [
+    HomePageComponent,
+    ActivitySectionComponent,
+  ],
+  exports: [HomePageComponent],
 })
 export class HomePageModule {}
