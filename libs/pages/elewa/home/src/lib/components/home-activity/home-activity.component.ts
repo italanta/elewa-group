50a35b8f9ab08789c04092d2ff568b4c09ea38b2
jeffoty <odhiambo47@gmail.com>
2023-02-08 16:42:12 +0300

    ✨feat: show an overview of company branches

--- libs/pages/elewa/home/src/lib/components/home-activity/home-activity.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-activity/home-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-home-activity',
@@ -6,5 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home-activity.component.scss'],
 })
 export class HomeActivityComponent {
-  
+
 }
