037252f2c83f8f51be29889b93cf9b1dda921d79
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-08 10:55:54 +0300

    ➕adds app-routing module import

--- apps/elewa-group-website/src/app/app.module.ts
+++ apps/elewa-group-website/src/app/app.module.ts
@@ -9,13 +9,16 @@ import { HomePageModule } from '@elewa-group/pages/elewa/home';
 
 import { CardsModule } from '@elewa-group/features/components/cards';
 
+import { AppRoutingModule } from './app.routing';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule, 
     ScullyLibModule,
     HomePageModule,
-    CardsModule
+    CardsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
