533ac0a68583802f0991b66257287fcbcecf1af5
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-02-08 14:27:59 +0300

    #31 updated the home-component to display the image and text banners component

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-
+import { ElewaGroupImageAndTextBannerComponent } from 'libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [HomePageComponent, HomeHeroSectionComponent,ElewaGroupImageAndTextBannerComponent],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
 export class HomePageModule {}
