156b84ec5483ebb6441788ccd66c64bcd48682ce
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-08 11:44:18 +0300

    🐛 update the imports path

--- libs/features/components/buttons/src/lib/features-components-buttons.module.ts
+++ libs/features/components/buttons/src/lib/features-components-buttons.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ElewaGroupButtonComponent } from './elewa-group-button.component';
+import { ElewaGroupButtonComponent } from './elewa-group-button/elewa-group-button.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ElewaGroupButtonComponent],
   exports: [ElewaGroupButtonComponent ]
 })
-
 export class ButtonsModule {}
