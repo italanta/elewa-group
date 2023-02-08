b02495e321c25a2ff94747b17c8e56a2df7f0990
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-08 08:22:52 +0300

    💄 🎨 updated rendering actions and added styles

--- libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
+++ libs/features/components/buttons/src/lib/elewa-group-button/elewa-group-button.component.ts
@@ -8,5 +8,6 @@ import { Component, Input } from '@angular/core';
 export class ElewaGroupButtonComponent {
   @Input() mode = "dark";
   @Input() message = "hello world";
-  @Input() action = "google.com";
+  @Input() action: string | undefined;
+
 }
