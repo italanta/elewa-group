34e5d7dfc92f4455fdff2bc242f5385c6f3440f4
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-09 11:09:21 +0300

    #31:New feature: Created a banners library and elewa-group-image-and-text-banner

---
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaGroupImageAndTextBannerComponent],
+})
+export class FeaturesComponentsBannersModule {}
