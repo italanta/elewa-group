aabca90244b6808efb2cd54ddd6d3233656f9406
Ian Mike <iammikeian@gmail.com>
2023-02-09 12:43:53 +0300

    🎨 chore: render home-page activity section

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { HomePageActivityComponent } from './home-page-activity/home-page-activity.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    HomePageActivityComponent,
+  ],
+  exports: [HomePageComponent, HomeHeroSectionComponent, HomePageActivityComponent],
 })
 export class HomePageModule {}
