72e3406a5024fd219959f3299e5403b8bcc5f67c
Snowflake <onyangogeorge954@gmail.com>
2023-02-09 20:16:40 +0300

    complete the team member view

--- libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
+++ libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
@@ -10,4 +10,5 @@ export class TeamMemberComponent {
   @Input () title = 'Founder,Managing Partner'
   @Input () imageSrc = 'https://i.postimg.cc/6pWDZNNp/jente.jpg'
   @Input () descrition = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quia quae aspernatur similique, adipisci, debitis hic commodi impedit natus exercitationem tempore error, ratione dolore aliquam quis. Officia officiis veniam commodi.'
+  @Input () linkedinUrl = ''
 }
