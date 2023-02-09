81b481ed4b8095b61a53f519960aa0f1cb3a60b7
Samuel Kabati <sam.kabati@gmail.com>
2023-02-09 11:58:27 +0300

    ✨ feat: created the activity-section component

---
+++ libs/pages/elewa/home/src/lib/components/activity-section/activity-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-activity-section',
+  templateUrl: './activity-section.component.html',
+  styleUrls: ['./activity-section.component.scss'],
+})
+export class ActivitySectionComponent {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { ActivitySectionComponent } from './components/activity-section/activity-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    ActivitySectionComponent,
+  ],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
 export class HomePageModule {}
