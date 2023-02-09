46182d3b924f7d3805a7078c39cf08eac4f6d5cf
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-02-09 19:39:49 +0300

    Created horizontal lists orgs component

--- libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
+++ libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaGroupVerticalListOneComponent } from './elewa-group-vertical-list-one/elewa-group-vertical-list-one.component';
+import { ElewaGroupHorizontalListOrgsComponent } from './elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaGroupVerticalListOneComponent],
-  exports: [ElewaGroupVerticalListOneComponent]
+  declarations: [
+    ElewaGroupVerticalListOneComponent,
+    ElewaGroupHorizontalListOrgsComponent,
+  ],
+  exports: [
+    ElewaGroupVerticalListOneComponent,
+    ElewaGroupHorizontalListOrgsComponent
+  ],
 })
 export class UiListsModule {}
