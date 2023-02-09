0ffd2f7312dc71e173224c36786a484a0fa560b1
Newton Ombese <newtonombese1@gmail.com>
2023-02-09 15:31:30 +0300

    :sparkles:feat: Added the homepage lander component and exported it

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -4,14 +4,16 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { HomepageFooterComponent } from './components/homepage-footer/homepage-footer.component';
+import { HomePageLanderComponent } from './components/home-page-lander/home-page-lander.component'
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
   declarations: [
     HomePageComponent,
+    HomePageLanderComponent,
     HomeHeroSectionComponent,
     HomepageFooterComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent, HomepageFooterComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent, HomepageFooterComponent, HomePageLanderComponent],
 })
 export class HomePageModule {}
