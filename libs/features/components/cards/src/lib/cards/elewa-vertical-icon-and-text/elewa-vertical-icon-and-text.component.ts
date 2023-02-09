33ad4c6c1853d8caa6bf775ba64a46cf712f12fc
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-09 17:20:44 +0300

    :building_construction:add properties array

--- libs/features/components/cards/src/lib/cards/elewa-vertical-icon-and-text/elewa-vertical-icon-and-text.component.ts
+++ libs/features/components/cards/src/lib/cards/elewa-vertical-icon-and-text/elewa-vertical-icon-and-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-vertical-icon-and-text',
@@ -6,7 +6,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./elewa-vertical-icon-and-text.component.scss'],
 })
 export class ElewaVerticalIconAndTextComponent {
-  @Input() icon = "fa fa-user-group";
-  @Input() title = "Cooperative";
-  @Input() description = "Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here";
+  properties=[
+    {
+      icon:"fa fa-user-group",
+      title:"Holistic solutions",
+      description:"We go beyond a simple patch-up but develop lasting solutions through holistic design"
+    },
+    {
+      icon:"fa-solid fa-repeat",
+      title:"Impact",
+      description:"Impact as a direct or distinct result. All our respective organizations have underlying theories of change."
+    },
+    {
+      icon:"fa-solid fa-globe",
+      title:"Cooperative",
+      description:"Sharing is caring. We share what we learn. All through our internal projects are open source."
+    },
+  ]
 }
