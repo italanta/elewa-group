b6e6dae46383e61c65145526e39846816506c5cd
Newton Ombese <newtonombese1@gmail.com>
2023-02-09 15:56:45 +0300

    :recycle:refactor: Fxing the tsconfig file

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
-import { HomepageFooterComponent } from './components/homepage-footer/homepage-footer.component';
 import { HomePageLanderComponent } from './components/home-page-lander/home-page-lander.component'
 
 @NgModule({
@@ -12,8 +11,7 @@ import { HomePageLanderComponent } from './components/home-page-lander/home-page
     HomePageComponent,
     HomePageLanderComponent,
     HomeHeroSectionComponent,
-    HomepageFooterComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent, HomepageFooterComponent, HomePageLanderComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,  HomePageLanderComponent],
 })
 export class HomePageModule {}
