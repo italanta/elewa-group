f78bd6709b631a705c6ae36aa4d7855ad932aea9
Snowflake <onyangogeorge954@gmail.com>
2023-02-09 12:59:34 +0300

    create team member component

---
+++ libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-team-member',
+  templateUrl: './team-member.component.html',
+  styleUrls: ['./team-member.component.scss'],
+})
+export class TeamMemberComponent {}
--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamMemberComponent } from './components/team-member/team-member.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TeamMemberComponent],
 })
 export class FeaturesComponentsBannersModule {}
