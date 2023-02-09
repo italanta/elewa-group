b12cfe4e4e651bd519ac45f55178d1fb1ee2dbf4
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2023-02-09 08:39:09 -0500

    Revert "💡 feat:adding homepage footer component import"
    This reverts commit 6b1b4f48735d69bbaf2422311e11e2f497b3cddd.

--- libs/pages/elewa/home/src/lib/components/homepage-footer/homepage-footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-homepage-footer',
-  templateUrl: './homepage-footer.component.html',
-  styleUrls: ['./homepage-footer.component.scss'],
-})
-export class HomepageFooterComponent {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
-import { HomepageFooterComponent } from './components/homepage-footer/homepage-footer.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [
-    HomePageComponent,
-    HomeHeroSectionComponent,
-    HomepageFooterComponent,
-  ],
-  exports: [HomePageComponent, HomeHeroSectionComponent, HomepageFooterComponent],
+  declarations: [HomePageComponent, HomeHeroSectionComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent],
 })
 export class HomePageModule {}
