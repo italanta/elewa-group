52ba0014b8413671f75558486fd086b5f6edc71e
Kangonga <guchu413@gmail.com>
2023-02-09 13:48:16 +0300

    feat: Added Home page jobs section

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    HomeJobsSectionComponent,
+  ],
+  exports: [HomePageComponent, HomeHeroSectionComponent, HomeJobsSectionComponent],
 })
 export class HomePageModule {}
