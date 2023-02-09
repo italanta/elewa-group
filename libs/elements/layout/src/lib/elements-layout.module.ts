339d3aff674089cc8e84c3e929a4252ddf26e1ef
martin mithi <gathitumithi9000@gmail.com>
2023-02-09 11:07:08 +0300

    Elements module update

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { ElewaGroupImageAndTextBannerComponent } from './components/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  declarations: [HeaderComponent, ElewaGroupImageAndTextBannerComponent],
+  exports: [HeaderComponent],
 })
 export class LayoutModule {}
