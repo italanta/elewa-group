23f6a64b91ad210f7682156a75be426e9a2ce840
Daniel Macharia <danielhuios@gmail.com>
2023-02-09 15:20:10 +0300

    feat: creates component folder

---
+++ libs/pages/elewa/about-us/src/lib/components/team-members-carousel/team-members-carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-team-members-carousel',
+  templateUrl: './team-members-carousel.component.html',
+  styleUrls: ['./team-members-carousel.component.scss'],
+})
+export class TeamMembersCarouselComponent {}
