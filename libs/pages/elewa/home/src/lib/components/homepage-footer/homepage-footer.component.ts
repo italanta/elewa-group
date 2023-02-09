36cda6a184e2b9f178de8568d142210bab00bf9c
Newton Ombese <newtonombese1@gmail.com>
2023-02-09 11:36:22 +0300

    :art:feat: Working on the home page footer styling

---
+++ libs/pages/elewa/home/src/lib/components/homepage-footer/homepage-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-homepage-footer',
+  templateUrl: './homepage-footer.component.html',
+  styleUrls: ['./homepage-footer.component.scss'],
+})
+export class HomepageFooterComponent {}
