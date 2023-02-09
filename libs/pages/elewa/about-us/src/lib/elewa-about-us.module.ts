62c6689328467a34d08caa0985ec44f7e3159963
martin mithi <gathitumithi9000@gmail.com>
2023-02-09 16:36:16 +0300

    Module.ts update

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamMembersComponent } from './team-members/team-members.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
+  declarations: [TeamMembersComponent],
+  exports: [TeamMembersComponent],
 })
 export class AboutUsModule {}
