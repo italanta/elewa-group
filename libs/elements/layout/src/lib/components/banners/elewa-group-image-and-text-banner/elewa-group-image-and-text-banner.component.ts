cece42bbae509c96178efc03643703476e13fcd0
martin mithi <gathitumithi9000@gmail.com>
2023-02-09 11:04:39 +0300

    feat: component initial update

--- libs/elements/layout/src/lib/components/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
+++ libs/elements/layout/src/lib/components/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-group-image-and-text-banner',
   templateUrl: './elewa-group-image-and-text-banner.component.html',
   styleUrls: ['./elewa-group-image-and-text-banner.component.scss'],
 })
-export class ElewaGroupImageAndTextBannerComponent {}
+export class ElewaGroupImageAndTextBannerComponent {
+  @Input() imgUrl!: string;
+  @Input() text!: string;
+  @Input() title!: string;
+  @Input() placement!: string;
+  @Input() background!: string;
+}
