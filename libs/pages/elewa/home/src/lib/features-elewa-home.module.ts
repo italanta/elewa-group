7623088d2697558ca5cbb3448e3862065054731c
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-02-09 10:44:23 +0300

    #31 som e updates on styling

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-
-import { ElewaGroupImageAndTextBannerComponent } from 'libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
-@NgModule({
-  imports: [CommonModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent,ElewaGroupImageAndTextBannerComponent],
-
 import { LayoutModule } from '@elewa-group/elements/layout';
+@NgModule({
+  imports: [CommonModule,LayoutModule],
+  declarations: [HomePageComponent, HomeHeroSectionComponent],
 
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
