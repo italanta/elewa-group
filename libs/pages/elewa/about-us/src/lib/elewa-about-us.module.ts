7b19b0db2838d4979cf59fa7c4ca67a140c03b4c
awadh <awadh.hassan@student.moringaschool.com>
2023-02-09 17:53:25 +0300

    addition of card

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { CardsModule } from '@elewa-group/features/components/cards';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,CardsModule],
   declarations: [AboutUsPageComponent],
   exports:[AboutUsPageComponent]
 })
