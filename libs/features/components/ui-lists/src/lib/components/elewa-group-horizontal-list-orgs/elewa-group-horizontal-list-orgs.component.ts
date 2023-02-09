384c1fb9783f26ce4c84ba222ef75a3aaa01f22c
nziokikevin <kevinnzioki10@gmail.com>
2023-02-09 12:02:38 +0300

    :art: :sparkles: feat: generated horizontal list orgs component

---
+++ libs/features/components/ui-lists/src/lib/components/elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-horizontal-list-orgs',
+  templateUrl: './elewa-group-horizontal-list-orgs.component.html',
+  styleUrls: ['./elewa-group-horizontal-list-orgs.component.scss'],
+})
+export class ElewaGroupHorizontalListOrgsComponent {}
