bbe46900e90cf3ceb04bcac91ac20655ed654aa2
Snowflake <onyangogeorge954@gmail.com>
2023-02-09 15:37:58 +0300

    create image and member definition,title and name

--- libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
+++ libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-team-member',
   templateUrl: './team-member.component.html',
   styleUrls: ['./team-member.component.scss'],
 })
-export class TeamMemberComponent {}
+export class TeamMemberComponent {
+  @Input () name = 'Jente Rosseel'
+  @Input () title = 'Founder,Managing Partner'
+  @Input () imageSrc = 'https://i.postimg.cc/6pWDZNNp/jente.jpg'
+  @Input () descrition = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quia quae aspernatur similique, adipisci, debitis hic commodi impedit natus exercitationem tempore error, ratione dolore aliquam quis. Officia officiis veniam commodi.'
+}
