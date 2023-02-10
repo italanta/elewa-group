1240b61e56fcc27fed9c78c01c49b1dc4b096dec
Snowflake <onyangogeorge954@gmail.com>
2023-02-10 10:12:30 +0300

    add dynamic rendering for the next team member on click

--- libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
+++ libs/features/components/banners/src/lib/components/team-member/team-member.component.ts
@@ -6,9 +6,26 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./team-member.component.scss'],
 })
 export class TeamMemberComponent {
-  @Input () name = 'Jente Rosseel'
-  @Input () title = 'Founder,Managing Partner'
-  @Input () imageSrc = 'https://i.postimg.cc/6pWDZNNp/jente.jpg'
-  @Input () descrition = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quia quae aspernatur similique, adipisci, debitis hic commodi impedit natus exercitationem tempore error, ratione dolore aliquam quis. Officia officiis veniam commodi.'
-  @Input () linkedinUrl = ''
+  @Input () members = [{
+    name : 'Jente Rosseel',
+    title : 'Founder,Managing Partner',
+    imageSrc : 'https://i.postimg.cc/6pWDZNNp/jente.jpg',
+    descrition : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quia quae aspernatur similique, adipisci, debitis hic commodi impedit natus exercitationem tempore error, ratione dolore aliquam quis. Officia officiis veniam commodi.',
+    linkedinUrl : ''
+  },
+  {
+    name : 'Kennedy Adhola',
+    title : 'Operations & Partnerships',
+    imageSrc : 'https://i.postimg.cc/6pWDZNNp/jente.jpg',
+    descrition : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quia quae aspernatur similique, adipisci, debitis hic commodi impedit natus exercitationem tempore error, ratione dolore aliquam quis. Officia officiis veniam commodi.',
+    linkedinUrl : ''
+  }
+]
+
+currentMemberIndex = 0;
+
+get whichMember () {
+  return (this.currentMemberIndex + 1)
+}
+
 }
