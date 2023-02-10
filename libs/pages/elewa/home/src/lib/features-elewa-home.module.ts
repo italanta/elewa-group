c5e26dcbeced42a2aab031dc7e0ad855f5051602
calvin <ckodalo@gmail.com>
2023-02-10 20:06:05 +0300

    fix: fiixed PR conflicts

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import {FeaturesComponentsBannersModule} from '@elewa-group/features/components/banners';
+
+
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, FeaturesComponentsBannersModule],
   declarations: [HomePageComponent, HomeHeroSectionComponent],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
