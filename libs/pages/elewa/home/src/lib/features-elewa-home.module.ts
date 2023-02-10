7bdddb8dc75345165f550f487e25b9390fd61646
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-02-10 14:23:41 +0300

    ✨feat:added the neccessary imports for the components.

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+
 import { CardsModule } from '@elewa-group/features/components/cards';
 
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
+import { ActivitySectionComponent } from './components/activity-section/activity-section.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
   imports: [CommonModule, CardsModule],
   declarations: [
     HomePageComponent,
+    HomeHeroSectionComponent,
+    ActivitySectionComponent
   ],
   exports: [HomePageComponent],
 
