93273b741db3f8733ff99328d3e795e99dadcdbe
Rokyt69 <royokite@gmail.com>
2023-02-10 12:12:53 +0300

    :lipstick: style: Add image element and styling to match design
    Add HTML image element
    Add CSS styling to the image
    Use NgOptimizedImage directive to optimize rendering

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { TeamAndPartnersComponent } from './components/team-and-partners/team-and-partners.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, NgOptimizedImage],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
