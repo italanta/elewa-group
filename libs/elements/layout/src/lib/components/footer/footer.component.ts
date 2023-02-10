d4dcef3af105b40ef1639ebd0a6f9ec7313eff53
vionaosiako <viona.osiako@student.moringaschool.com>
2023-02-10 10:50:52 +0300

    ✨feat: Add footer section

---
+++ libs/elements/layout/src/lib/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+
+  // Default:
+  //   <ng-content></ng-content>
+
+  //   Question:
+  //   <ng-content select="[question]"></ng-content>
+})
+export class FooterComponent {
+  
+}
