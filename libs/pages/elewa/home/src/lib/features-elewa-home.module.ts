8869b82c1668c10dc41988e28fccef8772c74322
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-10 11:21:32 +0300

    :sparkles: feat :added home-job-section-component to home folder

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { HomeJobSectionComponent } from './components/home-job-section/home-job-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    HomeJobSectionComponent,
+  ],
+  exports: [HomePageComponent, HomeHeroSectionComponent, HomeJobSectionComponent],
 })
 export class HomePageModule {}
