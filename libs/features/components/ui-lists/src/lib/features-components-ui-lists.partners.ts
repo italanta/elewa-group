104fe1797ab51bc48d4bd47745da3e99c7fdb016
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-02-10 11:55:39 +0300

    ✨feat: Added a file with partners logos

---
+++ libs/features/components/ui-lists/src/lib/features-components-ui-lists.partners.ts
@@ -0,0 +1,35 @@
+export interface Partner {
+    name: string;
+    logoUrl: string;
+}
+    
+export const partners = [
+    {
+        name: 'VVOB Rwanda',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966776/vvob_logo_rlh7p9.png'
+    },
+    {
+        name: 'FarmBetter',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/farmbetter_logo_iuv0za.png'
+    },
+    {
+        name: 'Enabel',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/enabel_logo_rdbvsw.png'
+    },
+    {
+        name: 'Savics',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/savics_logo_w81jn3.png'
+    },
+    {
+        name: 'Scouts',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/scouts_logo_e10wut.png'
+    },
+    {
+        name: 'Sportlights',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/sportlights_logo_pfalfc.png'
+    },
+    {
+        name: 'WWF',
+        logoUrl: 'https://res.cloudinary.com/daumrxfhi/image/upload/v1675966777/wwf_logo_uui3oo.png'
+    }
+]
