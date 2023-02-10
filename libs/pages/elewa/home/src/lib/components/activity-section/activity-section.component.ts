12f84c6160c5575f4873d8b0ece48472d219efa7
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-02-10 11:44:27 +0300

    :construction: WIP

--- libs/pages/elewa/home/src/lib/components/activity-section/activity-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/activity-section/activity-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'elewa-group-activity-section',
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
-import { ActivitySectionComponent } from './activity-section/activity-section.component';
 import { CardsModule } from '@elewa-group/features/components/cards';
+
+
 
 @NgModule({
   imports: [CommonModule, CardsModule],
   declarations: [
     HomePageComponent,
-    ActivitySectionComponent,
   ],
   exports: [HomePageComponent],
 
