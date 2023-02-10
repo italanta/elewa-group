14494ccfb777491ce0e0293a04dca2491c6094c3
jeffoty <odhiambo47@gmail.com>
2023-02-10 10:54:09 +0300

    ✨feat: reusable hero feture added

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { ElewaHeroComponent } from './components/elewa-hero/elewa-hero.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  declarations: [HeaderComponent, ElewaHeroComponent, LayoutPageComponent],
+  exports: [LayoutPageComponent],
 })
 export class LayoutModule {}
