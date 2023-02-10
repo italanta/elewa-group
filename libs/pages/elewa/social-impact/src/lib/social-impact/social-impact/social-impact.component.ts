576dcdc96eabb2f57068168840ffa51e5299b4eb
janekigenyi <janekigenyi@gmail.com>
2023-02-10 14:51:45 +0300

    completed issue

--- libs/pages/elewa/social-impact/src/lib/social-impact/social-impact/social-impact.component.ts
+++ libs/pages/elewa/social-impact/src/lib/social-impact/social-impact/social-impact.component.ts
@@ -6,14 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./social-impact.component.scss'],
 })
 export class SocialImpactComponent {
-  @Input() title = 'Beyond Business';
+  @Input() title:string | undefined
   @Input() paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec nequearcu. Sed venenatis enim at maximus euismod. Ut mi orci, vulputate eu commodo hendrerit nisi. Nam sapien metus, tristique sit amet
   consectetur eget, pretium nec sem. Etiam egestas feugiat lacus, ac feugiat
   sapien faucibus nec. Vivamus arcu sapien, luctus id augue eget, vestibulum
   molesed rutrum pretium facilisis. Vivamus ullamcorper purus mollis auctor
-  viverra. Aenean cursus bibendum felis non vulputate. Proin sed massa vitae
+  viverra. Aenean cursus bibendum felis non vulputate. Proin sed massa 
   sapien tincidunt pulvinar. Nunc mauris est, dapibus non tellus id,
   vehicula fermentum urna. Nullam iaculis purus massa`
-  @Input() imgSrc = "https://images.pexels.com/photos/11199063/pexels-photo-11199063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  @Input() imgSrc = "https://cdn.pixabay.com/photo/2023/01/30/06/43/village-7754827_640.jpg"
 
 }
