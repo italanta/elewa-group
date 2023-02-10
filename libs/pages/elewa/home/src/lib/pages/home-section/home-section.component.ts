9f0f20fad4d3a6894ae9beb3db3e20b07e6cc882
Machoka Joash Omao <aaronjoash44@gmail.com>
2023-02-10 12:38:17 +0300

    feat: removed home-section component from pages/home-page

--- libs/pages/elewa/home/src/lib/pages/home-section/home-section.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-home-section',
-  templateUrl: './home-section.component.html',
-  styleUrls: ['./home-section.component.scss'],
-  
-})
-export class HomeSectionComponent {}
