cd6bdd8ede9bc96e974c4025ad0072e23989ae70
Halkano Malise <kudezx@gmail.com>
2023-02-10 23:40:46 +0300

    ✨feat: Imported Button from ElewaGroupButton

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -4,9 +4,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { HomeImpactComponent} from  '../../home-impact/home-impact.component';
+import { ButtonsModule} from '@elewa-group/features/components/buttons'
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, ButtonsModule],
   declarations: [HomePageComponent, HomeHeroSectionComponent,HomeImpactComponent],
   exports: [HomePageComponent, HomeHeroSectionComponent,HomeImpactComponent],
 })
