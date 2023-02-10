bf71a3a50ee65756ca46c9a1de330052af1740e6
Machoka Joash Omao <aaronjoash44@gmail.com>
2023-02-10 12:30:04 +0300

    feat: moved home-section to components

---
+++ libs/pages/elewa/home/src/lib/components/home-section/home-section.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeSectionComponent } from './home-section.component';
+
+describe('HomeSectionComponent', () => {
+  let component: HomeSectionComponent;
+  let fixture: ComponentFixture<HomeSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeSectionComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    
+  });
+
+});
---
+++ libs/pages/elewa/home/src/lib/components/home-section/home-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-section',
+  templateUrl: './home-section.component.html',
+  styleUrls: ['./home-section.component.scss'],
+  
+})
+export class HomeSectionComponent {}
