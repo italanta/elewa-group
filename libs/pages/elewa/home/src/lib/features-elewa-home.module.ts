2b108c043b7cd94e5c8425efa5b6476975118493
Ian Mike <iammikeian@gmail.com>
2023-02-10 11:58:59 +0300

    ✨ feat: add our activity template and import cards module

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -4,9 +4,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { HomePageActivityComponent } from './home-page-activity/home-page-activity.component';
+import { CardsModule } from '@elewa-group/features/components/cards';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, CardsModule],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
