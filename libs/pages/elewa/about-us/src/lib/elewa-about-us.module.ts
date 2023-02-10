16a6efcaf47ed119d9013f4b66f0b8f2b5898dcf
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-10 21:35:54 +0300

    ✨Feat: adds export and declaration

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaAboutUsLocationSectionComponent } from './elewa-about-us-location-section/elewa-about-us-location-section.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [CommonModule,ElewaAboutUsLocationSectionComponent],
+  exports: [ElewaAboutUsLocationSectionComponent],
+  declarations: [ElewaAboutUsLocationSectionComponent],
 })
 export class AboutUsModule {}
