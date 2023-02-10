397f2b287b9f6af5f9ef31a01f3e654df26fb14f
littlephillips <phillipkinuthia72@gmail.com>
2023-02-10 11:06:57 +0300

    🎨 feat: added home-page-lander component and exported it

---
+++ libs/pages/elewa/home/src/lib/components/home-page-lander/home-page-lander.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-page-lander',
+  templateUrl: './home-page-lander.component.html',
+  styleUrls: ['./home-page-lander.component.scss'],
+})
+export class HomePageLanderComponent {}
