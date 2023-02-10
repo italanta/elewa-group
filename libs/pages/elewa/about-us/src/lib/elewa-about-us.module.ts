35a69c1d88e73bf643a6e7e43a79a912f36a852c
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-11 00:12:52 +0300

    ➕Added googlemapsmodule

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaAboutUsLocationSectionComponent } from './elewa-about-us-location-section/elewa-about-us-location-section.component';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 @NgModule({
   imports: [CommonModule,ElewaAboutUsLocationSectionComponent],
