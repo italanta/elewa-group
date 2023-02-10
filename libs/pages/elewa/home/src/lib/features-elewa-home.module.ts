f664535e71176455ea46710a34c41b52d65efd0b
Essyuge <ugeesther@gmail.com>
2023-02-10 13:00:33 +0300

    feat: include ImpactSection Component

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { ImpactSectionComponent } from './impact-section/impact-section.component';
+import{ButtonsModule}from '@elewa-group/features/components/buttons'
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
+  imports: [CommonModule, LayoutModule, ButtonsModule],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    ImpactSectionComponent,
+  ],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
 export class HomePageModule {}
