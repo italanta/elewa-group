6fe82d7d882b6a25724c522155f4539895d002a6
Rokyt69 <royokite@gmail.com>
2023-02-10 12:05:36 +0300

    :sparkles: feat: Generate team and partners component
    Generate the necessary files for the component.

---
+++ libs/pages/elewa/home/src/lib/components/team-and-partners/team-and-partners.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-team-and-partners',
+  templateUrl: './team-and-partners.component.html',
+  styleUrls: ['./team-and-partners.component.scss'],
+})
+export class TeamAndPartnersComponent {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-import { LayoutModule } from '@elewa-group/elements/layout';
+import { TeamAndPartnersComponent } from './components/team-and-partners/team-and-partners.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    TeamAndPartnersComponent,
+  ],
+  exports: [HomePageComponent, HomeHeroSectionComponent, TeamAndPartnersComponent],
 })
 export class HomePageModule {}
