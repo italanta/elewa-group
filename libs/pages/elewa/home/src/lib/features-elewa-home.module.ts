3f681505544afb14cd490b4ed35fbd6041220732
Halkano Malise <kudezx@gmail.com>
2023-02-10 12:11:22 +0300

    ✨feat:imported home-impact-component

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { HomeImpactComponent} from  '../../home-impact/home-impact.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [HomePageComponent, HomeHeroSectionComponent,HomeImpactComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeImpactComponent],
 })
 export class HomePageModule {}
