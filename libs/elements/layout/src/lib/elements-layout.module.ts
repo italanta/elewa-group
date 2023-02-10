cddc96e77d78fbb45ecdbfa96847d2224f2c3da4
martin mithi <gathitumithi9000@gmail.com>
2023-02-10 10:45:17 +0300

    Removed Banners component from elements directory

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { ElewaGroupImageAndTextBannerComponent } from './components/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
-
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent, ElewaGroupImageAndTextBannerComponent],
-  exports: [HeaderComponent, ElewaGroupImageAndTextBannerComponent],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class LayoutModule {}
