f1eed38857feef93f6864f808eac819b1edca819
George <sbeveme@gmail.com>
2023-02-10 11:56:37 +0300

    elewa-group-image-and-text-banner component created

---
+++ libs/features/components/banners/src/lib/component/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-image-and-text-banner',
+  templateUrl: './elewa-group-image-and-text-banner.component.html',
+  styleUrls: ['./elewa-group-image-and-text-banner.component.scss'],
+})
+export class ElewaGroupImageAndTextBannerComponent {}
