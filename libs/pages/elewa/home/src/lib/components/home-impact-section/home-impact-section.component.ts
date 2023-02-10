05569e1d12d33405f5bd43cb3d270355810b5389
Rajab Karume <rkarume@gmail.com>
2023-02-10 09:23:35 +0300

    ✨feat: added responsiveness to the home impact section

--- libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class HomeImpactSectionComponent {
   @Input() title = 'Growing Capital Impacting Lives';
-  @Input() image = 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
+  @Input() image = 'https://res.cloudinary.com/db15gy9h6/image/upload/v1675949508/Screenshot_from_2023-02-09_16-26-20_zdx7mn.png'
   @Input() description = `Elewa is proof that growing capital and impacting lives can work well together.
   Our group owned and managed by our founder, our employees (across our brands) and our network of 56 passionate angel investors.`
 }
