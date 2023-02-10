f09f12407e0dcf72d2512af4a83fc7d0cf76dc64
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-10 09:39:26 +0300

    📁 file: generated about-us location section

---
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us-location-section/elewa-about-us-location-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elewa-about-us-location-section',
+  templateUrl: './elewa-about-us-location-section.component.html',
+  styleUrls: ['./elewa-about-us-location-section.component.css']
+})
+export class ElewaAboutUsLocationSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
