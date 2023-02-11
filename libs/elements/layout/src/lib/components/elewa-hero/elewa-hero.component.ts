5d85b04135be462f768d20d0f9602a8928ff3d22
jeffoty <odhiambo47@gmail.com>
2023-02-11 20:36:07 +0300

    ✨ feat: created a top page banner

--- libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-hero',
   templateUrl: './elewa-hero.component.html',
   styleUrls: ['./elewa-hero.component.scss'],
 })
-export class ElewaHeroComponent {}
+export class ElewaHeroComponent {
+  @Input() title = 'HOW TO RECOGNIZE AN ELEWA MEMBER';
+  @Input() subTitle = 'Dependable, Creative, Supportive, Open Minded and Fun*';
+  @Input()  paragraph = '*SERIOUS WHEN IT MATTERS';
+
+}
