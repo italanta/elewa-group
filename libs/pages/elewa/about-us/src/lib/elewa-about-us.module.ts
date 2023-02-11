5e3d04a587ec214af952f3ee80fdef5a46869097
calvin <ckodalo@gmail.com>
2023-02-12 01:37:54 +0300

    feat: added styling and map functionality

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -5,5 +5,6 @@ import { AboutUsLocationSectionComponent } from './about-us-location-section/abo
 @NgModule({
   imports: [CommonModule],
   declarations: [AboutUsLocationSectionComponent],
+  exports: [AboutUsLocationSectionComponent]
 })
 export class AboutUsModule {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import {AboutUsModule} from '@elewa-group/pages/elewa/about-us'
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, AboutUsModule],
   declarations: [HomePageComponent, HomeHeroSectionComponent],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
