6c7bda9aa127345f900d9d7cfe7b8b2c1ab5466c
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-11 10:08:47 +0300

    ✨feat: added geolocation for maps marker position

--- libs/pages/elewa/about-us/src/lib/elewa-about-us-location-section/elewa-about-us-location-section.component.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us-location-section/elewa-about-us-location-section.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-about-us-location-section',
   templateUrl: './elewa-about-us-location-section.component.html',
   styleUrls: ['./elewa-about-us-location-section.component.css']
 })
-export class ElewaAboutUsLocationSectionComponent {
-
+export class ElewaAboutUsLocationSectionComponent implements OnInit {
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+ 
   // constructor() { }
-
-  // ngOnInit() {
-  // }
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+ 
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++;
+  }
+ 
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--;
+  }
 
 }
