c3884af859b1f06f3e76e0b20467198d0d989919
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-11 03:17:56 +0300

    :art:remove spaces

--- libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
+++ libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './component/footer/footer.component';
 
+
 @NgModule({
   imports: [CommonModule],
   declarations: [FooterComponent],
