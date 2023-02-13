894e1929873291aaf1798fde2afa081613eea576
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-13 09:05:22 +0300

    ✨:feat:added home-job-section to home components folder

---
+++ libs/pages/elewa/home/src/lib/components/home-job-section/home-job-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-job-section',
+  templateUrl: './home-job-section.component.html',
+  styleUrls: ['./home-job-section.component.scss'],
+})
+export class HomeJobSectionComponent {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
-
+import { HomeJobSectionComponent } from './components/home-job-section/home-job-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
-  
+    HomeJobSectionComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobSectionComponent],
 })
 export class HomePageModule {}
