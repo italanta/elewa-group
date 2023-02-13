2b309630ff4ea8fc58024b5a2d4947eed575e6dc
danstan <oduordanstan@gmail.com>
2023-02-13 13:48:43 +0300

    :sparkles: exported the hero component

--- libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-hero',
   templateUrl: './elewa-hero.component.html',
   styleUrls: ['./elewa-hero.component.scss'],
 })
-export class ElewaHeroComponent {}
+export class ElewaHeroComponent {
+  @Input() subtitle= "How to recognize an elewa member ";
+@Input() title = "Dependable, Creative, Supportive, Open Minded and Fun*";
+@Input() footerText= "*Serious when it matters";
+@Input() backgroundImageLink ="https://res.cloudinary.com/dghmulj4i/image/upload/v1667828146/cld-sample.jpg";
+}
