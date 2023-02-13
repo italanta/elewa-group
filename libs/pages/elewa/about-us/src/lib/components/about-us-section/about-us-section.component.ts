bee599369cf95d23d0ab568059dde2828c863de4
McRonaah <ronaldcheruiyot342@gmail.com>
2023-02-13 10:56:11 +0300

    fix:: images, title and description

--- libs/pages/elewa/about-us/src/lib/components/about-us-section/about-us-section.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/about-us-section/about-us-section.component.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './about-us-section.component.html',
   styleUrls: ['./about-us-section.component.scss'],
 })
-export class AboutUsSectionComponent {}
+export class AboutUsSectionComponent {
+  cultures = [
+    {
+      img: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png ',
+      title: 'Cooperative',
+      description: 'One for all all for one we have a cooperative and open mindset.if one of us grows and does well all should follow'
+    },
+    {
+      img: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/ownership_yno4a2.png',
+      title: 'Ownership',
+      description: 'We honor our commitments. We are dependable, and when things do not go as plan, as they sometimes do, we communicate early as to avoid catastrophe.'
+    },
+    {
+      img: 'https://static.thenounproject.com/png/1721983-200.png',
+      title: 'Detail',
+      description: 'We are a Centre of Excellence as per global standards. We only compete with ourselves but find in the global standard a worthy rival we continuously challenge.'
+    },
+    {
+      img: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png',
+      title: 'Empathy',
+      description: 'We are nothing without people, and people have different contexts and needs. Our organization acts as  a safe haven and shield for those in need.'
+    },
+    {
+      img: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690300/elewa-group-website/Icons/PNG/Transparancy_fqal8q.png',
+      title: 'Transparency',
+      description: 'Transparency, an open mind, and an open heart hold our organization together. We commit to and promote full transparency to ourselves, our stakeholders, and our beneficiaries. We furthermore invest heavily in open-source and open-data projects, as true potential lies everywhere. Not just within.'
+    }
+  ]
+}
+
+
