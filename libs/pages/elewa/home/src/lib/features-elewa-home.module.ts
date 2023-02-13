b8838ae85a7bc7d92f88b7474e95407563094391
Kangonga <guchu413@gmail.com>
2023-02-13 10:52:03 +0300

    refactor: change order of imports

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-import { LayoutModule } from '@elewa-group/elements/layout';
-import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
-import { Routes } from '@angular/router';
 
+import { LayoutModule } from '@elewa-group/elements/layout';
+
+import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
 @NgModule({
   imports: [CommonModule, LayoutModule],
   declarations: [
@@ -13,6 +14,6 @@ import { Routes } from '@angular/router';
     HomeHeroSectionComponent,
     HomeJobsSectionComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent],
 })
 export class HomePageModule {}
