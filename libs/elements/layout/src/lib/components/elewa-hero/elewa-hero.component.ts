837c6235f11e4794b6ab2882ad02dee2b7c22581
Vincent <makokhavomondi@gmail.com>
2023-02-13 13:00:48 +0300

    :recycle: use "ngIf" directive on the footer hero component footer element

--- libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -9,5 +9,5 @@ export class ElewaHeroComponent {
   @Input() subtitle = 'HOW TO RECOGNIZE AN ELEWA MEMBER';
   @Input() title = 'Dependable, Creative, Supportive, Open Minded and Fun*';
   @Input() backgroundImg = 'https://img.freepik.com/free-photo/abstract-flowing-neon-wave-background_53876-101942.jpg';
-  @Input() footer = '*SERIOUS WHEN IT MATTERS';
+  @Input() footerText = '*SERIOUS WHEN IT MATTERS';
 }
