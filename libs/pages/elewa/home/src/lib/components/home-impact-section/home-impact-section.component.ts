e9e3a19c90f4bb30c02ecf24c55d6928ace503d6
Rajab Karume <rkarume@gmail.com>
2023-02-13 11:31:51 +0300

    ✅ Removed Input in home-impact-section.component.ts

--- libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
@@ -1,13 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-home-impact-section',
   templateUrl: './home-impact-section.component.html',
   styleUrls: ['./home-impact-section.component.scss'],
 })
-export class HomeImpactSectionComponent {
-  @Input() title = 'Growing Capital Impacting Lives';
-  @Input() image = 'https://res.cloudinary.com/db15gy9h6/image/upload/v1675949508/Screenshot_from_2023-02-09_16-26-20_zdx7mn.png'
-  @Input() description = `Elewa is proof that growing capital and impacting lives can work well together.
-  Our group owned and managed by our founder, our employees (across our brands) and our network of 56 passionate angel investors.`
-}
+export class HomeImpactSectionComponent {}
