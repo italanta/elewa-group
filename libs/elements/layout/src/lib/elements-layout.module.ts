f8ff52dade08f0d744710bdcb00d67ca812c6f70
danstan <oduordanstan@gmail.com>
2023-02-13 13:32:17 +0300

    :sparkles: generated hero component

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { ElewaHeroComponent } from './components/elewa-hero/elewa-hero.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  declarations: [HeaderComponent, ElewaHeroComponent],
+  exports: [HeaderComponent],
 })
 export class LayoutModule {}
