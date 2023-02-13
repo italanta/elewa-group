bed78eb62e72d73eb30bb23e0ca768185fa041ee
Vincent <makokhavomondi@gmail.com>
2023-02-13 15:24:52 +0300

    :sparkles: Add input to the elewa-hero component

--- libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-hero',
   templateUrl: './elewa-hero.component.html',
   styleUrls: ['./elewa-hero.component.scss'],
 })
-export class ElewaHeroComponent {}
+export class ElewaHeroComponent {
+  @Input() subtitle = 'HOW TO RECOGNIZE AN ELEWA MEMBER';
+  @Input() title = 'Dependable, Creative, Supportive, Open Minded and Fun*';
+  @Input() backgroundImg = 'https://img.freepik.com/free-photo/abstract-flowing-neon-wave-background_53876-101942.jpg';
+  @Input() footerText = '*SERIOUS WHEN IT MATTERS';
+}
