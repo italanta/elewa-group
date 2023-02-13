2d443af23b21dd478ea76c5d7a42a5d9dd373b46
jeffoty <odhiambo47@gmail.com>
2023-02-13 13:58:12 +0300

    🔥 delete: removed layout page files

--- libs/elements/layout/src/lib/pages/layout-page/layout-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-layout-page',
-  templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.scss'],
-})
-export class LayoutPageComponent {}
