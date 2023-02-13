a405527df4db84178b52d928dd792df653606ea2
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-13 19:47:07 +0300

    ✨ Created horizontal timeline component

---
+++ libs/features/components/ui-lists/src/lib/elewa-horizontal-timeline-carousel/elewa-horizontal-timeline-carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-horizontal-timeline-carousel',
+  templateUrl: './elewa-horizontal-timeline-carousel.component.html',
+  styleUrls: ['./elewa-horizontal-timeline-carousel.component.scss'],
+})
+export class ElewaHorizontalTimelineCarouselComponent {}
--- libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
+++ libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaGroupVerticalListOneComponent } from './elewa-group-vertical-list-one/elewa-group-vertical-list-one.component';
+import { ElewaHorizontalTimelineCarouselComponent } from './elewa-horizontal-timeline-carousel/elewa-horizontal-timeline-carousel.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaGroupVerticalListOneComponent],
-  exports: [ElewaGroupVerticalListOneComponent]
+  declarations: [
+    ElewaGroupVerticalListOneComponent,
+    ElewaHorizontalTimelineCarouselComponent,
+  ],
+  exports: [ElewaGroupVerticalListOneComponent],
 })
 export class UiListsModule {}
