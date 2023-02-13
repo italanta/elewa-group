1ed6587d6e51400c36a5388a087b022ccfc7f4af
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-13 15:47:00 +0300

    :bricks:insert mission component into components folder

---
+++ libs/pages/elewa/about-us/src/lib/components/mission/mission.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-mission',
+  templateUrl: './mission.component.html',
+  styleUrls: ['./mission.component.scss'],
+})
+export class MissionComponent {}
