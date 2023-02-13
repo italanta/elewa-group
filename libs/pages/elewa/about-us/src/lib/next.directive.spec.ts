8b11cc8ad129cc9db841c73b61779f258a9ac603
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-02-13 12:50:52 +0300

    :truck: moved directives to dedicated folder

--- libs/pages/elewa/about-us/src/lib/next.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NextDirective } from './next.directive';
-
-describe('NextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
