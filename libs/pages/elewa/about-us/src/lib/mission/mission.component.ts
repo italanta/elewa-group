b5dc36218b28c645a86b7ae530b4493deef17851
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-13 16:00:18 +0300

    :fire:move mission component to components folder

--- libs/pages/elewa/about-us/src/lib/mission/mission.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-mission',
-  templateUrl: './mission.component.html',
-  styleUrls: ['./mission.component.scss'],
-})
-export class MissionComponent {}
