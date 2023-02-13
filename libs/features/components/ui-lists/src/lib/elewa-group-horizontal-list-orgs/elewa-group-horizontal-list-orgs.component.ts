33910717d4221abc18e791847eed4a3b1378bb8a
kahoragachau <joelkahoragachau6@gmail.com>
2023-02-13 09:38:12 +0300

    🎨Chore: Remove IDs

--- libs/features/components/ui-lists/src/lib/elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component.ts
+++ libs/features/components/ui-lists/src/lib/elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component.ts
@@ -8,31 +8,24 @@ import { Component } from '@angular/core';
 export class ElewaGroupHorizontalListOrgsComponent {
   imageUrl = [
     {
-      id: 1,
       "image": "https://rwanda.vvob.org/sites/rwanda/files/styles/780pxx330px/public/images/mainimagebig/ok.jpg?itok=zMR2_g-z"
     },
     {
-      id: 2,
       "image": "https://images.squarespace-cdn.com/content/v1/5c9b88e00cf57d033337013f/1560884247023-4T57CS4T1C6VX94JK5JV/farmbetter_for_website.png?format=1500w"
     },
     {
-      id: 3,
       "image": "https://www.greenclimate.fund/sites/default/files/organisation/logo-enabel.png"
     },
     {
-      id: 4,
       "image": "https://savics.org/wp-content/uploads/2020/10/SavicsREF_Horizontal_RGB.jpg"
     },
     {
-      id: 5,
       "image": "https://www.bigmaninthewoods.co.uk/wp-content/uploads/2018/05/1341315_scouts_logo_horizontal_purple.png"
     },
     {
-      id: 6,
       "image": "https://wwfke.awsassets.panda.org/img/wwf_logo_750_x_600_490968.jpg"
     },
     {
-      id: 7,
       "image": "https://wwfke.awsassets.panda.org/img/wwf_logo_750_x_600_490968.jpg"
     }
   ];
