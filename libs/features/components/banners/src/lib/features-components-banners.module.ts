85e20d75c7de8056933617d1c9763251e5f0d890
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-13 10:18:35 +0300

    #31:art:Deleted the extra lines or the whitespace

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
+import { ElewaGroupImageAndTextBannerComponent } from './components/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
