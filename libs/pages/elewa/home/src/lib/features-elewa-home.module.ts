4340384bb5fd887d7fe524c441de886faf493d99
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-13 12:41:13 +0300

    🔥🎨 Changes requested have been made to the issue

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { JobsSectionComponent } from './components/jobs-section/jobs-section.component';
-import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Routes, RouterModule } from '@angular/router';
+
+// routes
+const routes: Routes = [
+  {
+    path: 'jobs',
+    component: JobsSectionComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule, LayoutModule],
+  imports: [CommonModule, LayoutModule, RouterModule.forRoot(routes)],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
