e9421f91133fc9262066627c07af2e4b7ebc778e
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-13 12:24:36 +0300

    🔧fix: added spacing

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import { ElewaAboutUsLocationSectionComponent } from './elewa-about-us-location-section/elewa-about-us-location-section.component';
+
 import { GoogleMapsModule } from '@angular/google-maps'
+
 import { ButtonsModule } from "@elewa-group/features/components/buttons"
 
+
 @NgModule({
-  imports: [CommonModule,GoogleMapsModule, ButtonsModule],
+  imports: [
+    CommonModule,
+    GoogleMapsModule,
+    ButtonsModule],
+
   exports: [ElewaAboutUsLocationSectionComponent],
+
   declarations: [ElewaAboutUsLocationSectionComponent],
 })
+
 export class AboutUsModule {}
