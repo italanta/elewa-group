006a37e0a6b2c59be5dcc5cb6693e00b89464dd2
JohnstoneDev <marucha.webdev@yahoo.com>
2023-02-13 12:42:03 +0300

    fix: Move Local imports last

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
-import { ImpactSectionComponent } from './impact-section/impact-section.component';
 import{ButtonsModule}from '@elewa-group/features/components/buttons'
 
+
+import { ImpactSectionComponent } from './impact-section/impact-section.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 @NgModule({
   imports: [CommonModule, LayoutModule, ButtonsModule],
   declarations: [
