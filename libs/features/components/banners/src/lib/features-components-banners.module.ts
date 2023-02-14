422400055d4913778d6507477693f95d1d75a6fb
Vincent <makokhavomondi@gmail.com>
2023-02-14 14:57:46 +0300

    :construction: Update the kind of inputs accepted by the component

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -13,7 +13,13 @@ import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-ima
 })
 export class BannersModule {
   @Input() imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png'
-  @Input() paragraphText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel ante sagittis, tempus turpis in, condimentum erat. Praesent dictum velit nunc, sagittis tristique tellus imperdiet ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus libero felis, varius ac ante quis'
+  @Input() paragraphText = `Elewa's businesses have one common objective, to
+                            unlock true potential of individuals, teams, and
+                            communities. All our talents are enrolled in a personal
+                            growth track. In turn, they contribute teir own growth
+                            towards teh growth of others, the group and their
+                            communities.`
+  @Input() titleText = `A cooperative mindset`
   @Input() imageplacement = 'left'
   @Input() backgroundColor = '#000000'
 }
