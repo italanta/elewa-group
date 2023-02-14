3055c8e2adb1d4ecaf0e645c0723c062ca30a421
Vincent <makokhavomondi@gmail.com>
2023-02-14 15:24:47 +0300

    :construction: Move the input stuff to the right file

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -11,15 +11,4 @@ import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-ima
   ],
   exports: [ElewaGroupTeamMemberComponent, ElewaGroupImageAndTextBannerComponent],
 })
-export class BannersModule {
-  @Input() imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png'
-  @Input() paragraphText = `Elewa's businesses have one common objective, to
-                            unlock true potential of individuals, teams, and
-                            communities. All our talents are enrolled in a personal
-                            growth track. In turn, they contribute teir own growth
-                            towards teh growth of others, the group and their
-                            communities.`
-  @Input() titleText = `A cooperative mindset`
-  @Input() imageplacement = 'left'
-  @Input() backgroundColor = '#000000'
-}
+export class BannersModule {}
