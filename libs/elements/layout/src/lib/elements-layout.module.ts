ecec3d2e4f47ef81b4f94bd3f2c84c3fe67d070c
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-14 14:36:08 +0300

    :hammer:import buttonsmodule

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {ButtonsModule} from '@elewa-group/features/components/buttons'
 
 import { HeaderComponent } from './components/header/header.component';
 import { ElewaGroupMainPageComponent } from './pages/elewa-group-main-page/elewa-group-main-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,ButtonsModule],
   declarations: [HeaderComponent, ElewaGroupMainPageComponent, FooterComponent],
   exports: [ElewaGroupMainPageComponent, FooterComponent],
 })
