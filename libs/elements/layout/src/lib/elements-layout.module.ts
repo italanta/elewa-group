89bd8cd21a083c7fb47e158e55a0a5a4a7646570
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-14 07:51:22 +0300

    :hammer:add footer component

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ElewaGroupMainPageComponent } from './pages/elewa-group-main-page/elewa-group-main-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent, ElewaGroupMainPageComponent],
-  exports: [ElewaGroupMainPageComponent],
+  declarations: [HeaderComponent, ElewaGroupMainPageComponent, FooterComponent],
+  exports: [ElewaGroupMainPageComponent, FooterComponent],
 })
 export class LayoutModule {}
