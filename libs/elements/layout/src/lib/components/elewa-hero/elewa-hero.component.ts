63d315fd0ae99b7183306aff16cb4f8d3cafa9b4
danstan <oduordanstan@gmail.com>
2023-02-14 10:10:53 +0300

    :sparkles: generated the elewa hero component

---
+++ libs/elements/layout/src/lib/components/elewa-hero/elewa-hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-hero',
+  templateUrl: './elewa-hero.component.html',
+  styleUrls: ['./elewa-hero.component.scss'],
+})
+export class ElewaHeroComponent {}
