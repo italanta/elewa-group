806a2d541de5a4787d3700d04f812f1c4bced647
jeffoty <odhiambo47@gmail.com>
2023-02-14 23:26:42 +0300

    ✨ feat: added social impact top banner feature

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialImpactComponent } from './social-impact/social-impact.component';
+import { LayoutModule } from '@elewa-group/elements/layout'; 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule],
   declarations: [SocialImpactComponent],
+  exports: [SocialImpactComponent]
 })
 export class PagesElewaSocialImpactModule {}
+
--- libs/pages/elewa/social-impact/src/lib/social-impact/social-impact.component.ts
+++ libs/pages/elewa/social-impact/src/lib/social-impact/social-impact.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-social-impact',
   templateUrl: './social-impact.component.html',
   styleUrls: ['./social-impact.component.scss'],
 })
-export class SocialImpactComponent {}
+export class SocialImpactComponent {
+  heroes = [
+    {
+      title: 'A vehicle for rapid & schalable human development',
+      subtitle: 'Social Enterprise',
+      footerText: '',
+      backgroundImg: 'https://images.pexels.com/photos/7437489/pexels-photo-7437489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+  ]
+}
