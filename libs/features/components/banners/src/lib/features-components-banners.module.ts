4b8a00ceec54c652f7092608605c6a1f2bedb2fb
George <sbeveme@gmail.com>
2023-02-14 13:53:18 +0300

    exporting reusable banner component

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaGroupImageAndTextBannerComponent } from './component/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaGroupImageAndTextBannerComponent],
+  exports : [ElewaGroupImageAndTextBannerComponent]
 })
 export class FeaturesComponentsBannersModule {}
