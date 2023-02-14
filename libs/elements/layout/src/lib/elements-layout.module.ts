a8d363717c37f938b634392a9b77d3be88644bb8
Michael Gichuhi <gmikekamau@gmail.com>
2023-02-14 14:08:48 +0300

    Merge branch 'main' of github.com:JG-Mike/elewa-group into 15/website/footer

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -4,10 +4,12 @@ import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ElewaGroupMainPageComponent } from './pages/elewa-group-main-page/elewa-group-main-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent, ElewaGroupMainPageComponent],
+  imports: [CommonModule, RouterModule, ButtonsModule],
+  declarations: [HeaderComponent, ElewaGroupMainPageComponent, FooterComponent, ],
   exports: [ElewaGroupMainPageComponent],
 })
 export class LayoutModule {}
