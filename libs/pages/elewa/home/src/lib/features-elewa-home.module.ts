22f586230201ff631e282efe076d39c3a8433562
+ <+>
2023-02-14 14:51:21 +0300

    feat: Created a Reusable Timeline Carousel

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -5,6 +5,7 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 
 import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
+import { HorizontalTimelineCarouselComponent } from 'libs/features/components/ui-lists/src/lib/components/elewa-group-horizontal-timeline-carousel/elewa-group-horizontal-timeline-carousel.component';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
@@ -20,8 +21,9 @@ import { HomeRoutingModule } from './home.routing';
     HomePageComponent,
     HomeHeroSectionComponent,
     HomeJobsSectionComponent,
+    HorizontalTimelineCarouselComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent, HorizontalTimelineCarouselComponent],
 
 })
 export class HomePageModule {}
