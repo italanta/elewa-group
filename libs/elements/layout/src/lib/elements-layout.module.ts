8493c0d046b20174887d6aada8b21e095e4cb595
jeffoty <odhiambo47@gmail.com>
2023-02-14 12:00:33 +0300

    refactor: importing elewa hero into layout module

--- libs/elements/layout/src/lib/elements-layout.module.ts
+++ libs/elements/layout/src/lib/elements-layout.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ElewaGroupMainPageComponent } from './pages/elewa-group-main-page/elewa-group-main-page.component';
+import { ElewaHeroComponent} from './components/elewa-hero/elewa-hero.component'
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent, ElewaGroupMainPageComponent],
+  declarations: [HeaderComponent, ElewaGroupMainPageComponent, ElewaHeroComponent],
   exports: [ElewaGroupMainPageComponent],
 })
 export class LayoutModule {}
