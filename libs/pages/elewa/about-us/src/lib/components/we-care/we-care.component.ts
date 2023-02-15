63bc17931988387b288a97b47ccdc48ba0ce0b7e
Kangonga <guchu413@gmail.com>
2023-02-15 18:33:39 +0300

    feat: Add the icons array

--- libs/pages/elewa/about-us/src/lib/components/we-care/we-care.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/we-care/we-care.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './we-care.component.html',
   styleUrls: ['./we-care.component.scss'],
 })
-export class WeCareComponent {}
+export class WeCareComponent {
+  icons = [
+    {
+      icon: "fa fa-user-group",
+      title: "Holistic solutions",
+      description: "We go beyond a simple patch-up but develop lasting solutions through holistic design."
+    },
+    {
+      icon: "fa fa-infinity",
+      title: "Impact",
+      description: "Impact as a direct, or indirect, result. All our respective organizations have underlying theories of change."
+    },    {
+      icon: "fa fa-globe",
+      title: "Open data",
+      description: "Sharing is caring. We share what we learn. All our internal projects are open source."
+    },
+  ]
+}
