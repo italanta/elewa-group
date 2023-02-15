27b6df1784e9e59f84c738a6c9360361f6f730ea
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-15 11:49:50 +0300

    🔥 Removed edits from home page and module

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -5,7 +5,6 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 
 import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-import {ElewaHorizontalTimelineCarouselComponent} from 'libs/features/components/ui-lists/src/lib/elewa-horizontal-timeline-carousel/elewa-horizontal-timeline-carousel.component'
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
@@ -21,8 +20,7 @@ import { HomeRoutingModule } from './home.routing';
     HomePageComponent,
     HomeHeroSectionComponent,
     HomeJobsSectionComponent,
-    ElewaHorizontalTimelineCarouselComponent
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent, ElewaHorizontalTimelineCarouselComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent],
 })
 export class HomePageModule {}
