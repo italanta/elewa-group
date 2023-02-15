d88f0dd6210da9536aa87a3f44d1bf3e028cb168
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-15 10:19:54 +0300

    🎉 ✨ generate the html, ts and scss files for banner

--- libs/features/components/elewa-group-image-and-text-banner/src/lib/features-components-elewa-group-image-and-text-banner.module.ts
+++ libs/features/components/elewa-group-image-and-text-banner/src/lib/features-components-elewa-group-image-and-text-banner.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaGroupImageAndTextBannerComponent } from './elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaGroupImageAndTextBannerComponent],
 })
 export class FeaturesComponentsElewaGroupImageAndTextBannerModule {}
