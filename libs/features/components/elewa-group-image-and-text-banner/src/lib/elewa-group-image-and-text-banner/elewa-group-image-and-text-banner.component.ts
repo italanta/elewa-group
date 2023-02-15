e0fdf1e817ae7df9eb48e019238fdc56bc9d2df7
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-15 13:32:05 +0300

    content rendering using input decorators

--- libs/features/components/elewa-group-image-and-text-banner/src/lib/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
+++ libs/features/components/elewa-group-image-and-text-banner/src/lib/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-image-and-text-banner',
   templateUrl: './elewa-group-image-and-text-banner.component.html',
   styleUrls: ['./elewa-group-image-and-text-banner.component.scss'],
 })
-export class ElewaGroupImageAndTextBannerComponent {}
+export class ElewaGroupImageAndTextBannerComponent {
+  @Input() imagePlacement: string
+  @Input() title: string
+  @Input() imageUrl: string
+  @Input() paragraphText: string
+}
