449dc5de0cc99b43911e35b2a2b3db0eb3d7a1ca
munene brian <njerubrianmunene@gmail.com>
2023-02-15 13:37:26 +0300

    feat: export the component

--- libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
+++ libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaGroupVerticalListOneComponent } from './elewa-group-vertical-list-one/elewa-group-vertical-list-one.component';
 import { ElewaGroupHorizontalListOrgsComponent } from './elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component';
+import { ElewaGroupVerticalListAllComponent } from './elewa-group-vertical-list-all/elewa-group-vertical-list-all.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     ElewaGroupVerticalListOneComponent,
     ElewaGroupHorizontalListOrgsComponent,
+    ElewaGroupVerticalListAllComponent,
   ],
-  exports: [ElewaGroupVerticalListOneComponent, ElewaGroupHorizontalListOrgsComponent],
+  exports: [
+    ElewaGroupVerticalListOneComponent,
+    ElewaGroupHorizontalListOrgsComponent,
+    ElewaGroupVerticalListAllComponent
+  ],
 })
 export class UiListsModule {}
