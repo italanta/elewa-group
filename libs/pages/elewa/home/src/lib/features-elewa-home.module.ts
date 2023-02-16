f421bc3047002e5b44d64ceaccd9d1dd80e207f4
JohnstoneDev <marucha.webdev@yahoo.com>
2023-02-16 10:09:50 +0300

    feat: Import BannersModule and add it to the declarations

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
@@ -9,18 +10,19 @@ import { HomeHeroSectionComponent } from './components/home-hero-section/home-he
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
+import { SocialImpactComponent } from './social-impact/social-impact.component';
 @NgModule({
-  imports: [
-    CommonModule,
-    LayoutModule,
-
-    HomeRoutingModule
-  ],
+  imports: [CommonModule, LayoutModule, HomeRoutingModule, BannersModule],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
     HomeJobsSectionComponent,
+    SocialImpactComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent],
+  exports: [
+    HomePageComponent,
+    HomeHeroSectionComponent,
+    HomeJobsSectionComponent,
+  ],
 })
 export class HomePageModule {}
