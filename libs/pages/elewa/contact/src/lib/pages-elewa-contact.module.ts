689a6b18d1f63e8d25b0ba60d1c73b5767eb8689
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-16 23:37:33 +0300

    📦feat: added contact route and imports paths

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [ContactPageComponent],
 })
 export class ContactPageModule {}
