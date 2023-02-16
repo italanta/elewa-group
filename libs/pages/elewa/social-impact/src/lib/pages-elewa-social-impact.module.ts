223eab11142da7dfe5b1b8dfdda5b3c58a2afff8
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-16 10:54:55 +0300

    add social impact lib

---
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialImpactComponent } from './social-impact/social-impact.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [SocialImpactComponent],
+})
+export class PagesElewaSocialImpactModule {}
---
+++ libs/pages/elewa/social-impact/src/lib/social-impact/social-impact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-social-impact',
+  templateUrl: './social-impact.component.html',
+  styleUrls: ['./social-impact.component.scss'],
+})
+export class SocialImpactComponent {}
