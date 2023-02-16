df402f9a5a87c19e7f15cf0e4401c486bf8c4ed0
Daniel <danielngoys@outlook.com>
2023-02-16 11:27:33 +0300

    ✨: impact lander logic

---
+++ libs/pages/elewa/home/src/lib/components/impact/impact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-impact',
+  templateUrl: './impact.component.html',
+  styleUrls: ['./impact.component.scss'],
+})
+export class ImpactComponent {
+  paragraph1 = "Elewa is a proof that growing capital and impacting lives can work well together."
+  paragraph2 = "Our group owned and managed by our founder, our employees (acreoss our brands) and ou network of 56 passionate angel investors."
+  image = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png'
+  title = 'Growing capital impacting lives'
+}
