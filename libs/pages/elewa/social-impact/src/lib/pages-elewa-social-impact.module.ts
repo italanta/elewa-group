078fa068cf47c8f59766bb49c82e3895da4a97fe
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-16 12:49:12 +0300

    #34:art:import the bannersmodule

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialImpactOpenKnowledgeComponent } from './component/social-impact-open-knowledge/social-impact-open-knowledge.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
+import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [SocialImpactOpenKnowledgeComponent],
+  imports: [CommonModule, BannersModule],
+
+  declarations: [SocialImpactOpenKnowledgeComponent, SocialImpactPageComponent],
 })
 export class PagesElewaSocialImpactModule {}
