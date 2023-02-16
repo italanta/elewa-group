2b3a99ad12d4638eb47141927f9f4327aa4b8553
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-16 08:50:22 +0300

    #31:zap: recreating the deleted code due to merging conflicts

---
+++ libs/features/components/banners/src/lib/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-image-and-text-banner',
+  templateUrl: './elewa-group-image-and-text-banner.component.html',
+  styleUrls: ['./elewa-group-image-and-text-banner.component.scss'],
+})
+export class ElewaGroupImageAndTextBannerComponent {
+  @Input() img = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png';
+  @Input() description = 'Elewa\'s Businessess have one common objective, to unlock true potential of individuals, teams, and communities. All out talents are enrolled in a personal growth track. In turn, they contribute their own growth towards the growth of others, the group and their communities.';
+  @Input() title = 'A cooperative mindset';
+  @Input() imagePlacement: 'left' | 'right' = 'right';
+  @Input() backgroundColor = '#f4f4f4';
+}
