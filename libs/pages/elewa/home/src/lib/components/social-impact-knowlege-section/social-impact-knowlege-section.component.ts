d348e6e17b5f5aa951c31a9850d986ee63edab93
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-02-16 12:18:29 +0300

    feat: social impact knowledge section
    1. An image on the right side
    2. A paragraph of text
    3. A title

---
+++ libs/pages/elewa/home/src/lib/components/social-impact-knowlege-section/social-impact-knowlege-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-social-impact-knowlege-section',
+  templateUrl: './social-impact-knowlege-section.component.html',
+  styleUrls: ['./social-impact-knowlege-section.component.scss'],
+})
+export class SocialImpactKnowlegeSectionComponent {}
