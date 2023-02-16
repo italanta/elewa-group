1c907bd12f4f27de5a8e3e92ebafcf15355507b7
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-16 12:50:56 +0300

    #34:art:resolve the emerging conflicts in the files

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -8,14 +8,3 @@ import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-ima
   exports: [ElewaGroupImageAndTextBannerComponent],
 })
 export class BannersModule {}
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ElewaGroupTeamMemberComponent } from './banners/elewa-group-team-member/elewa-group-team-member.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [ElewaGroupTeamMemberComponent],
-  exports: [ElewaGroupTeamMemberComponent]
-})
-export class BannersModule {}
-
