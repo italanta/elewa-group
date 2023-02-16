e72e220c52fd6d250dcbda94506df8f630277296
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-16 12:57:35 +0300

    ✨feat: added all the routes

--- apps/elewa-group-website/src/app/app.routing.ts
+++ apps/elewa-group-website/src/app/app.routing.ts
@@ -13,7 +13,7 @@ import { RouterModule, Route }  from '@angular/router';
 //   {  path: 'activities/en', loadChildren: () => import('libs/pages/elewa/activities/src/lib/features-elewa-home.module').then(m => m.ActivitiesPageModule)},
 //   {  path: 'aboutUs', redirectTo: 'aboutUs/en'  },
 //   {  path: 'aboutUs/en', loadChildren: () => import('libs/pages/elewa/aboutUs/src/lib/features-elewa-home.module').then(m => m.AboutPageModule)},
-//   {  path: 'socialImpact', redirectTo: 'socialImpact/en'  },
+//   {  path: 'social-impact', redirectTo: 'socialImpact/en'  },
 //   {  path: 'socialImpact/en', loadChildren: () => import('libs/pages/elewa/socialImpact/src/lib/features-elewa-socialImpact.module').then(m => m.SocialImpactPageModule)},
 //   {  path: 'invest', redirectTo: 'invest/en'  },
 //   {  path: 'invest/en', loadChildren: () => import('libs/pages/elewa/invest/src/lib/features-elewa-invest.module').then(m => m.InvestPageModule)},
@@ -35,6 +35,25 @@ export const ELEWA_GROUP_ROUTES: Route[] = [
   { path: 'about', redirectTo: 'about/en' },
   { path: 'about/en', loadChildren: () => import('libs/pages/elewa/about-us/src/lib/elewa-about-us.module').then(m => m.AboutUsModule) },
 
+  // activities page route
+  { path: 'activities', redirectTo: 'activities/en' },
+  { path: 'activities/en', loadChildren: () => import('libs/pages/elewa/activities/src/lib/elewa-activities.module').then(m => m.ActivitiesModule) },
+
+  // social-impact page route
+  { path: 'social-impact', redirectTo: 'social-impact/en' },
+  { path: 'social-impact/en', loadChildren: () => import('libs/pages/elewa/social-impact/src/lib/elewa-social-impact.module').then(m => m.SocialImpactModule) },
+
+  // invest page route
+  {  path: 'invest', redirectTo: 'invest/en'  },
+  {  path: 'invest/en', loadChildren: () => import('libs/pages/elewa/invest/src/lib/features-elewa-invest.module').then(m => m.InvestPageModule)},
+
+  // news page route
+  {  path: 'news', redirectTo: 'news/en'  },
+  {  path: 'news/en', loadChildren: () => import('libs/pages/elewa/news/src/lib/features-elewa-news.module').then(m => m.NewsPageModule)},
+
+  // contact page route
+  {  path: 'contact', redirectTo: 'contact/en'  },
+  {  path: 'contact/en', loadChildren: () => import('libs/pages/elewa/contact/src/lib/features-elewa-contact.module').then(m => m.ContactPageModule)},
 ];
 
 @NgModule({
