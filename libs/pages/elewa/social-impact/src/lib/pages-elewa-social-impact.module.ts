9d547e98c1ee40e6d3a10cafc0e0a3452453fede
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-16 11:21:51 +0300

    :hammer:export SocialImpactComponent

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactComponent } from './social-impact/social-impact.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,LayoutModule],
   declarations: [SocialImpactComponent],
+  exports:[SocialImpactComponent]
 })
 export class PagesElewaSocialImpactModule {}
