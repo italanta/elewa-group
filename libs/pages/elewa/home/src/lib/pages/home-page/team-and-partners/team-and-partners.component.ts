405394bb37cc3024ef5ab4fe8961498f2f4ab205
Pronepoet <ochiengphillip84@gmail.com>
2023-02-16 03:17:06 +0300

    Created Team & Partners image component #9

--- libs/pages/elewa/home/src/lib/pages/home-page/team-and-partners/team-and-partners.component.ts
+++ libs/pages/elewa/home/src/lib/pages/home-page/team-and-partners/team-and-partners.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
 export class TeamAndPatrnersComponent {
   @Input() image= "https://cdn.pixabay.com/photo/2019/05/13/20/38/team-4200837_960_720.jpg";
   @Input() imgWidth = '300';
-  @Input() imgHeight = '200';
+  @Input() imgHeight = '180';
 }
