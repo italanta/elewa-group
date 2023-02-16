6168a2e94ae7436a6099eda07190e7d36e55be9e
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-16 15:21:51 +0300

    :hammer:move cta-mission to components folder

--- libs/pages/elewa/home/src/lib/cta-mission/cta-mission.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-cta-mission',
-  templateUrl: './cta-mission.component.html',
-  styleUrls: ['./cta-mission.component.scss'],
-})
-export class CtaMissionComponent {}
