12925882d9f429fda9431962a0f79a894e701f93
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-16 23:02:53 +0300

    📦feat: added news route and imports

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsPageComponent } from './pages/news-page/news-page/news-page.component';
+
+import { NewsRoutingModule } from './news.routing';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NewsRoutingModule],
+  declarations: [NewsPageComponent],
 })
 export class NewsPageModule {}
