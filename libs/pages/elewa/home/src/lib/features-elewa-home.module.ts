37deb5d851d8edd5920ced266a6d91f65127af61
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-02-16 12:08:49 +0300

    ✨feat:Addeb neccessary import for the Activity  Components

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -9,10 +9,17 @@ import { HomeHeroSectionComponent } from './components/home-hero-section/home-he
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
+import { ActivitySectionComponent } from './components/activity-section/activity-section.component';
+import { CardsModule } from '@elewa-group/features/components/cards';
+import { PrevDirective } from './components/Directives/prev.directive';
+import { NextDirective } from './components/Directives/next.directive';
+
+
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
+    CardsModule,
 
     HomeRoutingModule
   ],
@@ -20,8 +27,12 @@ import { HomeRoutingModule } from './home.routing';
     HomePageComponent,
     HomeHeroSectionComponent,
     HomeJobsSectionComponent,
+    ActivitySectionComponent,
+    NextDirective,
+    PrevDirective,
+    
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent],
+  exports: [HomePageComponent, HomeHeroSectionComponent,HomeJobsSectionComponent,ActivitySectionComponent],
 
 })
 export class HomePageModule {}
