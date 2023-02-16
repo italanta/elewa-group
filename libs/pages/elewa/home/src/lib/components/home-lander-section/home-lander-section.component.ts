0dcdba5a420a80a86e37e168f1b94c0ee92afd22
littlephillips <phillipkinuthia72@gmail.com>
2023-02-16 14:40:59 +0300

    🎨 feat: added elewa-home-lander and exported it

---
+++ libs/pages/elewa/home/src/lib/components/home-lander-section/home-lander-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-lander-section',
+  templateUrl: './home-lander-section.component.html',
+  styleUrls: ['./home-lander-section.component.scss'],
+})
+export class HomeLanderSectionComponent {}
