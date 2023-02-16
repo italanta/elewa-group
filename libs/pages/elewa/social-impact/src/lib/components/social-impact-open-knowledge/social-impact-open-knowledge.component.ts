e2c3c4f2b891d4ccb8c5f68fb958a5bbbfaeea1a
nziokikevin <kevinnzioki10@gmail.com>
2023-02-16 14:48:48 +0300

    :sparkles: feat: adding data to the reusable component

--- libs/pages/elewa/social-impact/src/lib/components/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './social-impact-open-knowledge.component.html',
   styleUrls: ['./social-impact-open-knowledge.component.scss'],
 })
-export class SocialImpactOpenKnowledgeComponent {}
+export class SocialImpactOpenKnowledgeComponent {
+  titleText = "Open knowledge"
+  paragraphText = "What we learn, we share. Through community events, open knowledge repositories, regular teaching moments(everyone a teacher) and academic partnerships. We build for today, with a lens for tomorrow."
+  backgroundColor = "black"
+  imageURL = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/unnamed_1_m3dvll.png"
+}
--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BannersModule],
   declarations: [SocialImpactOpenKnowledgeComponent],
+  exports: [SocialImpactOpenKnowledgeComponent]
 })
 export class PagesElewaSocialImpactModule {}
