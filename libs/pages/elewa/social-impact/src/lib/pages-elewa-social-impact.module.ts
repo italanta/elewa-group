6cbba41b7ec9a0c758864f4a5dd87aaf3940c1c4
nziokikevin <kevinnzioki10@gmail.com>
2023-02-16 14:24:45 +0300

    :sparkles: feat: generating of social impact open knowledge component

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [SocialImpactOpenKnowledgeComponent],
 })
 export class PagesElewaSocialImpactModule {}
