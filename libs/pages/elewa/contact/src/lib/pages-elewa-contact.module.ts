3d242914193d7a8b610d22799bd138f7660f0fc4
James Mburu Githire <jamesmburug24@gmail.com>
2023-02-17 00:25:36 +0300

    🔧 fix: added contactRoutingModule

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ContactRoutingModule],
   declarations: [ContactPageComponent],
 })
 export class ContactPageModule {}
