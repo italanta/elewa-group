94bbe50fb150c6929d11f9f102579abb08d99597
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-16 15:20:57 +0300

    :hammer:import ButttonsModule

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
@@ -9,9 +10,9 @@ import { HomeHeroSectionComponent } from './components/home-hero-section/home-he
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
-import { CtaMissionComponent } from './cta-mission/cta-mission.component';
+import { CtaMissionComponent } from './components/cta-mission/cta-mission.component';
 @NgModule({
-  imports: [CommonModule, LayoutModule, HomeRoutingModule],
+  imports: [CommonModule, LayoutModule, HomeRoutingModule,ButtonsModule],
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
