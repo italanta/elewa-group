31f1cf15e77e6498928e1cbf13c3fa4fad4992d2
Snowflake <onyangogeorge954@gmail.com>
2023-02-17 09:51:48 +0300

    complete task

--- libs/pages/elewa/social-impact/src/lib/component/people-section/people-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/component/people-section/people-section.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-people-section',
   templateUrl: './people-section.component.html',
   styleUrls: ['./people-section.component.scss'],
 })
-export class PeopleSectionComponent {}
+export class PeopleSectionComponent {
+
+peopleTrained = 0;
+
+peopleTrainedCountStop = setInterval(() => {
+  this.peopleTrained++;
+
+  if (this.peopleTrained == 733) {
+    clearInterval(this.peopleTrainedCountStop)
+  }
+}, 5)
+
+careers = 0;
+
+careersCountStop = setInterval(() => {
+  this.careers++;
+
+  if (this.careers == 13000) {
+    clearInterval(this.careersCountStop)
+  }
+}, 5)
+
+projectCount = 0;
+
+projectCountStop = setInterval(() => {
+  this.projectCount++;
+
+  if (this.projectCount == 200) {
+    clearInterval(this.projectCountStop)
+  }
+}, 27)
+
+investors = 0;
+
+investorsCountStop = setInterval(() => {
+  this.investors++;
+
+  if (this.investors == 56) {
+    clearInterval(this.investorsCountStop)
+  }
+}, 100)
+  
+}
