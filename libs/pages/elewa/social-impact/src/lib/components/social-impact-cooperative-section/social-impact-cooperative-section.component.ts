21d95236994336586fac2d9523a87450a4d9dbf7
Newton Ombese <newtonombese1@gmail.com>
2023-02-17 13:14:13 +0300

    :art:feat: Adding the background color declaration

---
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-cooperative-section/social-impact-cooperative-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-social-impact-cooperative-section',
+  templateUrl: './social-impact-cooperative-section.component.html',
+  styleUrls: ['./social-impact-cooperative-section.component.scss'],
+})
+export class SocialImpactCooperativeSectionComponent {
+  backgroundColor="#ececec"
+}
