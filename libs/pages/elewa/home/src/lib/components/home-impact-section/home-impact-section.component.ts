a5227aa094e0544be19bd087c5c2302fc0c15b3f
Newton Ombese <newtonombese1@gmail.com>
2023-02-17 09:12:10 +0300

    :wrench:config: Updating the ts component file

---
+++ libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-home-impact-section',
+  templateUrl: './home-impact-section.component.html',
+  styleUrls: ['./home-impact-section.component.scss'],
+})
+export class HomeImpactSectionComponent {
+  message = "Learn more"
+  message1 = "Become an angel investor"
+  mode = 'light'
+}
