af20700e5d2fee45121cd1ac6562d365d09e68c6
jeffoty <odhiambo47@gmail.com>
2023-02-17 11:30:15 +0300

    ✨feat:Added home impact section feature.

---
+++ libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'elewa-group-home-impact-section',
+  templateUrl: './home-impact-section.component.html',
+  styleUrls: ['./home-impact-section.component.scss'],
+})
+export class HomeImpactSectionComponent {
+  titleText = 'Growing capital impacting lives';
+  paragraphText = 'Elewa is proof that growing capital and impacting lives can work well together  Our group owned and managed by our founder ,our employees (across our brands) and our network of 56 passionate angel investors.';
+  imageUrl = '';
+  message1 = 'Learn more';
+  message2 = 'Become an angel investor';
+  light = 'light'
+
+}
