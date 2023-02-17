e9f13facf905ecfab9c97ee518b8513cd8b508b3
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-17 12:17:33 +0300

    :sparkles: :art: Generated the social impact people section and added styling

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
+import { ElewaGroupImageAndTextBannerComponent } from 'libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule],
+  declarations: [SocialImpactPageComponent, SocialImpactPeopleSectionComponent, ElewaGroupImageAndTextBannerComponent],
 })
 export class SocialImpactModule {}
