a49466c4fb65d7d55db94e27f090c94f66be7ec6
awadh <awadh.hassan@student.moringaschool.com>
2023-02-17 10:54:01 +0300

    🚧 additon of card array

--- libs/pages/elewa/about-us/src/lib/components/we-care-section/we-care-section.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/we-care-section/we-care-section.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./we-care-section.component.css']
 })
 export class WeCareSectionComponent {
+cards = [
+  {
+    icon : "fa fa-infinity",
+    title:'holistic solution',
+    description:'we go beyond a simple patch up bad develop testing solution through holistic design'
+    
+  },
+  {
+    icon : "fa fa-user-group" ,
+    title:'impact',
+    description:'impact as a direct or indirect result all our respective organizations organizations here underlying theories of change'
 
+  },
+  {
+    icon:' fa  fa-globe',
+    title:'open data',
+    description:'sharing is caring we share we learn all our internal projects are open sources'
+
+  }
+]
 }
