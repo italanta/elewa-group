941f0d5717cfb32c251ea1fb389369287968a6a0
queenslay jema <queenslayjema@gmail.com>
2023-02-17 14:57:25 +0300

    #32 Feat: import Banners module

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
-
+import { BannersModule } from '@elewa-group/features/components/banners';
+import { SocialImpactCooperativeComponent } from './pages/components/social-impact-cooperative/social-impact-cooperative.component';
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule,SocialImpactRoutingModule,BannersModule],
+  declarations: [SocialImpactPageComponent, SocialImpactCooperativeComponent],
 })
 export class SocialImpactModule {}
