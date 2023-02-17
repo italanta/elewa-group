5b0feb487fcb144ae58d8eac6be93e2f436954e9
Rajab Karume <rkarume@gmail.com>
2023-02-17 14:19:22 +0300

    ✨feat created beyond-business components

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { BeyondBusinessComponent } from './pages/components/beyond-business/beyond-business.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule, BannersModule],
+  declarations: [SocialImpactPageComponent, BeyondBusinessComponent],
 })
 export class SocialImpactModule {}
---
+++ libs/pages/elewa/social-impact/src/lib/pages/components/beyond-business/beyond-business.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-beyond-business',
+  templateUrl: './beyond-business.component.html',
+  styleUrls: ['./beyond-business.component.scss'],
+})
+export class BeyondBusinessComponent {}
