f686785ba802593122cab1a3a759b03f2ff7dc71
munene brian <njerubrianmunene@gmail.com>
2023-02-17 18:12:20 +0300

    feat: imported the modules necessary

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactFooteSectionComponent } from './social-impact-foote-section/social-impact-foote-section.component';
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [
+    CommonModule, 
+    SocialImpactRoutingModule,
+    LayoutModule,
+    ButtonsModule
+  ],
+  declarations: [SocialImpactPageComponent, SocialImpactFooteSectionComponent],
+  exports: [SocialImpactFooteSectionComponent]
 })
 export class SocialImpactModule {}
