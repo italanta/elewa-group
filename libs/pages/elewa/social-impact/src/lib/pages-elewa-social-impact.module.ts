857b3500b91aa90a284e117b58186b98e015bc30
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-17 11:51:03 +0300

    feat: Update module exports

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { ElewaGroupImageAndTextBannerComponent } from 'libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  declarations: [SocialImpactPageComponent, ElewaGroupImageAndTextBannerComponent],
+  exports: [SocialImpactPageComponent]
 })
 export class SocialImpactModule {}
