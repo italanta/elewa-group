77b9fb8fd25bd8060b518e38d8434bf52135647f
McRonaah <ronaldcheruiyot342@gmail.com>
2023-02-17 15:47:21 +0300

    updated care section

--- libs/pages/elewa/about-us/src/lib/components/we-care-section/we-care-section.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/we-care-section/we-care-section.component.ts
@@ -9,20 +9,20 @@ export class WeCareSectionComponent {
 cards = [
   {
     icon : "fa fa-infinity",
-    title:'holistic solution',
-    description:'we go beyond a simple patch up bad develop testing solution through holistic design'
-    
+    title:'Holistic Solutions',
+    description:'We go beyond a simple patch up bad develop testing solution through holistic design'
+
   },
   {
     icon : "fa fa-user-group" ,
-    title:'impact',
-    description:'impact as a direct or indirect result all our respective organizations organizations here underlying theories of change'
+    title:'Impact',
+    description:'Impact as a direct or indirect result all our respective organizations organizations here underlying theories of change'
 
   },
   {
     icon:' fa  fa-globe',
-    title:'open data',
-    description:'sharing is caring we share we learn all our internal projects are open sources'
+    title:'Open data',
+    description:'Sharing is caring we share we learn all our internal projects are open sources'
 
   }
 ]
