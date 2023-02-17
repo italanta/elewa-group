d0996d5f77ec6e99367b76f0a182a603d2f3d2c5
Snowflake <onyangogeorge954@gmail.com>
2023-02-17 11:04:47 +0300

    correct ngModule declaration

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PeopleSectionComponent } from './component/people-section/people-section.component';
 
-@NgModule({
-  imports: [CommonModule],
-  declarations: [PeopleSectionComponent],
-})
-export class PagesElewaSocialImpactModule {}
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule],
+  declarations: [SocialImpactPageComponent, PeopleSectionComponent],
 })
+
 export class SocialImpactModule {}
