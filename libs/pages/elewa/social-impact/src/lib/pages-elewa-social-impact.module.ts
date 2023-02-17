b3bc809a4761487213b2c526eb19996363cdb7ed
jeffoty <odhiambo47@gmail.com>
2023-02-17 15:29:25 +0300

    rebase: deleted unwanted code

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -10,15 +10,4 @@ import { SocialImpactRoutingModule } from './social-impact.routing';
   declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
   exports: [SocialImpactPageComponent],
 })
-export class PagesElewaSocialImpactModule {}
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
-
-import { SocialImpactRoutingModule } from './social-impact.routing';
-
-@NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
-})
 export class SocialImpactModule {}
