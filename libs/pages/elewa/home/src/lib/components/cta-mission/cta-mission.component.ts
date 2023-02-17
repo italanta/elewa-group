e4e917a5c222ed8331fe69d56f510800c934143e
Kangonga <guchu413@gmail.com>
2023-02-17 21:51:43 +0300

    feat: Create the call to action component

---
+++ libs/pages/elewa/home/src/lib/components/cta-mission/cta-mission.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-cta-mission',
+  templateUrl: './cta-mission.component.html',
+  styleUrls: ['./cta-mission.component.scss'],
+})
+export class CtaMissionComponent {}
