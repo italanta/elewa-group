25f5b2c7a895747873ccef6ad859e9fb3830987f
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-02-17 11:25:15 +0300

    :truck: moved directives and style fixes

--- libs/pages/elewa/home/src/lib/components/team-and-partners/team-and-partners.component.ts
+++ libs/pages/elewa/home/src/lib/components/team-and-partners/team-and-partners.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./team-and-partners.component.scss'],
 })
 export class TeamAndPartnersComponent {
-  @Input() imageSrc = 'https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+  @Input() imageSrc = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690292/elewa-group-website/hero-Images/team_nmmhnc.jpg';
   @Input() imgWidth = '340';
   @Input() imgHeight = '200';
 }
