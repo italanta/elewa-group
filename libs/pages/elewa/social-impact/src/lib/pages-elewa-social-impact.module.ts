6e22b1cc939e73330b88f74faa9d98b4cd585aff
Newton Ombese <newtonombese1@gmail.com>
2023-02-17 12:10:33 +0300

    :art::wrench:feat: Adding my imports to the module ts

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactBeyondBusinessSectionComponent } from './components/social-impact-beyond-business-section/social-impact-beyond-business-section.component';
+
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule, BannersModule],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactBeyondBusinessSectionComponent,
+  ],
 })
 export class SocialImpactModule {}
