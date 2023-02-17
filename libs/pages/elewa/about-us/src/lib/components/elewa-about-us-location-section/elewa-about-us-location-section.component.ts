e08a2430ac8fc2aa8ed28630c46f73f690a269e6
adamswonder <adamswill.i.am71002@gmail.com>
2023-02-17 09:57:04 +0300

    🔧fix: marker-position variable initialization

--- libs/pages/elewa/about-us/src/lib/components/elewa-about-us-location-section/elewa-about-us-location-section.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/elewa-about-us-location-section/elewa-about-us-location-section.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-about-us-location-section',
   templateUrl: './elewa-about-us-location-section.component.html',
-  styleUrls: ['./elewa-about-us-location-section.component.scss']
+  styleUrls: ['./elewa-about-us-location-section.component.scss'],
 })
+export class ElewaAboutUsLocationSectionComponent implements OnInit {
+  markerPositions: google.maps.LatLng;
 
-export class ElewaAboutUsLocationSectionComponent {
+  latitude = -1.256132162858307;
+  longitude = 36.80501589843353;
+
   mapOptions: google.maps.MapOptions = {
-    center: { lat: -1.256132162858307, lng: 36.80501589843353 },
-    zoom : 14,
+    center: { lat: this.latitude, lng: this.longitude },
+    zoom: 14,
     disableDefaultUI: true,
     zoomControl: false,
-    mapTypeControl: false, 
+    mapTypeControl: false,
     streetViewControl: false,
-    fullscreenControl: false
-  }
+    fullscreenControl: false,
+  };
+}
 
+constructor() {}
+
+ngOnInit(): void {
+  this.markerPositions = new google.maps.LatLng(this.latitude, this.longitude)
 }
