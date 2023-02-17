e4aea07f296043e64148c38eca81e95c00b660a3
codertinie <martin.munyao@moringaschool.com>
2023-02-17 12:57:52 +0300

    ✨Feat: Added social impact cooperative section

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
+  imports: [CommonModule,SocialImpactRoutingModule, BannersModule],
   declarations: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
