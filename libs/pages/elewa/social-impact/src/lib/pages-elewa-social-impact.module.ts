57cfd7b34648dc518423c92c8510ebc4aad9c154
Newton Ombese <newtonombese1@gmail.com>
2023-02-17 13:10:14 +0300

    :heavy_plus_sign:feat: Adding the Banner import to my file

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
 
+import { BannersModule } from '@elewa-group/features/components/banners';
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule, BannersModule],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactCooperativeSectionComponent,
+  ],
 })
 export class SocialImpactModule {}
