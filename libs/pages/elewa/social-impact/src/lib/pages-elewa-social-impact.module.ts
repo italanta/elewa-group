9b97bf1906760258e44a107720fda46754e65896
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-17 09:00:21 +0300

    #32:feat:create a new component; social-impact-cooperative-section

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactOpenKnowledgeComponent } from './component/social-impact-open-knowledge/social-impact-open-knowledge.component';
 import { BannersModule } from '@elewa-group/features/components/banners';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
+import { SocialImpactCooperativeSectionComponent } from './component/social-impact-cooperative-section/social-impact-cooperative-section.component';
 
 @NgModule({
   imports: [CommonModule, BannersModule],
 
-  declarations: [SocialImpactOpenKnowledgeComponent, SocialImpactPageComponent],
-  exports:[SocialImpactPageComponent]
+  declarations: [
+    SocialImpactOpenKnowledgeComponent,
+    SocialImpactPageComponent,
+    SocialImpactCooperativeSectionComponent,
+  ],
+  exports: [SocialImpactPageComponent],
 })
 export class PagesElewaSocialImpactModule {}
