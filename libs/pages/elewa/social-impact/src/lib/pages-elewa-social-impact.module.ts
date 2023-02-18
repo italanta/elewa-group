49a9b23f1e10beb6f1f23a7c93a27963108de105
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-02-19 01:05:18 +0300

    :sparkles: feat: Added the Social Impact Cooperative Section

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
 
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule, SocialImpactRoutingModule],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactCooperativeSectionComponent,
+  ],
 })
 export class SocialImpactModule {}
