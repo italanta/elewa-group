3d08f0d081c2abba95d23367db5a054929316bb9
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-20 16:32:19 +0300

    feat: Import hero and footer component

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
-
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactFooterComponent } from './components/social-impact-footer/social-impact-footer.component';
 
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
+import { SocialImpactHeroComponent } from './components/social-impact-hero/social-impact-hero.component';
 @NgModule({
-  imports: [CommonModule,SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent],
+  imports: [CommonModule,SocialImpactRoutingModule, LayoutModule, ButtonsModule],
+  declarations: [SocialImpactPageComponent, SocialImpactFooterComponent,SocialImpactHeroComponent],
+  exports: [SocialImpactFooterComponent]
 })
 export class SocialImpactModule {}
