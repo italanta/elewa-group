02ba55fc8d12f1402b1507f3fbac2566e1d3ba17
mbuimbogo <mbuimbogo99@gmail.com>
2023-02-20 16:50:49 +0300

    fix: delete social impact hero component

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -6,10 +6,10 @@ import { SocialImpactFooterComponent } from './components/social-impact-footer/s
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { ButtonsModule } from '@elewa-group/features/components/buttons';
-import { SocialImpactHeroComponent } from './components/social-impact-hero/social-impact-hero.component';
+
 @NgModule({
   imports: [CommonModule,SocialImpactRoutingModule, LayoutModule, ButtonsModule],
-  declarations: [SocialImpactPageComponent, SocialImpactFooterComponent,SocialImpactHeroComponent],
+  declarations: [SocialImpactPageComponent, SocialImpactFooterComponent,],
   exports: [SocialImpactFooterComponent]
 })
 export class SocialImpactModule {}
