76598968247e34c7fe21dde8878720d18acb13f3
Vincent <makokhavomondi@gmail.com>
2023-02-20 08:51:36 +0300

    :sparkles: Generate component "SocialImpactCooperativeComponent"

---
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-cooperative/social-impact-cooperative.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-social-impact-cooperative',
+  templateUrl: './social-impact-cooperative.component.html',
+  styleUrls: ['./social-impact-cooperative.component.scss'],
+})
+export class SocialImpactCooperativeComponent {}
--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,10 +4,15 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactCooperativeComponent } from './components/social-impact-cooperative/social-impact-cooperative.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactHeroSectionComponent,
+    SocialImpactCooperativeComponent,
+  ],
   exports: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
