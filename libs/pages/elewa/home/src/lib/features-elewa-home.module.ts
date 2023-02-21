a4b6e4f29345e2944e158ee03c408765d178f0cc
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2023-02-21 22:28:48 +0300

    :hammer: fix order of imports

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LayoutModule } from '@elewa-group/elements/layout';
-
-import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
-import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import {PagesElewaSocialImpactModule} from '@elewa-group/pages/elewa/social-impact';
 import { HomeRoutingModule } from './home.routing';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
+import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
     PagesElewaSocialImpactModule,
-
     HomeRoutingModule
   ],
   declarations: [
