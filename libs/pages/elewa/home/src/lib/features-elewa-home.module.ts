65dc8c49515c1316dd857af4087f087e26d0dca5
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 11:33:06 +0300

    :wrench:fix: Working on the imports fix

--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { CardsModule } from '@elewa-group/features/components/cards';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { TeamAndPartnersComponent } from './components/team-and-partners/team-and-partners.component';
 import { ActivitySectionComponent } from './components/activity-section/activity-section.component';
+import { HomeImpactSectionComponent } from './components/home-impact-section/home-impact-section.component';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
@@ -15,9 +17,6 @@ import { PrevDirective } from './directives/prev.directive';
 import { NextDirective } from './directives/next.directive';
 
 import { HomeRoutingModule } from './home.routing';
-
-import { HomeImpactSectionComponent } from './components/home-impact-section/home-impact-section.component';
-import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 
 @NgModule({
@@ -35,10 +34,10 @@ import { ButtonsModule } from '@elewa-group/features/components/buttons';
     HomeJobsSectionComponent,
     TeamAndPartnersComponent,
     ActivitySectionComponent,
+    HomeImpactSectionComponent,
     NextDirective,
     PrevDirective,
-    HomeImpactSectionComponent,
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent, HomeJobsSectionComponent, TeamAndPartnersComponent, ActivitySectionComponent, NextDirective, PrevDirective],
+  exports: [HomePageComponent, HomeHeroSectionComponent, HomeJobsSectionComponent, TeamAndPartnersComponent, ActivitySectionComponent, HomeImpactSectionComponent, NextDirective, PrevDirective],
 })
 export class HomePageModule { }
