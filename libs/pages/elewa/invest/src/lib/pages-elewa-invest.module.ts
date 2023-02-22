fd3fd71b4545e0a6c6728d74b9836e5950a17f38
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 13:35:10 +0300

    :heavy_plus_sign:import: Adding the layout module import

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { ElewaInvestHeroComponent } from './components/elewa-invest-hero/elewa-invest-hero.component';
+
+import { LayoutModule } from '@elewa-group/elements/layout';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  imports: [CommonModule, LayoutModule, InvestRoutingModule],
+  declarations: [InvestPageComponent, ElewaInvestHeroComponent],
 })
 export class InvestPageModule {}
