5d59201794a1da895d7898dab682f4031bc0abb0
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-22 15:35:58 +0300

    ✨🎨 Reuse of components and styling

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
+import { BannersModule } from '@elewa-group/features/components/banners';
+
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestInvestingInElewaSectionComponent } from './components/elewa-invest-investing-in-elewa-section/elewa-invest-investing-in-elewa-section.component';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
+  imports: [
+    CommonModule, 
+    InvestRoutingModule, 
+    ButtonsModule,
+    BannersModule
+  ],
   declarations: [
     InvestPageComponent,
     ElewaInvestInvestingInElewaSectionComponent,
   ],
+  exports: [InvestPageComponent]
 })
 export class InvestPageModule {}
