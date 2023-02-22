73a0a951f420061c8ec126ca9741b287e33aadfa
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-22 15:55:16 +0300

    ✨feat: passed in data using input decorator and implemented the navigation function

--- libs/pages/elewa/home/src/lib/components/home-jobs-section/home-jobs-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-jobs-section/home-jobs-section.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'elewa-group-home-jobs-section',
   templateUrl: './home-jobs-section.component.html',
   styleUrls: ['./home-jobs-section.component.scss'],
 })
-export class HomeJobsSectionComponent {}
+export class HomeJobsSectionComponent {
+  @Input () message1 = 'Become part of our family.'
+  @Input () message2 = 'View'
+  @Input () link = '/jobs'
+  @Input () linkText = 'open opportunities.'
+
+  constructor(private router: Router) { }
+  navigateToLink() {
+    this.router.navigate(['/jobs']);
+  }
+}
