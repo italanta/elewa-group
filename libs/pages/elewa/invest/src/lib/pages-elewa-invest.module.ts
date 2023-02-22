0466225aa4b7b29d4ce56ee0eb0f3d7e506d66a9
martin mithi <gathitumithi9000@gmail.com>
2023-02-22 16:23:52 +0300

    Import component into pages-elewa-invest module

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { ElewaInvestHeroComponent } from './components/elewa-invest-hero/elewa-invest-hero.component';
 
 @NgModule({
   imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  declarations: [InvestPageComponent, ElewaInvestHeroComponent],
 })
 export class InvestPageModule {}
