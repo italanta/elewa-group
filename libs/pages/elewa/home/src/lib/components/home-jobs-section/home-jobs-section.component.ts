2ffe95b713bc5715628dd5c8d97adee60ced071e
martin mithi <gathitumithi9000@gmail.com>
2023-02-22 18:42:40 +0300

    feat:Added a navigation function and relevant inputs

--- libs/pages/elewa/home/src/lib/components/home-jobs-section/home-jobs-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-jobs-section/home-jobs-section.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'elewa-group-home-jobs-section',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-jobs-section.component.scss'],
 })
 export class HomeJobsSectionComponent {
-  message = 'Become part of our mission. And start ';
-  callToAction = 'investing';
-  url = '/jobs';
-  textAlign = 'center';
+  @Input() message = 'Become part of our mission. And start ';
+  @Input() callToAction = 'investing';
+  @Input() url = 'https://italanta.net/';
+  @Input() textAlign = 'center';
+  constructor(private router: Router) {}
+  // Function to navigate when call to action link is clicked
+  navigate() {
+    window.location.href = this.url;
+  }
 }
