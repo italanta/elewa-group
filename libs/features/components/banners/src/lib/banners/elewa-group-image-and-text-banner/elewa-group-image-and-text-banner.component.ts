c8c08869f9cac500edefddb3c9d21e27db7ab79a
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 15:07:37 +0300

    #33:art:empty the strings used in the modules ts file to make the component re-usable

--- libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -7,14 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class ElewaGroupImageAndTextBannerComponent {
 
-  @Input() imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png'
-  @Input() paragraphTexts = [`Elewa's businesses have one common objective, to
-                            unlock true potential of individuals, teams, and
-                            communities. All our talents are enrolled in a personal
-                            growth track. In turn, they contribute teir own growth
-                            towards teh growth of others, the group and their
-                            communities.`]
-  @Input() titleText = `A cooperative mindset`
-  @Input() imagePlacement = 'right'
-  @Input() backgroundColor = 'grey'
+  @Input() imageURL = ''
+  @Input() paragraphTexts: string[]
+  @Input() titleText = ''
+  @Input() imagePlacement = ''
+  @Input() backgroundColor = ''
 }
