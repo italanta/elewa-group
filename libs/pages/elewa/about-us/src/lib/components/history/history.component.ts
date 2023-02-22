1f2ff599aec01916c978a51df4419922b287f28a
Daniel <danielngoys@outlook.com>
2023-02-22 16:53:24 +0300

    ✨: logic

---
+++ libs/pages/elewa/about-us/src/lib/components/history/history.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-history',
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.scss'],
+})
+export class HistoryComponent {}
