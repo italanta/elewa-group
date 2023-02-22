c9eb9bcacd82e71b1a1521b4be6b9855ab7cf648
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 13:20:30 +0300

    #32:feat:create a new component called social-impact-cooperative-section

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,10 +4,17 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { CooperativeSectionComponent } from './components/cooperative-section/cooperative-section.component';
+import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactHeroSectionComponent,
+    CooperativeSectionComponent,
+    SocialImpactCooperativeSectionComponent,
+  ],
   exports: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
