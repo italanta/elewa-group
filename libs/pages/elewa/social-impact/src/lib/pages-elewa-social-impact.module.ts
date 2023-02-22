1df17408161b2189edb6574d9508ba425f3066fe
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 17:32:37 +0300

    #34:tada:create a new component

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,10 +4,15 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactHeroSectionComponent,
+    SocialImpactOpenKnowledgeComponent,
+  ],
   exports: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
