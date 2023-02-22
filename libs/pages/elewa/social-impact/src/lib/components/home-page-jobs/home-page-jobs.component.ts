6ba39d3f8abd290a1a1eaf594b2d773e9fd361c9
Vincent <makokhavomondi@gmail.com>
2023-02-22 15:14:07 +0300

    :construction: Make our component reusable

--- libs/pages/elewa/social-impact/src/lib/components/home-page-jobs/home-page-jobs.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/home-page-jobs/home-page-jobs.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-home-page-jobs',
   templateUrl: './home-page-jobs.component.html',
   styleUrls: ['./home-page-jobs.component.scss'],
 })
-export class HomePageJobsComponent {}
+export class HomePageJobsComponent {
+  @Input() message = "Become part of the family"
+  @Input() callToActionIntroducer = "View"
+  @Input() callToAction = "open opportunities"
+  @Input() callToActionURL = "jobs"
+  @Input() align = "left"
+}
