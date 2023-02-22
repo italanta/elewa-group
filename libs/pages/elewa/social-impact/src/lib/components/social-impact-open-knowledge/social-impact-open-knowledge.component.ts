6837a802d839571df57ef35c10f0a76b4de69777
kahoragachau <joelkahoragachau6@gmail.com>
2023-02-22 17:10:12 +0300

    🎨styles: Indent code with  2 spaces

--- libs/pages/elewa/social-impact/src/lib/components/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class SocialImpactOpenKnowledgeComponent {
   title = 'Open Knowledge'
   image = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/unnamed_1_m3dvll.png'
-  paragraph = ['What we learn, we share. Through community events open knowledge repositories, regular teaching moment (everyone a teacher) and academic partnerships.',' We buid for today, with a lens of tomorrow.  ']
+  paragraph = ['What we learn, we share. Through community events open knowledge repositories, regular teaching moment (everyone a teacher) and academic partnerships.','',' We buid for today, with a lens of tomorrow.  ']
   bgColor="black"
   textColor="white"
 }
--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
+  imports: [CommonModule, LayoutModule, SocialImpactRoutingModule, BannersModule],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactHeroSectionComponent,
+    SocialImpactOpenKnowledgeComponent,
+  ],
   exports: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
