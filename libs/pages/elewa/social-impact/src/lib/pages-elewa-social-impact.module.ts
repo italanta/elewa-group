cc0311a2e183f26a37c9151a2892dcad9fc9f5fd
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 12:27:55 +0300

    :wrench:fix: Another import order fix

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
-import { SocialImpactRoutingModule } from './social-impact.routing';
 import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
+import { SocialImpactRoutingModule } from './social-impact.routing';
 
 import { BannersModule } from '@elewa-group/features/components/banners';
 @NgModule({
