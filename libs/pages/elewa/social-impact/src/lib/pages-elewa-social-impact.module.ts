3fba5d5ae94b235793518c7f59ae1eb9965a89da
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 14:58:36 +0300

    #33:art:social impact beyond business imports were added

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,10 +4,15 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactBeyondBusinessComponent } from './components/social-impact-beyond-business/social-impact-beyond-business.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
+  declarations: [
+    SocialImpactPageComponent,
+    SocialImpactHeroSectionComponent,
+    SocialImpactBeyondBusinessComponent,
+  ],
   exports: [SocialImpactPageComponent],
 })
 export class SocialImpactModule {}
