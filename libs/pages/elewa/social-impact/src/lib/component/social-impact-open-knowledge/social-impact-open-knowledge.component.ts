36aff6bb1a131cb7a4125d7a8f54be060ea75fb4
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 12:05:32 +0300

    #34:art:restructure the code in ts and html pages to allow the paragraphs take in an array of multiple strings

--- libs/pages/elewa/social-impact/src/lib/component/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
+++ libs/pages/elewa/social-impact/src/lib/component/social-impact-open-knowledge/social-impact-open-knowledge.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-social-impact-open-knowledge',
   templateUrl: './social-impact-open-knowledge.component.html',
   styleUrls: ['./social-impact-open-knowledge.component.scss'],
 })
-export class SocialImpactOpenKnowledgeComponent {}
+export class SocialImpactOpenKnowledgeComponent {
+@Input() paragraphs=["What we learn, we share. Through community events, open knowledge repositories, regular teaching moments(everyone a teacher) and academic partnerships.", "We build for today, with a lens on tomorrow."]
+}
