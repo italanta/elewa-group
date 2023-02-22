11f930cdbd02f671aac19cb99494f8abd8e89172
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 14:02:50 +0300

    :heavy_plus_sign:import: Importing Banners module

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { ElewaInvestStakeholderSectionComponent } from './components/elewa-invest-stakeholder-section/elewa-invest-stakeholder-section.component';
+
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  imports: [CommonModule, BannersModule, InvestRoutingModule],
+  declarations: [InvestPageComponent, ElewaInvestStakeholderSectionComponent],
 })
 export class InvestPageModule {}
