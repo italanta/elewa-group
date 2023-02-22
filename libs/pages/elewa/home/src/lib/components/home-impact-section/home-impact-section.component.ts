cb543db972b7ff9e7cd76496624fd1cac2775606
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-02-22 11:25:15 +0300

    Revert "Merge pull request #102 from RajabKarume/12-create-the-home-impact-section"
    This reverts commit d0060c11ae11e10d138c4907575583a2e1cadf24, reversing
    changes made to 3603e8bb525499b1c9d0424b5dc3ca139618bf79.

--- libs/pages/elewa/home/src/lib/components/home-impact-section/home-impact-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-home-impact-section',
-  templateUrl: './home-impact-section.component.html',
-  styleUrls: ['./home-impact-section.component.scss'],
-})
-export class HomeImpactSectionComponent {}
