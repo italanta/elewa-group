a8a7e66b774fae0f5becf026106ec60a239e1667
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-22 16:22:37 +0300

    feat: Updated the file structure, add files to components

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
+
+import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
 
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
-
-import { SocialImpactPeopleSectionComponent } from './social-impact-people-section/social-impact-people-section.component';
-
-import { BannersModule } from '@elewa-group/features/components/banners';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 
 @NgModule({
-  imports: [CommonModule, BannersModule, SocialImpactRoutingModule],
+  imports: [CommonModule, LayoutModule, BannersModule, SocialImpactRoutingModule],
   declarations: [
     SocialImpactPageComponent,
     SocialImpactPeopleSectionComponent,
