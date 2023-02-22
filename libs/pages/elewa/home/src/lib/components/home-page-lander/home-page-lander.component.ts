367a597c0dfd58404391cf4303a92299f08c783c
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-02-22 11:59:51 +0300

    :truck: move to correct folder

--- libs/pages/elewa/home/src/lib/components/home-page-lander/home-page-lander.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-page-lander/home-page-lander.component.ts
@@ -8,8 +8,6 @@ import { Component,Input } from '@angular/core';
 export class HomePageLanderComponent {
   @Input() subtitle = 'THE WAY IN WHICH WE TRAIN IS OUTDATED';
   @Input() title = 'We are a group of companies which unlock true human potential.';
-  @Input() backgroundImg = 'https://img.freepik.com/free-vector/meerkat-family-silhouette-savanna-forest_1308-87928.jpg?w=740&t=st=1676618911~exp=1676619511~hmac=51cee9365f45dbae94166c5642f40d9117a3a96af9949ee93c70d34aec31d541';
+  @Input() backgroundImg = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png';
   @Input() description = 'Irrespective of origin,nationality, gender, and belief.';
-
-
 }
