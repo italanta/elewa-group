fa8205b0f88ce7c5e18616f45e068a3f2718fc4f
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-22 16:06:33 +0300

    ✨feat: imported the reusable component to the social impact module

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,10 +4,11 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
+import { SocialImpactCallToActionSectionComponent } from './components/social-impact-call-to-action-section/social-impact-call-to-action-section.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent],
-  exports: [SocialImpactPageComponent],
+  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent,SocialImpactCallToActionSectionComponent],
+  exports: [SocialImpactPageComponent,SocialImpactCallToActionSectionComponent],
 })
 export class SocialImpactModule {}
