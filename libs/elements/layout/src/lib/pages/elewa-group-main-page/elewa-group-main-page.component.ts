f860bded6bb799b35a2816cd7ca13aa1060d98b8
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-22 21:40:08 +0300

    :sparkles: introducing dynamic rendering of the footer

--- libs/elements/layout/src/lib/pages/elewa-group-main-page/elewa-group-main-page.component.ts
+++ libs/elements/layout/src/lib/pages/elewa-group-main-page/elewa-group-main-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+
 
 @Component({
-  selector: 'elewa-group-elewa-group-main-page',
-  templateUrl: './elewa-group-main-page.component.html',
-  styleUrls: ['./elewa-group-main-page.component.scss'],
+ selector: 'elewa-group-elewa-group-main-page',
+ templateUrl: './elewa-group-main-page.component.html',
+ styleUrls: ['./elewa-group-main-page.component.scss'],
 })
-export class ElewaGroupMainPageComponent {}
+export class ElewaGroupMainPageComponent implements OnInit{
+ currentPage: string;
+
+
+ constructor(private route: ActivatedRoute) {}
+
+
+ ngOnInit() {
+   this.route.url.subscribe(urlSegments => {
+           // Update currentPage based on current route
+           this.currentPage = urlSegments[0].path;
+   }) 
+ }
+}
+
