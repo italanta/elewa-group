29876eb9ef1059fa5f7aec48aa9eabcafe6e8340
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 12:16:43 +0300

    #32:art:re-structure the code to allow several paragraphs by using an array that takes in strings

--- libs/pages/elewa/social-impact/src/lib/component/social-impact-cooperative-section/social-impact-cooperative-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/component/social-impact-cooperative-section/social-impact-cooperative-section.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-social-impact-cooperative-section',
   templateUrl: './social-impact-cooperative-section.component.html',
   styleUrls: ['./social-impact-cooperative-section.component.scss'],
 })
-export class SocialImpactCooperativeSectionComponent {}
+export class SocialImpactCooperativeSectionComponent {
+@Input() paragraphs = ["Elewa's Businessess have one common objective, to unlock true potential of individuals, teams, and communities. All our talents are enrolled in a personal growth track. In turn, they contribute their own growth towards the growth of others, the group and their communities."];
+}
