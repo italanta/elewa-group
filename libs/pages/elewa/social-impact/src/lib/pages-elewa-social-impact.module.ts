4c17ccf5a9e3a03693d8f937f7aa783aa0592f66
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-22 14:37:20 +0300

    ⚡️Using module in place of importing

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
 import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ElewaGroupImageAndTextBannerComponent } from 'libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, SocialImpactRoutingModule],
-  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent, SocialImpactPeopleSectionComponent, ElewaGroupImageAndTextBannerComponent],
+  imports: [CommonModule, LayoutModule, SocialImpactRoutingModule, BannersModule],
+  declarations: [SocialImpactPageComponent, SocialImpactHeroSectionComponent, SocialImpactPeopleSectionComponent],
   exports: [SocialImpactPageComponent]
 })
 export class SocialImpactModule {}
