cc9b7fd90149b467416966bf953ebd38a26f8387
marcokumu <marcokumu10@gmail.com>
2023-02-22 19:43:50 +0300

    ✨feat: imported the BannerModule and the ElewaInvestCreativeHubSectionComponent to enable component reusability

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
-
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { InvestRoutingModule } from './invest.routing';
-
+import { ElewaInvestCreativeHubSectionComponent } from './pages/elewa-invest-creative-hub-section/elewa-invest-creative-hub-section.component';
+import { LayoutModule } from '@elewa-group/elements/layout';
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  imports: [CommonModule, InvestRoutingModule, BannersModule, LayoutModule],
+  declarations: [InvestPageComponent, ElewaInvestCreativeHubSectionComponent],
+  exports: [ElewaInvestCreativeHubSectionComponent],
 })
 export class InvestPageModule {}
