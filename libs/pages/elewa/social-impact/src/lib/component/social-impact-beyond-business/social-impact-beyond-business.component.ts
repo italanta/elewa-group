a72a0ea40257f144746e87c79d6a7a15750ec3d3
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-22 11:51:12 +0300

    #33:art:create an input for the paragraph that allows multiple array of strings

--- libs/pages/elewa/social-impact/src/lib/component/social-impact-beyond-business/social-impact-beyond-business.component.ts
+++ libs/pages/elewa/social-impact/src/lib/component/social-impact-beyond-business/social-impact-beyond-business.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-social-impact-beyond-business',
   templateUrl: './social-impact-beyond-business.component.html',
   styleUrls: ['./social-impact-beyond-business.component.scss'],
 })
-export class SocialImpactBeyondBusinessComponent {}
+export class SocialImpactBeyondBusinessComponent {
+  @Input() paragraphs = ['Some of that youthful energy is channeled beyond business. Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu and our open-source coding initiatives providing entry-level opportunities to hundreds of junior engineers.', 'These projects don\'t necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term.']
+}
