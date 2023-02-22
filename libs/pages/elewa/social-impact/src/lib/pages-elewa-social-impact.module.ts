d93e139d88f3f95a92864790f66ce76b3aca1447
Vincent Makokha <81687679+VinceXIV@users.noreply.github.com>
2023-02-22 13:58:00 +0300

    Update pages-elewa-social-impact.module.ts

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -4,8 +4,8 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactRoutingModule } from './social-impact.routing';
-import { SocialImpactCooperativeComponent } from './components/social-impact-cooperative/social-impact-cooperative.component';
 import { BannersModule } from '@elewa-group/features/components/banners'
+import { SocialImpactCooperativeComponent } from './components/social-impact-cooperative/social-impact-cooperative.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, SocialImpactRoutingModule, BannersModule],
