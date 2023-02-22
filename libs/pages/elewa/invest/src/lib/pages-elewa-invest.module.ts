ab1e54cae1129bdeff661412db3f48b22b1b9096
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 17:24:41 +0300

    :wrench:fix: Working on fixing the import

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestStakeholderSectionComponent } from './components/elewa-invest-stakeholder-section/elewa-invest-stakeholder-section.component';
 
-import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
   imports: [CommonModule, BannersModule, InvestRoutingModule],
