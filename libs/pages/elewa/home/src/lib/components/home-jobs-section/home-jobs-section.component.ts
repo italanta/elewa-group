ed4778c49d8a7bc49f75c6f3cba65329fcc7e9cb
Vincent <makokhavomondi@gmail.com>
2023-02-22 12:40:43 +0300

    :fire: Remove all instances of HomeJobsSectionComponent use

--- libs/pages/elewa/home/src/lib/components/home-jobs-section/home-jobs-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-home-jobs-section',
-  templateUrl: './home-jobs-section.component.html',
-  styleUrls: ['./home-jobs-section.component.scss'],
-})
-export class HomeJobsSectionComponent {}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -5,7 +5,6 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { CardsModule } from '@elewa-group/features/components/cards';
 
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-import { HomeJobsSectionComponent } from './components/home-jobs-section/home-jobs-section.component';
 import { TeamAndPartnersComponent } from './components/team-and-partners/team-and-partners.component';
 import { ActivitySectionComponent } from './components/activity-section/activity-section.component';
 
@@ -28,12 +27,11 @@ import { HomeRoutingModule } from './home.routing';
   declarations: [
     HomePageComponent,
     HomeHeroSectionComponent,
-    HomeJobsSectionComponent,
     TeamAndPartnersComponent,
     ActivitySectionComponent,
     NextDirective,
     PrevDirective
   ],
-  exports: [HomePageComponent, HomeHeroSectionComponent, HomeJobsSectionComponent, TeamAndPartnersComponent, ActivitySectionComponent, NextDirective, PrevDirective],
+  exports: [HomePageComponent, HomeHeroSectionComponent, TeamAndPartnersComponent, ActivitySectionComponent, NextDirective, PrevDirective],
 })
 export class HomePageModule { }
