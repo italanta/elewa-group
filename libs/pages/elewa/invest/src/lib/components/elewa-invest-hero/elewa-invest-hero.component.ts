d6296a8384038f3ff7eb5f0709f86780d5f5cf2b
martin mithi <gathitumithi9000@gmail.com>
2023-02-22 16:22:52 +0300

    Initial commit

---
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-hero/elewa-invest-hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-invest-hero',
+  templateUrl: './elewa-invest-hero.component.html',
+  styleUrls: ['./elewa-invest-hero.component.scss'],
+})
+export class ElewaInvestHeroComponent {}
