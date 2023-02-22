08596b6004fb7463422069f54a86ef7428707ace
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-22 15:31:31 +0300

    :sparkles: Added invest-investing-in-elewa component

---
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-investing-in-elewa-section/elewa-invest-investing-in-elewa-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-invest-investing-in-elewa-section',
+  templateUrl: './elewa-invest-investing-in-elewa-section.component.html',
+  styleUrls: ['./elewa-invest-investing-in-elewa-section.component.scss'],
+})
+export class ElewaInvestInvestingInElewaSectionComponent {}
--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { ElewaInvestInvestingInElewaSectionComponent } from './components/elewa-invest-investing-in-elewa-section/elewa-invest-investing-in-elewa-section.component';
 
 @NgModule({
   imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestInvestingInElewaSectionComponent,
+  ],
 })
 export class InvestPageModule {}
