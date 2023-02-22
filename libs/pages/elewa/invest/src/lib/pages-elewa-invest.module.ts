0a010f97a365a1c3fc1e12213389ce6d023e4eea
martin mithi <gathitumithi9000@gmail.com>
2023-02-22 19:20:30 +0300

    feat: Add the Banners module import

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { ElewaInvestCreativeHubSectionComponent } from './components/elewa-invest-creative-hub-section/elewa-invest-creative-hub-section.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent],
+  imports: [CommonModule, InvestRoutingModule, BannersModule],
+  declarations: [InvestPageComponent, ElewaInvestCreativeHubSectionComponent],
 })
 export class InvestPageModule {}
