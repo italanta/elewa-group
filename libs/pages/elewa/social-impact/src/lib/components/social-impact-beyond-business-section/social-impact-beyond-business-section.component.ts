d1de33a7115bf54d4fcf0b43a7925dfb7fe0ae2e
Newton Ombese <newtonombese1@gmail.com>
2023-02-22 10:01:18 +0300

    :wrench:fix: Updated the paragraphtext as it was not declared as an array

--- libs/pages/elewa/social-impact/src/lib/components/social-impact-beyond-business-section/social-impact-beyond-business-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-beyond-business-section/social-impact-beyond-business-section.component.ts
@@ -4,13 +4,16 @@ import { Component } from '@angular/core';
   selector: 'elewa-group-social-impact-beyond-business-section',
   templateUrl: './social-impact-beyond-business-section.component.html',
   styleUrls: ['./social-impact-beyond-business-section.component.scss'],
+  template: `
+    <div *ngFor="let paragraphtext of paragraphtexts">
+      {{ paragraphtext }}
+    </div>
+  `,
 })
 export class SocialImpactBeyondBusinessSectionComponent {
   imageUrl = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/IMG_0764_twyo3k.png'
   title = 'Beyond Business'
-  paragraph = `Some of that youthful energy is channeled beyond business. Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu and our open-source coding initiatives providing entry-level oppurtunities to hundreds of junior engineers.
-  `
-  paragraph2 = 'These projects don\'t necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term'
+  paragraphTexts: string[] = ['Some of that youthful energy is channeled beyond business. Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu and our open-source coding initiatives providing entry-level oppurtunities to hundreds of junior engineers', 'These projects don\'t necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term'];
   backgroundColor="var(--elewa-group-website-color)"
   imagePlacement = 'left'
 }
