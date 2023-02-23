c8c32ab1ec0d0c264cbea14643266444959e92c4
George <sbeveme@gmail.com>
2023-02-23 09:19:36 +0300

    social-impact-beyond business component created

---
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-beyond-business-section/social-impact-beyond-business-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-social-impact-beyond-business-section',
+  templateUrl: './social-impact-beyond-business-section.component.html',
+  styleUrls: ['./social-impact-beyond-business-section.component.scss'],
+})
+export class SocialImpactBeyondBusinessSectionComponent {}
