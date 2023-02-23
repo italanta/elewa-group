8799377b5db43743a46e2e2544e004ad194b3052
Snow <onyangogeorge954@gmail.com>
2023-02-23 12:18:23 +0300

    clear repetitive code

--- libs/pages/elewa/invest/src/lib/components/investing-in-elewa/investing-in-elewa.component.ts
+++ libs/pages/elewa/invest/src/lib/components/investing-in-elewa/investing-in-elewa.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-investing-in-elewa',
@@ -6,10 +6,4 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./investing-in-elewa.component.scss'],
 })
 export class InvestingInElewaComponent {
-  @Input() imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/f2b2ea6afc31e9720638da1bbd0d2d69_p58coq.png';
-  @Input() paragraphTexts : string[] = ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique mollitia, hic voluptate aperiam facilis quisquam quidem. Unde accusantium explicabo dolor, rem voluptates aliquid nisi facilis ea provident, nobis, vel veniam?"];
-  @Input() titleText = 'Investing in Elewa NV';
-  @Input() imagePlacement = 'left';
-  @Input() backgroundColor = 'white';
-Join: string|undefined;
 }
