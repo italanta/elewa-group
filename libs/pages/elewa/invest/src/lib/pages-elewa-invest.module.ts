4775df6ec81916b943ce644e1c32c3f5e0d1119f
Newton Ombese <newtonombese1@gmail.com>
2023-02-23 14:26:19 +0300

    :airplane:import: Importing the BannerModule

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
+import { ElewaInvestInvestingInElewaSectionComponent } from './components/elewa-invest-investing-in-elewa-section/elewa-invest-investing-in-elewa-section.component';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, BannersModule, InvestRoutingModule],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestDetailSectionComponent,
+    ElewaInvestInvestingInElewaSectionComponent,
+  ],
 })
 export class InvestPageModule {}
