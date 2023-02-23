9ba54f36d0d77930306bd08c375efcdc7fffebe3
nziokikevin <kevinnzioki10@gmail.com>
2023-02-23 10:21:24 +0300

    :art: :sparkles: feat: adding required data to the reusable component

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
-import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
 import { ElewaInvestStakeholderSectionComponent } from './components/elewa-invest-stakeholder-section/elewa-invest-stakeholder-section.component';
 
+import { InvestRoutingModule } from './invest.routing';
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
+  imports: [CommonModule, BannersModule, InvestRoutingModule],
   declarations: [
     InvestPageComponent,
     ElewaInvestDetailSectionComponent,
