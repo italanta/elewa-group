ac7bda0de13198f372138998bce9d6fce4e45523
Snow <onyangogeorge954@gmail.com>
2023-02-23 19:16:36 +0300

    create creative hub section

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -9,9 +9,16 @@ import { ElewaInvestHeroComponent } from './components/elewa-invest-hero/elewa-i
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
+import { CreativeHubSectionComponent } from './components/creative-hub-section/creative-hub-section.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestHeroComponent, ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, LayoutModule, InvestRoutingModule, BannersModule],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestHeroComponent,
+    ElewaInvestDetailSectionComponent,
+    CreativeHubSectionComponent,
+  ],
 })
 export class InvestPageModule {}
