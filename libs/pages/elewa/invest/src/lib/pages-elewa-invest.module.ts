684486a88fec2affd5d6d6b100f52fbebf669f74
Snow <onyangogeorge954@gmail.com>
2023-02-23 12:14:51 +0300

    adjust for a reusable component

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 
-import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
 import { ElewaInvestHeroComponent } from './components/elewa-invest-hero/elewa-invest-hero.component';
 
 import { InvestPageComponent } from './pages/invest-page/invest-page.component';
@@ -12,10 +11,11 @@ import { InvestRoutingModule } from './invest.routing';
 import { InvestingInElewaComponent } from './components/investing-in-elewa/investing-in-elewa.component';
 import { ButtonsModule } from '@elewa-group/features/components/buttons';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule, ButtonsModule],
-  declarations: [InvestPageComponent, InvestingInElewaComponent,ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, InvestRoutingModule, ButtonsModule,BannersModule,LayoutModule],
+  declarations: [InvestPageComponent, InvestingInElewaComponent,ElewaInvestDetailSectionComponent,ElewaInvestHeroComponent],
 
 })
 export class InvestPageModule {}
