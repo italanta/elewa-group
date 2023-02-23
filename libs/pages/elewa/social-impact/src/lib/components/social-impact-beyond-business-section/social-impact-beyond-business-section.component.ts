2c6f66d29e8a1a46648b4f6a7bb21fbf7b45fedb
George <sbeveme@gmail.com>
2023-02-23 09:54:52 +0300

    social-impact-beyond business component data added

--- libs/pages/elewa/social-impact/src/lib/components/social-impact-beyond-business-section/social-impact-beyond-business-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-beyond-business-section/social-impact-beyond-business-section.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./social-impact-beyond-business-section.component.scss'],
 })
 export class SocialImpactBeyondBusinessSectionComponent {
-  imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/gettyimages-525701055-2048x2048_g7nbt1.png'
+  imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Images/IMG_0764_twyo3k.png'
   paragraphTexts = [`Some of that youthful energy is channeled beyond
   business. Through projects such as our coding school
   in Kakuma refugee camo, training teachers in pastoral Samburu and our open-source coding initiatives
