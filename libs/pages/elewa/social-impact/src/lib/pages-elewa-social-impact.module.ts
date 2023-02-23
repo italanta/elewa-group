7ef8784c262991b009b6a002179d3fb858145abe
George <sbeveme@gmail.com>
2023-02-23 10:31:40 +0300

    changes merged

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -8,20 +8,26 @@ import { SocialImpactPeopleSectionComponent } from './components/social-impact-p
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
 
-
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
+import { SocialImpactBeyondBusinessSectionComponent } from './components/social-impact-beyond-business-section/social-impact-beyond-business-section.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, BannersModule, SocialImpactRoutingModule],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    BannersModule,
+    SocialImpactRoutingModule,
+  ],
   declarations: [
     SocialImpactPageComponent,
     SocialImpactHeroSectionComponent,
     SocialImpactPeopleSectionComponent,
     SocialImpactOpenKnowledgeComponent,
     SocialImpactCooperativeSectionComponent,
+    SocialImpactBeyondBusinessSectionComponent,
   ],
   exports: [SocialImpactPageComponent],
 })
