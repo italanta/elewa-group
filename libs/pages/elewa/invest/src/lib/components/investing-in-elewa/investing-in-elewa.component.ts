102408486b280306db7fd72163d51626c57ab843
Snow <onyangogeorge954@gmail.com>
2023-02-23 11:22:30 +0300

    reuse text and image banner to complete task

--- libs/pages/elewa/invest/src/lib/components/investing-in-elewa/investing-in-elewa.component.ts
+++ libs/pages/elewa/invest/src/lib/components/investing-in-elewa/investing-in-elewa.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-investing-in-elewa',
   templateUrl: './investing-in-elewa.component.html',
   styleUrls: ['./investing-in-elewa.component.scss'],
 })
-export class InvestingInElewaComponent {}
+export class InvestingInElewaComponent {
+  @Input() imageURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/f2b2ea6afc31e9720638da1bbd0d2d69_p58coq.png';
+  @Input() paragraphTexts : string[] = ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique mollitia, hic voluptate aperiam facilis quisquam quidem. Unde accusantium explicabo dolor, rem voluptates aliquid nisi facilis ea provident, nobis, vel veniam?"];
+  @Input() titleText = 'Investing in Elewa NV';
+  @Input() imagePlacement = 'left';
+  @Input() backgroundColor = 'white';
+Join: string|undefined;
+}
--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -4,9 +4,10 @@ import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { InvestingInElewaComponent } from './components/investing-in-elewa/investing-in-elewa.component';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
+  imports: [CommonModule, InvestRoutingModule, ButtonsModule],
   declarations: [InvestPageComponent, InvestingInElewaComponent],
 })
 export class InvestPageModule {}
