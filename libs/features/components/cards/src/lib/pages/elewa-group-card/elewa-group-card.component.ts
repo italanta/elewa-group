f374a4d668c987e4c60788f5923528a6a31c2353
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-23 11:26:36 +0300

    delete the unnecessary file

--- libs/features/components/cards/src/lib/pages/elewa-group-card/elewa-group-card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-group-elewa-group-card',
-  templateUrl: './elewa-group-card.component.html',
-  styleUrls: ['./elewa-group-card.component.scss'],
-})
-export class ElewaGroupCardComponent {}
