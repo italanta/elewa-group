30ccea61fc63dbb02547a5c988b9387667c8ee91
Snow <onyangogeorge954@gmail.com>
2023-02-23 18:12:02 +0300

    add reusable component

--- libs/pages/elewa/invest/src/lib/components/creative-hub-section/creative-hub-section.component.ts
+++ libs/pages/elewa/invest/src/lib/components/creative-hub-section/creative-hub-section.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './creative-hub-section.component.html',
   styleUrls: ['./creative-hub-section.component.scss'],
 })
-export class CreativeHubSectionComponent {}
+export class CreativeHubSectionComponent {
+  imgURL = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/Mask_Group_20_yshsq2.png'
+  position = 'right'
+  bgColor = 'white'
+  titleText = 'Elewa Creative Hub'
+  graphText : string[] = ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut quisquam consequuntur nisi, pariatur dignissimos itaque aliquid ullam reprehenderit vel quas est quae atque assumenda dolor, ab expedita sequi mollitia sed?']
+}
