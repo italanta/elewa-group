9af9ac8df2e9b3db387ad83207df0ebecb71a631
danstan <oduordanstan@gmail.com>
2023-02-23 11:12:56 +0300

    :art: imported the banner module

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -4,9 +4,15 @@ import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
+import { ElewaInvestCreativeHubSectionComponent } from './components/elewa-invest-creative-hub-section/elewa-invest-creative-hub-section.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, InvestRoutingModule, BannersModule],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestDetailSectionComponent,
+    ElewaInvestCreativeHubSectionComponent,
+  ]
 })
 export class InvestPageModule {}
