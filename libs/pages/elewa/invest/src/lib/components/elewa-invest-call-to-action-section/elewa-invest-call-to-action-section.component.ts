12ab072687770e6e2f0546a4820ff90d1cd5bd4b
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2023-02-23 11:26:24 +0300

    Generated the component
    Generated the component for the invest page call to action

---
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-call-to-action-section/elewa-invest-call-to-action-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-invest-call-to-action-section',
+  templateUrl: './elewa-invest-call-to-action-section.component.html',
+  styleUrls: ['./elewa-invest-call-to-action-section.component.scss'],
+})
+export class ElewaInvestCallToActionSectionComponent {}
