fc095bc904250edf3d0fa6affb45d94a58507248
nziokikevin <kevinnzioki10@gmail.com>
2023-02-23 10:14:05 +0300

    :sparkles: feat: generating invest stakeholder component

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -4,9 +4,14 @@ import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
+import { ElewaInvestStakeholderSectionComponent } from './components/elewa-invest-stakeholder-section/elewa-invest-stakeholder-section.component';
 
 @NgModule({
   imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestDetailSectionComponent,
+    ElewaInvestStakeholderSectionComponent,
+  ],
 })
 export class InvestPageModule {}
