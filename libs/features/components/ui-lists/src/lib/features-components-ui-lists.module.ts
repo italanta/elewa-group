d4147669f51cfe783b6e559714a5f9b1f089ac65
Dennis Njogu <Dcolonel6@users.noreply.github.com>
2023-02-23 10:13:42 +0300

    :truck: moved social impact projects

--- libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
+++ libs/features/components/ui-lists/src/lib/features-components-ui-lists.module.ts
@@ -4,21 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ElewaGroupVerticalListOneComponent } from './elewa-group-vertical-list-one/elewa-group-vertical-list-one.component';
 import { ElewaHorizontalTimelineCarouselComponent } from './elewa-horizontal-timeline-carousel/elewa-horizontal-timeline-carousel.component';
 import { ElewaGroupHorizontalListOrgsComponent } from './elewa-group-horizontal-list-orgs/elewa-group-horizontal-list-orgs.component';
-import { ElewaGroupSocialImpactProjectsSectionComponent } from './elewa-group-social-impact-projects-section/elewa-group-social-impact-projects-section.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     ElewaGroupVerticalListOneComponent,
     ElewaGroupHorizontalListOrgsComponent,
-    ElewaHorizontalTimelineCarouselComponent,    
-    ElewaGroupSocialImpactProjectsSectionComponent,
+    ElewaHorizontalTimelineCarouselComponent 
   ],
   exports: [
     ElewaGroupVerticalListOneComponent,
     ElewaGroupHorizontalListOrgsComponent,
-    ElewaHorizontalTimelineCarouselComponent,
-    ElewaGroupSocialImpactProjectsSectionComponent
+    ElewaHorizontalTimelineCarouselComponent
   ],
 })
 export class UiListsModule {}
--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,27 +1,33 @@
 import { NgModule } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
+
 import { LayoutModule } from '@elewa-group/elements/layout';
+
 import { BannersModule } from '@elewa-group/features/components/banners';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 
 import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
+import { ElewaGroupSocialImpactProjectsSectionComponent } from './components/elewa-group-social-impact-projects-section/elewa-group-social-impact-projects-section.component';
+import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
 
 
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
-import { SocialImpactOpenKnowledgeComponent } from './components/social-impact-open-knowledge/social-impact-open-knowledge.component';
+
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, BannersModule, SocialImpactRoutingModule],
+  imports: [CommonModule, LayoutModule, UiListsModule, BannersModule, SocialImpactRoutingModule],
   declarations: [
     SocialImpactPageComponent,
     SocialImpactHeroSectionComponent,
     SocialImpactPeopleSectionComponent,
     SocialImpactOpenKnowledgeComponent,
     SocialImpactCooperativeSectionComponent,
+    ElewaGroupSocialImpactProjectsSectionComponent
+    
   ],
   exports: [SocialImpactPageComponent],
 })
