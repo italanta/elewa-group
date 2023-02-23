a2bf1acdc06a8e4849bed6ab9ca853cebd5885bc
danstan <oduordanstan@gmail.com>
2023-02-23 10:15:46 +0300

    :sparkles: generated files for creative hub section

---
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-creative-hub-section/elewa-invest-creative-hub-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-invest-creative-hub-section',
+  templateUrl: './elewa-invest-creative-hub-section.component.html',
+  styleUrls: ['./elewa-invest-creative-hub-section.component.scss'],
+})
+export class ElewaInvestCreativeHubSectionComponent {}
