ad3f4f11882dda553bc9b25b4f41021d5381ea20
marcokumu <marcokumu10@gmail.com>
2023-02-23 11:32:20 +0300

    ✨feat: imported the required module and component

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -4,9 +4,10 @@ import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
-
+import { BannersModule } from '@elewa-group/features/components/banners';
+import {ElewaInvestStakeholderSectionComponent} from './components/elewa-invest-stakeholder-section/elewa-invest-stakeholder-section.component';
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, InvestRoutingModule, BannersModule],
+  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent, ElewaInvestStakeholderSectionComponent],
 })
 export class InvestPageModule {}
