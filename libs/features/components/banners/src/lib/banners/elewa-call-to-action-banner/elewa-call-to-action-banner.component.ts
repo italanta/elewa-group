e5015f0b46cd0a5b4266eddf6c4a2708119c8fd6
Kangonga <guchu413@gmail.com>
2023-02-25 00:30:51 +0300

    Revert "feat: create input properties to accept data passed down to the reusable component"
    This reverts commit aad883baf77b03132425c88cd18cd40888d3047e.

--- libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
@@ -1,15 +1,8 @@
-import { Component,Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-call-to-action-banner',
   templateUrl: './elewa-call-to-action-banner.component.html',
   styleUrls: ['./elewa-call-to-action-banner.component.scss'],
 })
-export class ElewaCallToActionBannerComponent {
-  acceptable_text_align = ["center","left","right","justified"]
-  @Input() message:string
-  @Input() callToAction:string
-  @Input() textAlign:string
-  @Input() destinationUrl:string
-  @Input() callToActionIntro:string
-}
+export class ElewaCallToActionBannerComponent {}
