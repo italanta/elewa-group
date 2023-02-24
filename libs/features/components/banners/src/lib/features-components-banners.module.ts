3466ba16506377120554030f96a0dd9ed6676e2d
Kangonga <guchu413@gmail.com>
2023-02-25 00:21:09 +0300

    feat: Import routermodule

--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
@@ -7,7 +8,7 @@ import { ElewaGroupTeamMemberComponent } from './banners/elewa-group-team-member
 import { ElewaCallToActionBannerComponent } from './banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component';
 
 @NgModule({
-  imports: [CommonModule, ButtonsModule],
+  imports: [CommonModule, ButtonsModule,RouterModule],
   declarations: [
     ElewaGroupTeamMemberComponent,
     ElewaGroupImageAndTextBannerComponent,
