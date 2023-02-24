fca60a7d09078de33b2028edcf0b4ddbddcf962b
Kangonga <guchu413@gmail.com>
2023-02-25 00:15:35 +0300

    feat: create the reusable component and import it in the parent module

---
+++ libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-call-to-action-banner',
+  templateUrl: './elewa-call-to-action-banner.component.html',
+  styleUrls: ['./elewa-call-to-action-banner.component.scss'],
+})
+export class ElewaCallToActionBannerComponent {}
--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -4,12 +4,18 @@ import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 import { ElewaGroupTeamMemberComponent } from './banners/elewa-group-team-member/elewa-group-team-member.component';
+import { ElewaCallToActionBannerComponent } from './banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
-  declarations: [ElewaGroupTeamMemberComponent, ElewaGroupImageAndTextBannerComponent],
-  exports: [ElewaGroupTeamMemberComponent, ElewaGroupImageAndTextBannerComponent],
+  declarations: [
+    ElewaGroupTeamMemberComponent,
+    ElewaGroupImageAndTextBannerComponent,
+    ElewaCallToActionBannerComponent,
+  ],
+  exports: [
+    ElewaGroupTeamMemberComponent,
+    ElewaGroupImageAndTextBannerComponent,
+  ],
 })
 export class BannersModule {}
-
-
