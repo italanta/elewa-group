aad883baf77b03132425c88cd18cd40888d3047e
Kangonga <guchu413@gmail.com>
2023-02-25 00:22:08 +0300

    feat: create input properties to accept data passed down to the reusable component

--- libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-call-to-action-banner',
   templateUrl: './elewa-call-to-action-banner.component.html',
   styleUrls: ['./elewa-call-to-action-banner.component.scss'],
 })
-export class ElewaCallToActionBannerComponent {}
+export class ElewaCallToActionBannerComponent {
+  acceptable_text_align = ["center","left","right","justified"]
+  @Input() message:string
+  @Input() callToAction:string
+  @Input() textAlign:string
+  @Input() destinationUrl:string
+  @Input() callToActionIntro:string
+}
