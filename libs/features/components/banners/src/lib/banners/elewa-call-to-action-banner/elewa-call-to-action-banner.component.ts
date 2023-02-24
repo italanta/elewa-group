d4d666d5374959d6b5a475faa01362a1bef78e72
Kangonga <guchu413@gmail.com>
2023-02-25 00:33:39 +0300

    feat: create the input properties to accept data to pass to the template.

--- libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-call-to-action-banner/elewa-call-to-action-banner.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-call-to-action-banner',
   templateUrl: './elewa-call-to-action-banner.component.html',
   styleUrls: ['./elewa-call-to-action-banner.component.scss'],
 })
-export class ElewaCallToActionBannerComponent {}
+export class ElewaCallToActionBannerComponent {
+  @Input() message:string
+  @Input() callToAction:string
+  @Input() textAlign:string
+  @Input() destinationUrl:string
+  @Input() callToActionIntro:string
+}
