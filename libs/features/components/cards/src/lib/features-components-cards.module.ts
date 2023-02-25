b3c15fb319d5e6e2ec43959519d02fc02adb4bd6
+ <+>
2023-02-25 17:18:05 +0300

    feat:added configuration to the angular.json file for the cad component

--- libs/features/components/cards/src/lib/features-components-cards.module.ts
+++ libs/features/components/cards/src/lib/features-components-cards.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaGroupCardComponent } from './cards/elewa-group-card/elewa-group-card.component';
-import { ElewaVerticalIconAndTextComponent } from './cards/elewa-vertical-icon-and-text/elewa-vertical-icon-and-text.component'
-
+import { ElewaVerticalIconAndTextComponent } from './cards/elewa-vertical-icon-and-text/elewa-vertical-icon-and-text.component';
+import { ElewaKeyFigureCardComponent } from './cards/elewa-key-figure-card/elewa-key-figure-card.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaGroupCardComponent, ElewaVerticalIconAndTextComponent],
-  exports: [ ElewaGroupCardComponent, ElewaVerticalIconAndTextComponent]
+  declarations: [
+    ElewaGroupCardComponent,
+    ElewaVerticalIconAndTextComponent,
+    ElewaKeyFigureCardComponent,
+  ],
+  exports: [ElewaGroupCardComponent, ElewaVerticalIconAndTextComponent],
 })
 export class CardsModule {}
