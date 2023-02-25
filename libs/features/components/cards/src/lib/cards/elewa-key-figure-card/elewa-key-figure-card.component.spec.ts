64e67dbfd757792f405ba5df3635eb0c95be00d5
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-02-25 15:37:45 +0300

    feat: Added styling

---
+++ libs/features/components/cards/src/lib/cards/elewa-key-figure-card/elewa-key-figure-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ElewaKeyFigureCardComponent } from './elewa-key-figure-card.component';
+
+describe('ElewaKeyFigureCardComponent', () => {
+  let component: ElewaKeyFigureCardComponent;
+  let fixture: ComponentFixture<ElewaKeyFigureCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ElewaKeyFigureCardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ElewaKeyFigureCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
