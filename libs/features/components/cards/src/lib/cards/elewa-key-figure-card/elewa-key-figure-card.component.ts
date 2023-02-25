808d9234567fb13f39ed75e1a536d0bcd8a633a0
+ <+>
2023-02-25 17:16:14 +0300

    feat:created the elewa-key-figure-component

---
+++ libs/features/components/cards/src/lib/cards/elewa-key-figure-card/elewa-key-figure-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-key-figure-card',
+  templateUrl: './elewa-key-figure-card.component.html',
+  styleUrls: ['./elewa-key-figure-card.component.scss'],
+})
+export class ElewaKeyFigureCardComponent {}
