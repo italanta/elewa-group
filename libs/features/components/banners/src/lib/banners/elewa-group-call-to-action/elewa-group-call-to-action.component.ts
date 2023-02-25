2f9615dbbada16f48f4281ec658fd59ff3a366ee
Vincent <makokhavomondi@gmail.com>
2023-02-25 10:09:13 +0300

    :construction: Copy stuff from HomePageJobsComponent to ElewaGroupCallToActionComponent

--- libs/features/components/banners/src/lib/banners/elewa-group-call-to-action/elewa-group-call-to-action.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-group-call-to-action/elewa-group-call-to-action.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-group-call-to-action',
   templateUrl: './elewa-group-call-to-action.component.html',
   styleUrls: ['./elewa-group-call-to-action.component.scss'],
 })
-export class ElewaGroupCallToActionComponent {}
+export class ElewaGroupCallToActionComponent {
+  @Input() message = "Become part of the family"
+  @Input() callToActionIntroducer = "View"
+  @Input() callToAction = "open opportunities"
+  @Input() callToActionURL = "jobs"
+  @Input() align = "left"
+}
--- libs/features/components/banners/src/lib/features-components-banners.module.ts
+++ libs/features/components/banners/src/lib/features-components-banners.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonsModule } from '@elewa-group/features/components/buttons';
+import { RouterModule } from '@angular/router';
 
 import { ElewaGroupImageAndTextBannerComponent } from './banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component';
 import { ElewaGroupTeamMemberComponent } from './banners/elewa-group-team-member/elewa-group-team-member.component';
 import { ElewaGroupCallToActionComponent } from './banners/elewa-group-call-to-action/elewa-group-call-to-action.component';
 
 @NgModule({
-  imports: [CommonModule, ButtonsModule],
+  imports: [CommonModule, ButtonsModule, RouterModule],
   declarations: [
     ElewaGroupTeamMemberComponent,
     ElewaGroupImageAndTextBannerComponent,
@@ -16,6 +17,7 @@ import { ElewaGroupCallToActionComponent } from './banners/elewa-group-call-to-a
   exports: [
     ElewaGroupTeamMemberComponent,
     ElewaGroupImageAndTextBannerComponent,
+    ElewaGroupCallToActionComponent
   ],
 })
 export class BannersModule {}
