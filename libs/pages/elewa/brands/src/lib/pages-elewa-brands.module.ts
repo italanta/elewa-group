bd363bcd5cc05bc7be909a971aafd46ffd316eb6
calvin <ckodalo@gmail.com>
2023-02-27 18:17:46 +0300

    added component

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -9,9 +9,14 @@ import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opp
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
 import { BrandsRoutingModule } from './brands.routing';
+import { ElewaBrandsItalantaComponent } from './components/elewa-brands-italanta/elewa-brands-italanta.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
-  declarations: [ElewaBrandsPageComponent, ElewaBrandsOpportunitiesComponent],
+  declarations: [
+    ElewaBrandsPageComponent,
+    ElewaBrandsOpportunitiesComponent,
+    ElewaBrandsItalantaComponent,
+  ],
 })
 export class BrandsModule {}
