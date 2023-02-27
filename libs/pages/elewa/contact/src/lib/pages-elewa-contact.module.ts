1ccd571c98584646b2343e25b82650e2ab4dadb1
marcokumu <marcokumu10@gmail.com>
2023-02-27 12:01:22 +0300

    ✨feat: generated the contact-form component and imported it in the contact page

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent],
+  imports: [CommonModule, ContactRoutingModule, ContactFormComponent],
+  declarations: [ContactPageComponent, ContactFormComponent],
+
 })
 export class ContactPageModule {}
