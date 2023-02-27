d273e829fbe8d71431a1826658142d32548da87e
marcokumu <marcokumu10@gmail.com>
2023-02-27 21:06:07 +0300

    ✨feat: added navigation  function

--- libs/pages/elewa/careers/src/lib/components/careers-vacancies/careers-vacancies.component.ts
+++ libs/pages/elewa/careers/src/lib/components/careers-vacancies/careers-vacancies.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'elewa-group-careers-vacancies',
   templateUrl: './careers-vacancies.component.html',
   styleUrls: ['./careers-vacancies.component.scss'],
 })
 export class CareersVacanciesComponent {
-message1= 'Apply Now';
+  message1 = 'Apply Now';
+  constructor(private router: Router) {}
+  btnClick = () => {
+    this.router.navigate([]);
+  };
+  
 }
