f4136991ff440fd6a987788afd1cb41ce74ee9b2
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-28 00:17:42 +0300

    ✨feat: passed in the elewa education brand data in the ts file

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-elewa-education',
+  templateUrl: './elewa-brands-elewa-education.component.html',
+  styleUrls: ['./elewa-brands-elewa-education.component.scss'],
+})
+export class ElewaBrandsElewaEducationComponent {
+  logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/Elewa_education_pp1gnc.svg';
+  name = 'Elewa Education';
+  brandDescription = 'The secret to successful education is empathy. By understanding people and the holistic contexts in which they operate, Elewa Education develops proffesional training programs that are measured, sustainable, effective, and highly scalable. '
+  buttonText = 'Visit website'
+  url = '';
+  logoPlacement: 'right' | 'left' = 'left';
+}
