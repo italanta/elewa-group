cf411dc99da87eaddbe6c61bc5d22f601fc57834
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-27 13:37:00 +0300

    :sparkles: Generate the brands page component

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaBrandsPageComponent],
 })
 export class PagesElewaBrandsModule {}
