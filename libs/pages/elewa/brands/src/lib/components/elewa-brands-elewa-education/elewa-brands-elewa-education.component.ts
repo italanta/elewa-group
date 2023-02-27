cfe3774663462a1ea0cc199cb9990e5a9bcbf20c
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-27 18:27:53 +0300

    ✨:feat added elewa brands education component to pages/elewa/brands lib

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-elewa-education',
+  templateUrl: './elewa-brands-elewa-education.component.html',
+  styleUrls: ['./elewa-brands-elewa-education.component.scss'],
+})
+export class ElewaBrandsElewaEducationComponent {}
