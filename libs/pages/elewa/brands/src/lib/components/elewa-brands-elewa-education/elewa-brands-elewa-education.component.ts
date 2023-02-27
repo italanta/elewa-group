ff9fada4e3e6ea1add25c18bf771643d6574bd7b
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-28 02:22:45 +0300

    Revert "📝:edited the elewa/brands/education for grammar error"
    This reverts commit c80bf32a3ffd2a1d259199e086a3e6219391265d.

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class ElewaBrandsElewaEducationComponent {
   @Input () logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/Elewa_education_pp1gnc.svg';
   @Input () name  = 'Elewa Education';
-  @Input () brandDescription = "The secret to successful education is empathy.By understanding people holistic contexts they operate,Elewa education develops proffessional training programs that are measured,sustainable,effective,and highly scalable.";
+  @Input () brandDescription = "The secret to successful education is empathy.By understanding people holistic contexts they operate,Elewa educaation develops proffessional training programs that are measured,sustainable,effective,and highly scalable.";
   @Input () buttonText  = 'Visit website';
   @Input() url = '';
   @Input () logoPlacement: 'right' | 'left' = 'right'; 
