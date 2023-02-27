5cdcd7f3ce97f93dd9796ee07b465e15b5d70877
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-27 21:36:32 +0300

    ✨feat: imported the brand venture labs component into the module file

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -5,6 +5,7 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
 
 import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
+import { ElewaBrandsVentureLabsComponent } from './components/elewa-brands-venture-labs/elewa-brands-venture-labs.component';
 
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
@@ -12,6 +13,6 @@ import { BrandsRoutingModule } from './brands.routing';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
-  declarations: [ElewaBrandsPageComponent, ElewaBrandsOpportunitiesComponent],
+  declarations: [ElewaBrandsPageComponent, ElewaBrandsOpportunitiesComponent,ElewaBrandsVentureLabsComponent],
 })
 export class BrandsModule {}
