8926933cc331eb2eefb042defc4211ea3363b263
kahoragachau <joelkahoragachau6@gmail.com>
2023-02-27 17:29:32 +0300

    ✨chore: Reuse  elewa hero component

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-hero/elewa-brands-hero.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-hero/elewa-brands-hero.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './elewa-brands-hero.component.html',
   styleUrls: ['./elewa-brands-hero.component.scss'],
 })
-export class ElewaBrandsHeroComponent {}
+export class ElewaBrandsHeroComponent {
+  title = "OUR BRANDS"
+  subtitle = "Changing the world One person at a time"
+  backgroundImage = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690292/elewa-group-website/hero-Images/Brands_ewjhj2.jpg"
+  footer=""
+}
