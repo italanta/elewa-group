258a5316822c67e74eb2eea7b6740e3f55bb3c16
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-02-27 21:52:16 +0300

    🎨 feat: commented out the stylrUrl

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'elewa-group-elewa-brands-italanta',
   templateUrl: './elewa-brands-italanta.component.html',
-  styleUrls: ['./elewa-brands-italanta.component.scss'],
+  // styleUrls: ['./elewa-brands-italanta.component.scss'],
 })
 export class ElewaBrandsItalantaComponent {
   @Input () logo: string = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690298/elewa-group-website/Icons/SVG/Italanta_mpxz8d.svg';
