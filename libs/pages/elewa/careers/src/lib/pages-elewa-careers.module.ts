102c395b47aad9fd41fe9bf9369e12e7fbaeb3a9
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-02-27 14:58:12 +0300

    :hammer:add careers-hero component

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { CareersPageComponent } from './pages/careers-page/careers-page.component';
+import { CareersHeroComponent } from './components/careers-hero/careers-hero.component';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule],
-  declarations: [CareersPageComponent],
+  imports: [CommonModule, LayoutModule],
+  declarations: [CareersPageComponent, CareersHeroComponent],
 })
 export class PagesElewaCareersModule {}
