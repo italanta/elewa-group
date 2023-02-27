bd697385218982c443a47c89c0beb6e67471fa91
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-27 12:27:40 +0300

    :sparkles: adding routing to the module

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CareersPageComponent } from './pages/careers-page/careers-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [CareersPageComponent],
 })
 export class PagesElewaCareersModule {}
