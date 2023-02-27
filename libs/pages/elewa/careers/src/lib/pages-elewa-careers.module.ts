80df38c16249291d13b11c72138be4ff1d4766e9
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-27 12:52:12 +0300

    :feat Add a careers tab on the header and it's routerLink

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
 import { CareersPageComponent } from './pages/careers-page/careers-page.component';
 
+import { CareersRoutingModule } from './careers.routing';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule, BannersModule, CareersRoutingModule],
   declarations: [CareersPageComponent],
 })
 export class PagesElewaCareersModule {}
