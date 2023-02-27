00a8090893e55d2bf0f98d6da12b7e21986ee871
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-27 14:41:19 +0300

    :sparkles: :tada: generating a reusable brand component

---
+++ libs/features/components/banners/src/lib/banners/elewa-group-brands/elewa-group-brands/elewa-group-brands.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-brands',
+  templateUrl: './elewa-group-brands.component.html',
+  styleUrls: ['./elewa-group-brands.component.scss'],
+})
+export class ElewaGroupBrandsComponent {}
