145cab92e192cccc14f42c15b3107dc276c38c14
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-28 02:52:35 +0300

    📝:edit the elewa/brands/education

--- libs/pages/elewa/brands/src/lib/brands.routing.ts
+++ libs/pages/elewa/brands/src/lib/brands.routing.ts
@@ -16,4 +16,3 @@ export const ELEWA_BRANDS_ROUTES: Route[] = [
 })
 export class BrandsRoutingModule { }
 
-
--- libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-elewa-education/elewa-brands-elewa-education.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-brands-elewa-education',
@@ -8,8 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ElewaBrandsElewaEducationComponent {
   @Input () logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/Elewa_education_pp1gnc.svg';
   @Input () name  = 'Elewa Education';
-  @Input () brandDescription = "The secret to successful education is empathy.By understanding people holistic contexts they operate,Elewa educaation develops proffessional training programs that are measured,sustainable,effective,and highly scalable.";
+  @Input () brandDescription = "The secret to successful education is empathy.By understanding people holistic contexts they operate,Elewa education develops proffessional training programs that are measured,sustainable,effective,and highly scalable.";
   @Input () buttonText  = 'Visit website';
   @Input() url = '';
-  @Input () logoPlacement: 'right' | 'left' = 'right'; 
+  @Input () logoPlacement: 'right' | 'left' = 'right';
 }
