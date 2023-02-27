b1b2eac3a50005dd7203c339fbe2bf869cd8bdd4
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-27 18:42:44 +0300

    📝:added a text to the button  input

--- libs/features/components/banners/src/lib/banners/elewa-group-brands/elewa-group-brands/elewa-group-brands.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-group-brands/elewa-group-brands/elewa-group-brands.component.ts
@@ -10,7 +10,7 @@ export class ElewaGroupBrandsComponent {
   @Input () logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/Elewa_education_pp1gnc.svg';
   @Input () name  = 'Elewa Education';
   @Input () brandDescription = "The secret to successful education is empathy.By understanding people holistic contexts they operate, Elewa educaation develops proffessional training programs that are measured, sustainable,effective, and highly scallable"
-  @Input () buttonText  = ''
+  @Input () buttonText  = 'Visit website'
   @Input() url = '';
   @Input () logoPlacement: 'right' | 'left' = 'left';
 }
