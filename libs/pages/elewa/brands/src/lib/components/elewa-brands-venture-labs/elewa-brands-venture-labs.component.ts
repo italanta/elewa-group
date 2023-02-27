81fbf9403854b753f5758bc62c0920c09c581f09
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-27 21:35:26 +0300

    ✨feat: passed i new data from the reusable component.

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-venture-labs/elewa-brands-venture-labs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-venture-labs',
+  templateUrl: './elewa-brands-venture-labs.component.html',
+  styleUrls: ['./elewa-brands-venture-labs.component.scss'],
+})
+export class ElewaBrandsVentureLabsComponent {
+  logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690293/elewa-group-website/Icons/SVG/VentureLabs_i5cq9y.svg';
+  name = 'Venture Labs';
+  brandDescription = 'A moonshoot factory, Elewa Venture Labs invests and incubates gobal SaaS startups with high potential. Developed and grown by Elewa talents and our network.'
+  buttonText = 'Explore our startups'
+  url = '';
+  logoPlacement: 'right' | 'left' = 'left';
+}
