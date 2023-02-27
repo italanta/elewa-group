4640ee0a2c3c9736601b7772e3fd95420ded8768
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-28 00:20:48 +0300

    ✨feat: imported the elewa brands elewa education component in to the elewa brands module file.

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -5,6 +5,7 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
 
 import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
+import { ElewaBrandsElewaEducationComponent } from './components/elewa-brands-elewa-education/elewa-brands-elewa-education.component';
 
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
@@ -12,6 +13,6 @@ import { BrandsRoutingModule } from './brands.routing';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
-  declarations: [ElewaBrandsPageComponent, ElewaBrandsOpportunitiesComponent],
+  declarations: [ElewaBrandsPageComponent, ElewaBrandsOpportunitiesComponent,ElewaBrandsElewaEducationComponent],
 })
 export class BrandsModule {}
