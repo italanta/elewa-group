82419b2674f2245d03ed1a765b8892810ac27c2f
Kangonga <guchu413@gmail.com>
2023-02-27 12:18:45 +0300

    feat: create the component and add it in its parent module.

---
+++ libs/features/components/ui-lists/src/lib/elewa-social-impact-projects-section/elewa-social-impact-projects-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-social-impact-projects-section',
+  templateUrl: './elewa-social-impact-projects-section.component.html',
+  styleUrls: ['./elewa-social-impact-projects-section.component.scss'],
+})
+export class ElewaSocialImpactProjectsSectionComponent {}
