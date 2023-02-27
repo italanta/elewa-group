1f50f8a2194c9a7f9435252ec1487aafbf5f74bb
marcokumu <marcokumu10@gmail.com>
2023-02-27 17:33:53 +0300

    ✨feat: added the contact form to the contact page.

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-@NgModule({
-  imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent, ContactFormComponent],
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
+import { LayoutModule} from '@elewa-group/elements/layout';
 
+@NgModule({
+  imports: [
+    CommonModule,
+    ContactRoutingModule,
+    ReactiveFormsModule,
+    ButtonsModule,
+    LayoutModule,
+  ],
+  declarations: [ContactPageComponent, ContactFormComponent],
 })
 export class ContactPageModule {}
