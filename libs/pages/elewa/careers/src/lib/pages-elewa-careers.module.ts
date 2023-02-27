142dcc0643da2fc256e773cb237ce07b704935c6
marcokumu <marcokumu10@gmail.com>
2023-02-27 20:45:11 +0300

    ✨feat: imported the buttonsmodule and vacancies component

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -6,9 +6,12 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { CareersPageComponent } from './pages/careers-page/careers-page.component';
 
 import { CareersRoutingModule } from './careers.routing';
+import { CareersVacanciesComponent} from './components/careers-vacancies/careers-vacancies.component';
+import {ButtonsModule} from '@elewa-group/features/components/buttons';
+
 
 @NgModule({
-  imports: [CommonModule,LayoutModule, CareersRoutingModule],
-  declarations: [CareersPageComponent],
+  imports: [CommonModule, LayoutModule, CareersRoutingModule, ButtonsModule],
+  declarations: [CareersPageComponent, CareersVacanciesComponent],
 })
 export class CareersModule {}
