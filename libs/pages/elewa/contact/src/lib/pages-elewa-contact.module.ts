d3561d1a574213e13dd3d62e077c4c1130011d6a
Kangonga <guchu413@gmail.com>
2023-02-27 17:09:48 +0300

    feat: import the created component, alongside reactive forms module and the buttons module

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent],
+  imports: [CommonModule, ContactRoutingModule,ReactiveFormsModule,ButtonsModule],
+  declarations: [ContactPageComponent, ContactFormComponent],
 })
 export class ContactPageModule {}
