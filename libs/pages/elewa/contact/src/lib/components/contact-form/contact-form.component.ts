684448ca8e17215fc4be1fd9a7bd66a92e696903
marcokumu <marcokumu10@gmail.com>
2023-02-27 17:33:15 +0300

    ✨feat: used the angular FormGroup to manage form data

--- libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
+++ libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'elewa-group-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  message1 = "Send message"
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: '',
+      email: '',
+      companyName: '',
+      category: '',
+      message: '',
+    });
+  }
+}
