b88285a7f18e304ee55ca878713774dccf562cc1
calvin <ckodalo@gmail.com>
2023-02-27 18:47:07 +0300

    customized inputs

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './elewa-brands-italanta.component.html',
   styleUrls: ['./elewa-brands-italanta.component.scss'],
 })
-export class ElewaBrandsItalantaComponent {}
+export class ElewaBrandsItalantaComponent {
+
+  logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690298/elewa-group-website/Icons/SVG/Italanta_mpxz8d.svg';
+  name = 'ITalanta';
+  brandDescription = 'From Silicon Savannah to the world, Italanta is an offshore tech devleopment agency that doubles as a tech talent accelerator. italanta has a portolio of clients that consist of some of the hottest global startups'
+  buttonText = 'Visit Website'
+  url = 'https://italanta.net/';
+  logoPlacement: 'right'
+}
