7d84ac836a5285bd98523a4805c8c22d72f640af
marcokumu <marcokumu10@gmail.com>
2023-02-27 20:41:59 +0300

    ✨feat: passed data to the reused button i.e "Apply Now " message "

---
+++ libs/pages/elewa/careers/src/lib/components/careers-vacancies/careers-vacancies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-careers-vacancies',
+  templateUrl: './careers-vacancies.component.html',
+  styleUrls: ['./careers-vacancies.component.scss'],
+})
+export class CareersVacanciesComponent {
+message1= 'Apply Now';
+}
