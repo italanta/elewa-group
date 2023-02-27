b43f970147b52c86785223f94c4f56c2e9fd3f93
kahoragachau <joelkahoragachau6@gmail.com>
2023-02-27 16:38:13 +0300

    ✨feat: Create Elewa Brands Hero Component

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-hero/elewa-brands-hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-hero',
+  templateUrl: './elewa-brands-hero.component.html',
+  styleUrls: ['./elewa-brands-hero.component.scss'],
+})
+export class ElewaBrandsHeroComponent {}
