89b8090810658ccc13d80a62ef1a5665bbab6cf4
Kangonga <guchu413@gmail.com>
2023-02-27 17:10:29 +0300

    feat: create the form variables and form control

---
+++ libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'elewa-group-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss'],
+})
+export class ContactFormComponent {
+  message = "Send Message"
+  contactData = new FormGroup({ 
+    name: new FormControl(''),
+    company_name: new FormControl(''),
+    email: new FormControl(''),
+    option: new FormControl(''),
+    message: new FormControl(''),
+  })
+  handleSubmit(){
+    return('')
+  }
+}
