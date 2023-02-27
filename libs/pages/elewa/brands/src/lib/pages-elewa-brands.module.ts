e29afd980ed9bf63d34621e159a62d56c81cf15c
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-27 13:44:58 +0300

    :+1: Importing the routing module to the brands module imports file

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
+import { BrandsRoutingModule } from './brands.routing';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BrandsRoutingModule],
   declarations: [ElewaBrandsPageComponent],
 })
 export class PagesElewaBrandsModule {}
