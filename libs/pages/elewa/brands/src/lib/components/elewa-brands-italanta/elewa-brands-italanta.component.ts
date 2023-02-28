a214d79de00dbb3d79b62e224a077b3de0335c18
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-02-28 04:14:47 +0300

    📝:edit url to go to home page

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
@@ -10,5 +10,6 @@ export class ElewaBrandsItalantaComponent {
   @Input () name  = 'ITalanta';
   @Input () brandDescription = "From Silicon Savannah to the world,iTalanta is an offshore tech development agency that doubles as a tech accelerator. Italanta has a portfolio of clients consisting out of the some of the hottest global startups.";
   @Input () buttonText  = 'Visit website';
+  @Input() url = '/home';
   @Input()  logoPlacement: 'right'| 'left' = 'right'
 }
