f15aa67df5ea66e5921ab0db49353cc351142144
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-02-28 18:39:34 +0300

    :hammer:add elewa brands creative hub

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-creative-hub/elewa-brands-creative-hub.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-creative-hub',
+  templateUrl: './elewa-brands-creative-hub.component.html',
+  styleUrls: ['./elewa-brands-creative-hub.component.scss'],
+})
+export class ElewaBrandsCreativeHubComponent {}
