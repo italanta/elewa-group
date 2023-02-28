118d77fa5a6fd87e717e0c6004e1f0fd7fc47586
marcokumu <marcokumu10@gmail.com>
2023-02-28 11:02:53 +0000

    ✨feat: created the cards array to pass the data for each card

--- libs/pages/elewa/invest/src/lib/components/elewa-invest-key-figures-section/elewa-invest-key-figures-section.component.ts
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-key-figures-section/elewa-invest-key-figures-section.component.ts
@@ -5,4 +5,58 @@ import { Component } from '@angular/core';
   templateUrl: './elewa-invest-key-figures-section.component.html',
   styleUrls: ['./elewa-invest-key-figures-section.component.scss'],
 })
-export class ElewaInvestKeyFiguresSectionComponent {}
+export class ElewaInvestKeyFiguresSectionComponent {
+  //  figure2 ="1.3M";
+  // description2 ="Approx. number of Kenyan talents reaching the age of 18 in 2022.";
+  // isImage2 = false;
+  cards = [
+    {
+      figure2: '1.3M',
+      description2:
+        'Approx. number of Kenyan talents reaching the age of 18 in 2022.',
+      isImage2: false,
+    },
+    {
+      figure2: '5M',
+      description2:
+        'Number of East-Africans (EAC) reaching the age of 18 in 2022.',
+      isImage2: false,
+    },
+    {
+      figure2: '2022',
+      description2:
+        'Demecratic Republic of Congo joins the East African Community.',
+      isImage2: false,
+    },
+    {
+      figure2: '1.5M',
+      description2:
+        'Number of unfilled IT vacancies in the US and EU Markets.',
+      isImage2: false,
+    },
+    {
+      figure2: '10M',
+      description2:
+        'Number of unfilled technical vacancies in the US and EU Markets.',
+      isImage2: false,
+    },
+    {
+      figure2: '7.5%',
+      description2:
+        'Kenyan 2021 GDP growth.',
+      isImage2: false,
+    },
+    {
+      figure2: '$4.6M',
+      description2:
+        'Startup funding raised by African startups in 2022.',
+      isImage2: false,
+    },
+    {
+      figure2: '310%',
+      description2:
+        'Increase in startup funding for companies with HQ in Kenya.',
+      isImage2: false,
+    },
+  ];
+}
