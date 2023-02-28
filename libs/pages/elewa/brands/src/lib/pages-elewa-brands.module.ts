6fe9d74c84b2778c93c006acc1a16e76c810b371
calvin <ckodalo@gmail.com>
2023-02-28 17:54:18 +0300

    FIX: order of imports

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
 
-import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
-
-import { ElewaBrandsItalantaComponent } from './components/elewa-brands-italanta/elewa-brands-italanta.component';
-
 import { ElewaBrandsHeroComponent } from './components/elewa-brands-hero/elewa-brands-hero.component';
+
+import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
+import { ElewaBrandsItalantaComponent } from './components/elewa-brands-italanta/elewa-brands-italanta.component';
 
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
