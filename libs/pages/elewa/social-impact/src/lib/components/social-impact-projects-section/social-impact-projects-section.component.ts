d7351658a1a15aa4ae204d05b3a69558d6efc792
Vincent <makokhavomondi@gmail.com>
2023-02-28 15:57:16 +0300

    :construction: Pass values to the call to action component

--- libs/pages/elewa/social-impact/src/lib/components/social-impact-projects-section/social-impact-projects-section.component.ts
+++ libs/pages/elewa/social-impact/src/lib/components/social-impact-projects-section/social-impact-projects-section.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'elewa-group-social-impact-projects-section',
   templateUrl: './social-impact-projects-section.component.html',
   styleUrls: ['./social-impact-projects-section.component.scss'],
 })
-export class SocialImpactProjectsSectionComponent {}
+export class SocialImpactProjectsSectionComponent {
+  @Input() message = "Become part of our mission.";
+  @Input() word = "And join";
+  @Input()callToActionText ="Elewa";
+  @Input()url= "/join-elewa";
+}
