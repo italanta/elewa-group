2c4396d79c5b46ffd760a196a4c9c03eb728758f
marcokumu <marcokumu10@gmail.com>
2023-02-28 11:53:50 +0000

    ♻️refactor: slight change

--- libs/pages/elewa/invest/src/lib/components/elewa-invest-key-figures-section/elewa-invest-key-figures-section.component.ts
+++ libs/pages/elewa/invest/src/lib/components/elewa-invest-key-figures-section/elewa-invest-key-figures-section.component.ts
@@ -11,52 +11,52 @@ export class ElewaInvestKeyFiguresSectionComponent {
   // isImage2 = false;
   cards = [
     {
-      figure2: '1.3M',
-      description2:
+      figure: '1.3M',
+      description:
         'Approx. number of Kenyan talents reaching the age of 18 in 2022.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '5M',
-      description2:
+      figure: '5M',
+      description:
         'Number of East-Africans (EAC) reaching the age of 18 in 2022.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '2022',
-      description2:
+      figure: '2022',
+      description:
         'Demecratic Republic of Congo joins the East African Community.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '1.5M',
-      description2:
+      figure: '1.5M',
+      description:
         'Number of unfilled IT vacancies in the US and EU Markets.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '10M',
-      description2:
+      figure: '10M',
+      description:
         'Number of unfilled technical vacancies in the US and EU Markets.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '7.5%',
-      description2:
+      figure: '7.5%',
+      description:
         'Kenyan 2021 GDP growth.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '$4.6M',
-      description2:
+      figure: '$4.6M',
+      description:
         'Startup funding raised by African startups in 2022.',
-      isImage2: false,
+      isImage: false,
     },
     {
-      figure2: '310%',
-      description2:
+      figure: '310%',
+      description:
         'Increase in startup funding for companies with HQ in Kenya.',
-      isImage2: false,
+      isImage: false,
     },
   ];
 }
