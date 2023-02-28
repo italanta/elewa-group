759c524d9880b9b320ea2119be09f1e6800c1a54
Vincent <makokhavomondi@gmail.com>
2023-02-28 15:48:05 +0300

    :construction: Import UiListsModule to enable its use

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
+import {UiListsModule } from '@elewa-group/features/components/ui-lists';
 import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
@@ -20,6 +21,7 @@ import { SocialImpactProjectsSectionComponent } from './components/social-impact
     LayoutModule,
     BannersModule,
     SocialImpactRoutingModule,
+    UiListsModule
   ],
   declarations: [
     SocialImpactPageComponent,
