9a9b41425dcb43f20e96cee0bbf85d0a60837b01
Mercy Kamene <mercykmualuko@gmail.com>
2023-02-28 14:56:48 +0300

    import Cards module

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -6,9 +6,11 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { CareersPageComponent } from './pages/careers-page/careers-page.component';
 
 import { CareersRoutingModule } from './careers.routing';
+import { CareersVacanciesComponent } from './careers-vacancies/careers-vacancies.component';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule, CareersRoutingModule],
-  declarations: [CareersPageComponent],
+  imports: [CommonModule, LayoutModule, CareersRoutingModule,ButtonsModule],
+  declarations: [CareersPageComponent, CareersVacanciesComponent],
 })
 export class CareersModule {}
