6ab5c76cf59a4a70618fb271db0fa6dd4e98518b
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-02-28 22:55:02 +0300

    :hammer: import buttons module

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
-
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
 
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
@@ -12,7 +12,7 @@ import { BrandsRoutingModule } from './brands.routing';
 import { ElewaBrandsCreativeHubComponent } from './components/elewa-brands-creative-hub/elewa-brands-creative-hub.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
+  imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule,ButtonsModule],
   declarations: [
     ElewaBrandsPageComponent,
     ElewaBrandsOpportunitiesComponent,
