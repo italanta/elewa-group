95a0075967b20ac18e67d7322068c7f7c8280798
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-02-28 12:01:02 +0300

    :sparkles: feat: generated careers-value-proposition component

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -7,10 +7,15 @@ import { CareersPageComponent } from './pages/careers-page/careers-page.componen
 
 import { CareersRoutingModule } from './careers.routing';
 import { CareersHeroComponent } from './components/careers-hero/careers-hero.component';
-import {ButtonsModule} from '@elewa-group/features/components/buttons';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
+import { CareersValuePropositionComponent } from './components/careers-value-proposition/careers-value-proposition.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, CareersRoutingModule, ButtonsModule],
-  declarations: [CareersPageComponent, CareersHeroComponent],
+  declarations: [
+    CareersPageComponent,
+    CareersHeroComponent,
+    CareersValuePropositionComponent,
+  ],
 })
 export class CareersModule {}
