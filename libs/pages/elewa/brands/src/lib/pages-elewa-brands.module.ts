c6c0f6c5ad49d04c5aad8073215d11be39215b9c
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-02-28 16:52:06 +0300

    ♻️refactor: organize the imports in the module file

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
-
-import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
 import { ElewaBrandsElewaEducationComponent } from './components/elewa-brands-elewa-education/elewa-brands-elewa-education.component';
+import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
+
 
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
