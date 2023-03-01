6b67c9ce53907d3b81b514f17383af73f1c14e20
nziokikevin <kevinnzioki10@gmail.com>
2023-03-01 18:21:19 +0300

    :recycle: refactor: changing position of name property

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-venture-labs/elewa-brands-venture-labs.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-venture-labs/elewa-brands-venture-labs.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class ElewaBrandsVentureLabsComponent {
   logoPlacement: 'right' | 'left' = 'left'
   logo = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690293/elewa-group-website/Icons/SVG/VentureLabs_i5cq9y.svg'
+  name = 'Venture Labs'
   brandDescription = 'A moonshot factory. Elewa Venture Labs invests and incubates global SaaS startups with high potential. Developed and grown by Elewa talents and our network.'
   buttonText = 'Explore our startups'
-  name = 'Venture Labs'
 }
