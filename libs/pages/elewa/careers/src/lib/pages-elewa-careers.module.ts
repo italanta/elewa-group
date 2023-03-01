c660cdcc6d27a13803955af97cea5d9d2a0bfc29
Rajab Karume <rkarume@gmail.com>
2023-03-01 09:40:22 +0300

    ✨created career value component

--- libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
+++ libs/pages/elewa/careers/src/lib/pages-elewa-careers.module.ts
@@ -6,9 +6,10 @@ import { LayoutModule } from '@elewa-group/elements/layout';
 import { CareersPageComponent } from './pages/careers-page/careers-page.component';
 
 import { CareersRoutingModule } from './careers.routing';
+import { CareersValuePropositionComponent } from './components/careers-value-proposition/careers-value-proposition.component';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule, CareersRoutingModule],
-  declarations: [CareersPageComponent],
+  imports: [CommonModule, LayoutModule, CareersRoutingModule],
+  declarations: [CareersPageComponent, CareersValuePropositionComponent],
 })
 export class CareersModule {}
