928d5a8446f6e525b839c596d0ca46cd35f83ede
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-01 13:48:34 +0300

    let it remain

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -7,14 +7,14 @@ import { BannersModule } from '@elewa-group/features/components/banners';
 import { SocialImpactPeopleSectionComponent } from './components/social-impact-people-section/social-impact-people-section.component';
 import { SocialImpactHeroSectionComponent } from './components/social-impact-hero-section/social-impact-hero-section.component';
 import { SocialImpactCooperativeSectionComponent } from './components/social-impact-cooperative-section/social-impact-cooperative-section.component';
-
+import { CardsModule } from '@elewa-group/features/components/cards';
 
 import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
 
 import { SocialImpactRoutingModule } from './social-impact.routing';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, BannersModule, SocialImpactRoutingModule],
+  imports: [CommonModule, LayoutModule, BannersModule, CardsModule,  SocialImpactRoutingModule],
   declarations: [
     SocialImpactPageComponent,
     SocialImpactHeroSectionComponent,
