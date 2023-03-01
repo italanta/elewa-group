843f6cb498fbf69f80f45729749961c941bd1210
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-01 06:19:30 +0300

    updating  the pages/elewa/brands

--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -1,10 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { ElewaBrandsElewaEducationComponent } from './components/elewa-brands-elewa-education/elewa-brands-elewa-education.component';
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
+
 import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opportunities/elewa-brands-opportunities.component';
+import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
+
+import { BrandsRoutingModule } from './brands.routing';
+import { ElewaBrandsElewaEducationComponent } from './components/elewa-brands-elewa-education/elewa-brands-elewa-education.component';
 import { ElewaBrandsHeroComponent } from './components/elewa-brands-hero/elewa-brands-hero.component';
 
-import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
-import { BrandsRoutingModule } from './brands.routing';
+
 @NgModule({
   imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
   declarations: [
@@ -12,7 +19,6 @@ import { BrandsRoutingModule } from './brands.routing';
     ElewaBrandsOpportunitiesComponent,
     ElewaBrandsElewaEducationComponent,
     ElewaBrandsHeroComponent,
-
   ],
 })
 export class BrandsModule {}
