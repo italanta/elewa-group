b1cd43f5e801ee11f5676307ffe2e8896ea4feac
nziokikevin <kevinnzioki10@gmail.com>
2023-03-02 10:32:31 +0300

    :sparkles: :lipstick: feat: generating component and displaying on website

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-italanta',
+  templateUrl: './elewa-brands-italanta.component.html',
+  styleUrls: ['./elewa-brands-italanta.component.scss'],
+})
+export class ElewaBrandsItalantaComponent {}
