af6a6fb529f1b1e45d14966421a43b605e7f17aa
Snow <onyangogeorge954@gmail.com>
2023-03-02 08:33:33 +0300

    rename component to match technical instructions

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-creative-hub/elewa-brands-creative-hub.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-creative-hub/elewa-brands-creative-hub.component.ts
@@ -1,11 +1,11 @@
 import { Component,Input } from '@angular/core';
 
 @Component({
-  selector: 'elewa-group-brands-creative-hub',
-  templateUrl: './brands-creative-hub.component.html',
-  styleUrls: ['./brands-creative-hub.component.scss'],
+  selector: 'elewa-group-elewa-brands-creative-hub',
+  templateUrl: './elewa-brands-creative-hub.component.html',
+  styleUrls: ['./elewa-brands-creative-hub.component.scss'],
 })
-export class BrandsCreativeHubComponent {
+export class ElewaBrandsCreativeHubComponent {
   @Input() creativeImage = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/CreativeHub_r5pshe.svg'
   @Input() creativeName = 'Elewa Creative Hub'
   @Input() creativeDescription = 'Elewa Creative Hub is a two-acre space, located in the heart of Nairobi, offering refuge to Elewa talents and creatives to explore and grow'
--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -9,19 +9,18 @@ import { ElewaBrandsOpportunitiesComponent } from './components/elewa-brands-opp
 import { ElewaBrandsPageComponent } from './pages/elewa-brands-page/elewa-brands-page/elewa-brands-page.component';
 
 import { BrandsRoutingModule } from './brands.routing';
-import { BrandsCreativeHubComponent } from './components/brands-creative-hub/brands-creative-hub.component';
 import { ElewaBrandsStakeholderComponent } from './components/elewa-brands-stakeholder/elewa-brands-stakeholder.component';
 import { ElewaBrandsHeroComponent } from './components/elewa-brands-hero/elewa-brands-hero.component';
+import { ElewaBrandsCreativeHubComponent } from './components/elewa-brands-creative-hub/elewa-brands-creative-hub.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, BannersModule, BrandsRoutingModule],
   declarations: [
     ElewaBrandsPageComponent,
     ElewaBrandsOpportunitiesComponent,
-    BrandsCreativeHubComponent,
     ElewaBrandsStakeholderComponent,
     ElewaBrandsHeroComponent,
-
+    ElewaBrandsCreativeHubComponent,
   ],
 })
 export class BrandsModule {}
