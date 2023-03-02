77889e821f9a5c9be1f3138706ee3444361eb77e
nziokikevin <kevinnzioki10@gmail.com>
2023-03-02 10:38:55 +0300

    :art: code: adding relevant data to the reusable component

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-italanta/elewa-brands-italanta.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './elewa-brands-italanta.component.html',
   styleUrls: ['./elewa-brands-italanta.component.scss'],
 })
-export class ElewaBrandsItalantaComponent {}
+export class ElewaBrandsItalantaComponent {
+  logoPlacement: 'right' | 'left' = 'right';
+  logo =
+    'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690298/elewa-group-website/Icons/SVG/Italanta_mpxz8d.svg';
+  brandDescription =
+    'From Silicon Savannah to the world, iTalanta is an offshore tech development agency that doubles as a tech talent accelerator. iTalanta has a portfolio of clients consisting out of some of the hottest global startups.';
+  buttonText = 'Visit website'
+  name = "ITalanta"
+}
