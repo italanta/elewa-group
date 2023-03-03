b9cbffc3834ed725d86e50a9d996e79043955769
Snow <onyangogeorge954@gmail.com>
2023-03-03 10:51:16 +0300

    add design for web and tablet

--- libs/pages/elewa/contact/src/lib/components/contact-banner/contact-banner.component.ts
+++ libs/pages/elewa/contact/src/lib/components/contact-banner/contact-banner.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-contact-banner',
   templateUrl: './contact-banner.component.html',
   styleUrls: ['./contact-banner.component.scss'],
 })
-export class ContactBannerComponent {}
+export class ContactBannerComponent {
+@Input() title = 'Contact';
+@Input() questionEmail = 'info@elewagroup.com';
+@Input() pressEmail = 'press@elewa.com';
+@Input() mobile = '+254 78 92 27 755'
+}
