773bf7b551ad9fac48e053fdaccaddb798bb7f53
Snow <onyangogeorge954@gmail.com>
2023-03-03 15:09:45 +0300

    create a handle submit function

--- libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
+++ libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'elewa-group-contact-form',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
 export class ContactFormComponent {
   message = "Send Message"
   url = "contact"
+  contactData:FormGroup
+
+  handleSubmit(){
+    console.log(this.contactData.value)
+  }
  
 }
