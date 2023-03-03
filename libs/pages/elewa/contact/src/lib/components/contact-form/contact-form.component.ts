1a71c7858f71ee05a482716f55b7da0a09d63bbf
marcokumu <marcokumu10@gmail.com>
2023-03-03 12:54:11 +0000

    ♻️refactor: moved the logic a function "buildContactForm" and called function in ngOnInit

--- libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
+++ libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'elewa-group-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent {
-  message1 = "Send message"
+export class ContactFormComponent implements OnInit{
+  message1 = 'Send message';
+
   contactForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildContactForm();
+  }
+
+  buildContactForm(): void {
     this.contactForm = this.formBuilder.group({
       name: '',
       email: '',
