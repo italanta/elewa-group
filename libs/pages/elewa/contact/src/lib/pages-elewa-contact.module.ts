6917f114dcfd05a5597eab40def5dff6db2010ac
marcokumu <marcokumu10@gmail.com>
2023-03-03 12:42:06 +0000

    📦️chore: changed order of imports

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonsModule } from '@elewa-group/features/components/buttons';
-import { LayoutModule} from '@elewa-group/elements/layout';
+import { LayoutModule } from '@elewa-group/elements/layout';
 
 @NgModule({
   imports: [
