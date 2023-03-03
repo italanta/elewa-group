986d3329b51b144941e99a09c36fe0752da09c49
Snow <onyangogeorge954@gmail.com>
2023-03-03 14:40:39 +0300

    add contact form component

---
+++ libs/pages/elewa/contact/src/lib/components/contact-form/contact-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss'],
+})
+export class ContactFormComponent {}
--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContactBannerComponent } from './components/contact-banner/contact-banner.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent, ContactBannerComponent],
+  declarations: [
+    ContactPageComponent,
+    ContactBannerComponent,
+    ContactFormComponent,
+  ],
 })
 export class ContactPageModule {}
