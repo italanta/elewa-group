6b9b4d26faea81e80f26b3de30913251ce49cbc8
nziokikevin <kevinnzioki10@gmail.com>
2023-03-06 10:56:24 +0300

    :art: feat: generating venture-labs-discover component

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [VentureLabsDiscoverComponent],
 })
 export class PagesElewaVentureLabsModule {}
