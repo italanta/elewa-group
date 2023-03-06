05eccd4569068df1ad534de288f9e9362a3193fb
Kangonga <guchu413@gmail.com>
2023-03-06 12:03:32 +0300

    feat: create the venture labs main page component

---
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages/venture-lab-detail-page/venture-lab-detail-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-venture-lab-detail-page',
+  templateUrl: './venture-lab-detail-page.component.html',
+  styleUrls: ['./venture-lab-detail-page.component.scss'],
+})
+export class VentureLabDetailPageComponent {}
