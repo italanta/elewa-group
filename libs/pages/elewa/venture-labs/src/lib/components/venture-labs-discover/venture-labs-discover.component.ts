4b58dc8d548bc79b7fec8a61baa5a22d00d923b7
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-03-06 19:21:53 +0300

    feat: pass data to parameters

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-discover/venture-labs-discover.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-discover/venture-labs-discover.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-venture-labs-discover',
   templateUrl: './venture-labs-discover.component.html',
   styleUrls: ['./venture-labs-discover.component.scss'],
 })
-export class VentureLabsDiscoverComponent {}
+export class VentureLabsDiscoverComponent {
+  @Input() message = ' Discover what VentureLabs';
+  @Input() callToActionText = 'can do for';
+  @Input() word = 'your Startup';
+  @Input() url = '/';
+}
