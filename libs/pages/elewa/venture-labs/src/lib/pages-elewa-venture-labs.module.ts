52e9a713e02dfbea767b1e6734ce2d2ee4cc9271
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-03-06 16:00:43 +0300

    :hammer:add venture labs info component

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabsInfoComponent } from './components/venture-labs-info/venture-labs-info.component';
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule],
-  declarations: [VentureLabsPageComponent],
+  imports: [CommonModule, VentureLabsRoutingModule, LayoutModule],
+  declarations: [VentureLabsPageComponent, VentureLabsInfoComponent],
 })
 export class PagesElewaVentureLabsModule {}
