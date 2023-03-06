27ab8995bf21b3b7eb5c6cb9b8a1d9fe9028f93f
kahoragachau <joelkahoragachau6@gmail.com>
2023-03-06 09:06:55 +0300

    ✨feat: Create Main Page component for venture Lab

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [VentureLabsPageComponent],
 })
 export class PagesElewaVentureLabsModule {}
