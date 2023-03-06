82f84317dadaa71c055231e5a01aa4e08c7d2025
Kangonga <guchu413@gmail.com>
2023-03-06 12:06:10 +0300

    feat: add imports to the venture labs module

--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
+
+import { VentureLabsRoutingModule } from './venture-labs.routing';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,LayoutModule, VentureLabsRoutingModule],
+  declarations: [VentureLabDetailPageComponent],
 })
 export class PagesElewaVentureLabDetailModule {}
