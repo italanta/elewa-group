b87dd9a5e2f26d7ced50d2c803bf3845b790d89f
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-03-06 19:23:39 +0300

    feat: imported [elewa-group-venture-labs-discover]

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule],
-  declarations: [VentureLabsPageComponent],
+  imports: [CommonModule, VentureLabsRoutingModule, LayoutModule, BannersModule],
+  declarations: [VentureLabsPageComponent, VentureLabsDiscoverComponent],
 })
 export class PagesElewaVentureLabsModule {}
