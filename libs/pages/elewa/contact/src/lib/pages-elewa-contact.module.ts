2c2ab612139bdd526136ea4925d454011d1f1de2
marcokumu <marcokumu10@gmail.com>
2023-03-06 23:52:40 +0000

    ✨feat: import the layoutmodule for header testing purposes

--- libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
+++ libs/pages/elewa/contact/src/lib/pages-elewa-contact.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { ContactRoutingModule } from './contact.routing';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ContactBannerComponent } from './components/contact-banner/contact-banner.component';
 
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule],
+  imports: [CommonModule, ContactRoutingModule, LayoutModule],
   declarations: [ContactPageComponent, ContactBannerComponent],
 })
 export class ContactPageModule {}
