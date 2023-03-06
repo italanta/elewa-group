060f281ca3b2165d157a8a9ec0c8d490ff9a4e81
Kangonga <guchu413@gmail.com>
2023-03-06 12:04:14 +0300

    feat: create routing file and add routes for the venture labs page

---
+++ libs/pages/elewa/venture-lab-detail/src/lib/venture-labs.routing.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { VentureLabDetailPageComponent } from "./pages/venture-lab-detail-page/venture-lab-detail-page.component";
+
+
+export const ELEWA_VENTURE_LABS_ROUTES: Route[]= [
+
+    { 'path':'',component:VentureLabDetailPageComponent }
+
+]
+
+@NgModule({
+    imports:[RouterModule.forChild(ELEWA_VENTURE_LABS_ROUTES)],
+    exports:[RouterModule]
+})
+export class VentureLabsRoutingModule { }
