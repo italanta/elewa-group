a6b9e827714dd424a2ea1e0cf49c4f0818ac427a
Kangonga <guchu413@gmail.com>
2023-03-07 10:28:36 +0300

    feat: create the component logic.

--- libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Article,data } from './article-interface';
 @Component({
   selector: 'elewa-group-elewa-group-article-list',
   templateUrl: './elewa-group-article-list.component.html',
   styleUrls: ['./elewa-group-article-list.component.scss'],
 })
-export class ElewaGroupArticleListComponent {}
+export class ElewaGroupArticleListComponent {
+  articleList:Article[] = data
+  generateReadTime(article:string){
+    const length = Number(article.split(' ').length)
+    const time = Math.ceil(length/200)
+    return time
+  }
+}
