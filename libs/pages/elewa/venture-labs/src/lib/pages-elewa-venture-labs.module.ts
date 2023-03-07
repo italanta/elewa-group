0208ba9f2fc0db68bb5c9ac1ca5010ae1702e758
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-03-07 11:17:40 +0300

    :sparkles: feat: created component

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
 import { BannersModule } from '@elewa-group/features/components/banners';
+import { VentureLabsHeroComponent } from './components/venture-labs-hero/venture-labs-hero.component';
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule,BannersModule],
-  declarations: [VentureLabsPageComponent,VentureLabsDiscoverComponent],
+  imports: [
+    CommonModule,
+    VentureLabsRoutingModule,
+    LayoutModule,
+    BannersModule,
+  ],
+  declarations: [
+    VentureLabsPageComponent,
+    VentureLabsDiscoverComponent,
+    VentureLabsHeroComponent,
+  ],
 })
 export class PagesElewaVentureLabsModule {}
