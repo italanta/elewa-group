3d93fc86a2770d374631eac88cd810e76d3ee7f1
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-03-07 15:49:50 +0300

    :add venture startup component

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabsStartupsComponent } from './components/venture-labs-startups/venture-labs-startups.component';
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule],
-  declarations: [VentureLabsPageComponent],
+  imports: [CommonModule, VentureLabsRoutingModule, LayoutModule],
+  declarations: [VentureLabsPageComponent, VentureLabsStartupsComponent],
 })
 export class PagesElewaVentureLabsModule {}
