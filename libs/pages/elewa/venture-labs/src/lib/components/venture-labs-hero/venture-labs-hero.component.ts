bed81b71380e9c0d69f999cfe605e9f3c465478c
Yego Baroni <yegokipkemoi254@gmail.com>
2023-03-07 18:39:13 +0300

    feat:401:venture-labs-top-banner
    created venture labs top banner

---
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-hero/venture-labs-hero.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-venture-labs-hero',
+  templateUrl: './venture-labs-hero.component.html',
+  styleUrls: ['./venture-labs-hero.component.scss'],
+})
+export class VentureLabsHeroComponent {
+  subtitle = 'About';
+  title = 'Sustainable business meets moonshot Venture Labs';
+  backgroundImg = 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690293/elewa-group-website/hero-Images/Venture_labs_y4tnjx.jpg';
+  footerText = '';
+}
