560e75dcdcb9e09bbc32ed2a27ac49ed040f851c
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2023-03-07 08:30:41 -0500

    🔨feat: adding founders section in venture details

--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LayoutModule} from '@elewa-group/elements/layout';
-import { VentureLabDetailPageComponent} from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
-import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing'
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
+import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing';
+import { VentureDetailFoundersComponent } from './venture-detail-founders/venture-detail-founders.component';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule,VentureLabDetailRoutingModule],
+  imports: [CommonModule, LayoutModule, VentureLabDetailRoutingModule],
 
-  declarations: [VentureLabDetailPageComponent],
-
+  declarations: [VentureLabDetailPageComponent, VentureDetailFoundersComponent],
 })
 export class PagesElewaVentureLabDetailModule {}
