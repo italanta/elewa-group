538dc8e52de6bcf5a12a649507b4c8e4cf41f5c6
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-07 17:23:09 +0300

    #409:art:import the banners module

--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { BannersModule } from '@elewa-group/features/components/banners';
+
 import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
 import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing';
 import { VentureDetailHeroComponent } from './components/venture-detail-hero/venture-detail-hero.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, VentureLabDetailRoutingModule],
+  imports: [CommonModule, LayoutModule, BannersModule, VentureLabDetailRoutingModule],
 
   declarations: [VentureLabDetailPageComponent, VentureDetailHeroComponent],
 })
