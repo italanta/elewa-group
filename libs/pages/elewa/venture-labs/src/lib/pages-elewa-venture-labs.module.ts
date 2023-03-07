2289f7ba8af5dd367383666c183045f77bcf0603
Rajab Karume <rkarume@gmail.com>
2023-03-07 11:47:14 +0300

    ✨created venture labs startup component

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
 import { BannersModule } from '@elewa-group/features/components/banners';
+import { VentureLabsStartupsComponent } from './components/venture-labs-startups/venture-labs-startups.component';
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule,BannersModule],
-  declarations: [VentureLabsPageComponent,VentureLabsDiscoverComponent],
+  imports: [
+    CommonModule,
+    VentureLabsRoutingModule,
+    LayoutModule,
+    BannersModule,
+  ],
+  declarations: [
+    VentureLabsPageComponent,
+    VentureLabsDiscoverComponent,
+    VentureLabsStartupsComponent,
+  ],
 })
 export class PagesElewaVentureLabsModule {}
