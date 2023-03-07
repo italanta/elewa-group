bab803fd289b170507243bd4d57a7e13feee5db5
Kangonga <guchu413@gmail.com>
2023-03-07 11:46:00 +0300

    create generateReadTime component and use input

--- libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
-import { Article,data } from './article-interface';
+import { Article } from './article-interface';
 @Component({
   selector: 'elewa-group-elewa-group-article-list',
   templateUrl: './elewa-group-article-list.component.html',
   styleUrls: ['./elewa-group-article-list.component.scss'],
 })
 export class ElewaGroupArticleListComponent {
-  articleList:Article[] = data
+  @Input() articleList:Article[]
+  
   generateReadTime(article:string){
-    const length = Number(article.split(' ').length)
-    const time = Math.ceil(length/200)
+    const words_length = Number(article.split(' ').length)
+    const time = Math.ceil(words_length/200)
     return time
   }
 }
