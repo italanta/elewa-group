cecc62eb16c3dd6b1cf3683180e6b688c39c6a77
calvin <ckodalo@gmail.com>
2023-03-07 07:03:49 +0300

    edit

--- libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
@@ -1,31 +1,20 @@
 import { Component, Input } from '@angular/core';
-
 //import this interface in the component you will be reusing article-list in. e.g
 //import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component';
-
 export interface Article {
   image: string;
   timestamp: string;
   title: string;
   body: string;
 }
-
 @Component({
   selector: 'elewa-group-elewa-group-article-list',
   templateUrl: './elewa-group-article-list.component.html',
   styleUrls: ['./elewa-group-article-list.component.scss'],
 })
-export class ElewaGroupArticleListComponent {
-
-  @Input() articles: Article[
-
-    
-  ];
-
-
-  currentIndex = 0;
-
-
+export class ElewaGroupArticleListComponent {  
+@Input() articles: Article[];
+ currentIndex = 0;
   calculateReadTime(body: string): number {
     const wordCount = body.split(' ').length;
     const readTime = Math.round(wordCount / 200);
@@ -53,5 +42,4 @@ export class ElewaGroupArticleListComponent {
       });
     }
   }
-  
-}
+  }
