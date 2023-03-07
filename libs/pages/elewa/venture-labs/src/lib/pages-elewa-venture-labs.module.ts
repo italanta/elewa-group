40ad500295679a8dbbba9ecfefbd9156a34eb11f
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-03-08 00:04:47 +0300

    🎨 feat: Imported  venture lab startups component and corrected order of imports

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
-import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout'
-import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
+import { LayoutModule } from '@elewa-group/elements/layout';
 import { BannersModule } from '@elewa-group/features/components/banners';
+import { CardsModule } from '@elewa-group/features/components/cards'
+
+
+
+import { VentureLabsStartupsComponent } from './components/venture-labs-startups/venture-labs-startups.component';
+import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
+
+import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
+
+import { VentureLabsRoutingModule } from './venture-labs.routing';
+
+
+
 
 @NgModule({
-  imports: [CommonModule,VentureLabsRoutingModule,LayoutModule,BannersModule],
-  declarations: [VentureLabsPageComponent,VentureLabsDiscoverComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    BannersModule,
+    CardsModule,
+    VentureLabsRoutingModule,
+  ],
+  declarations: [
+    VentureLabsPageComponent,
+    VentureLabsDiscoverComponent,
+    VentureLabsStartupsComponent,
+  ],
 })
 export class PagesElewaVentureLabsModule {}
