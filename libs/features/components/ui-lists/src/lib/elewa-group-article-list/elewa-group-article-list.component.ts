fb0936ea2d61bf5f394897ca49eabdbce2901c42
Kangonga <guchu413@gmail.com>
2023-03-07 08:24:58 +0300

    feat: create the component

---
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-group-article-list',
+  templateUrl: './elewa-group-article-list.component.html',
+  styleUrls: ['./elewa-group-article-list.component.scss'],
+})
+export class ElewaGroupArticleListComponent {}
