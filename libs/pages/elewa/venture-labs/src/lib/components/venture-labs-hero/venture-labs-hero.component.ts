ab00086e08462a785a56351110ec606b1d51cafa
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-03-07 11:18:58 +0300

    :sparkles: feat: added details section

---
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-hero/venture-labs-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-venture-labs-hero',
+  templateUrl: './venture-labs-hero.component.html',
+  styleUrls: ['./venture-labs-hero.component.scss'],
+})
+export class VentureLabsHeroComponent {
+  backgroundImage="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690293/elewa-group-website/hero-Images/Venture_labs_y4tnjx.jpg"
+  subtitle= "About"
+  title="Sustainable business meets moonshot Venture Labs"
+  footer=""}
