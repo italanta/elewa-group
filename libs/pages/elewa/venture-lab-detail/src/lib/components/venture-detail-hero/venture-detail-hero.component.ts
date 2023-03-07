9abbbef949a4cb1ec582fa873b1a4b935898f3a5
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-03-07 17:54:10 +0300

    :hammer:add elewa detail hero component

---
+++ libs/pages/elewa/venture-lab-detail/src/lib/components/venture-detail-hero/venture-detail-hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-venture-detail-hero',
+  templateUrl: './venture-detail-hero.component.html',
+  styleUrls: ['./venture-detail-hero.component.scss'],
+})
+export class VentureDetailHeroComponent {}
--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LayoutModule} from '@elewa-group/elements/layout';
-import { VentureLabDetailPageComponent} from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
-import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing'
+import { LayoutModule } from '@elewa-group/elements/layout';
+import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing';
+import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
+import { VentureDetailHeroComponent } from './components/venture-detail-hero/venture-detail-hero.component';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule,VentureLabDetailRoutingModule],
+  imports: [CommonModule, LayoutModule, VentureLabDetailRoutingModule],
 
-  declarations: [VentureLabDetailPageComponent],
-
+  declarations: [VentureLabDetailPageComponent, VentureDetailHeroComponent],
 })
 export class PagesElewaVentureLabDetailModule {}
