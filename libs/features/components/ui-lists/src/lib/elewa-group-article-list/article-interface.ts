7798dc8bab28f2f65afec46fbef1a91e135469a7
Kangonga <guchu413@gmail.com>
2023-03-07 10:26:47 +0300

    feat: add interface

---
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/article-interface.ts
@@ -0,0 +1,27 @@
+export interface Article {
+    image:string;
+    timestamp:string;
+    title:string;
+    body:string;
+}
+
+export const data:Article[] = [
+    {
+        image:"https://unsplash.com/photos/bzqU01v-G54",
+        timestamp:"12 Oct 2022",
+        title: "Introducing a conversational learning",
+        body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat magna ipsum, eu lobortis lectus ultrices sit amet. Pellentesque sed felis a turpis malesuada vehicula egestas sed justo. Donec quis finibus libero, sed mattis odio. Vestibulum id tincidunt nulla, et tincidunt tortor. Sed et faucibus tellus. Aenean maximus tellus ut condimentum rhoncus. Quisque a hendrerit lectus. Vestibulum hendrerit risus tellus, a efficitur nibh sodales id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+        image:"",
+        timestamp:"12 Oct 2022",
+        title: "Introducing a conversational learning",
+        body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat magna ipsum, eu lobortis lectus ultrices sit amet. Pellentesque sed felis a turpis malesuada vehicula egestas sed justo. Donec quis finibus libero, sed mattis odio. Vestibulum id tincidunt nulla, et tincidunt tortor. Sed et faucibus tellus. Aenean maximus tellus ut condimentum rhoncus. Quisque a hendrerit lectus. Vestibulum hendrerit risus tellus, a efficitur nibh sodales id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+        image:"",
+        timestamp:"12 Oct 2022",
+        title: "Introducing a conversational learning",
+        body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat magna ipsum, eu lobortis lectus ultrices sit amet. Pellentesque sed felis a turpis malesuada vehicula egestas sed justo. Donec quis finibus libero, sed mattis odio. Vestibulum id tincidunt nulla, et tincidunt tortor. Sed et faucibus tellus. Aenean maximus tellus ut condimentum rhoncus. Quisque a hendrerit lectus. Vestibulum hendrerit risus tellus, a efficitur nibh sodales id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+]
