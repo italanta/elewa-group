bfb699c014395f622538f9a1d405aaa49023f994
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-08 10:03:36 +0300

    #409:art:Import the buttons module

--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
-import { BannersModule } from '@elewa-group/features/components/banners';
+import { ButtonsModule } from '@elewa-group/features/components/buttons'; 
 
 import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
 import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing';
 import { VentureDetailHeroComponent } from './components/venture-detail-hero/venture-detail-hero.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, BannersModule, VentureLabDetailRoutingModule],
+  imports: [CommonModule, LayoutModule, ButtonsModule, VentureLabDetailRoutingModule],
 
   declarations: [VentureLabDetailPageComponent, VentureDetailHeroComponent],
 })
