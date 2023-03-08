5659305f2beb26703c8eddd3d59253dcdc5d038b
Michael-Otieno <otieno.oduor.michael@gmail.com>
2023-03-08 03:41:34 +0300

    :hammer:import ButtonsModeule

--- libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/pages-elewa-venture-lab-detail.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { VentureLabDetailRoutingModule } from './venture-lab-detail.routing';
+import { ButtonsModule } from '@elewa-group/features/components/buttons';
 import { VentureLabDetailPageComponent } from './pages/venture-lab-detail-page/venture-lab-detail-page.component';
 import { VentureDetailHeroComponent } from './components/venture-detail-hero/venture-detail-hero.component';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, VentureLabDetailRoutingModule],
+  imports: [CommonModule, LayoutModule, VentureLabDetailRoutingModule,ButtonsModule],
 
   declarations: [VentureLabDetailPageComponent, VentureDetailHeroComponent],
 })
