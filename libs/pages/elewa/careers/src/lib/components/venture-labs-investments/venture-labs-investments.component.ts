f38d8323d38456e3f3b0db110a630ebddc5c0ef6
Ashley Njoroge <ashleynjeri7@gmail.com>
2023-03-08 14:56:53 +0300

    :sparkles: feat: page resources

---
+++ libs/pages/elewa/careers/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
@@ -0,0 +1,82 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-venture-labs-investments',
+  templateUrl: './venture-labs-investments.component.html',
+  styleUrls: ['./venture-labs-investments.component.scss'],
+})
+export class VentureLabsInvestmentsComponent {
+  figures = [
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    },
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    }, 
+    {
+      Logo: "https://i.postimg.cc/wxnQLbYY/nextsteps.png"
+    }
+  ]
+  
+  // figures = [
+  //   {
+  //     string: "1.3M",
+  //     description: "Approx. number of Kenyan talents reaching the age of 18 in 2022",
+
+  //   },
+  //   {
+  //     string: "5M",
+  //     description: "number of East-Africans(EAC) reaching the age of 18 in 2022",
+
+  //   },
+  //   {
+  //     string: "2022",
+  //     description: "Democratic Republic of Congo joins the East African Community",
+
+  //   },
+  //   {
+  //     string: "1.5M",
+  //     description: "The number of unfilled IT vacancies in the US and EU markets",
+
+  //   },
+  //   {
+  //     string: "10M",
+  //     description: "Number of unified technical vacancies in US and EU markets",
+
+  //   },
+  //   {
+  //     string: "7.5%",
+  //     description: "Kenya 2021 GDP growth",
+
+  //   },
+  //   {
+  //     string: "$4.6",
+  //     description: "Startup funding rised by African startups in 2022",
+
+  //   },
+  //   {
+  //     string: "310%",
+  //     description: "Increase in startup funding for companies with HQ in Kenya",
+
+  //   }
+  // ]
+
+
+  // @Input () logo ="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/CreativeHub_r5pshe.svg"
+
+}
