4eac691af8baa82e5491dd790d6c0a9ceada20bc
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-08 10:41:32 +0300

    #409:art:re-use the banner component

--- libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
+++ libs/features/components/banners/src/lib/banners/elewa-group-image-and-text-banner/elewa-group-image-and-text-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input,} from '@angular/core';
 
 @Component({
   selector: 'elewa-group-elewa-group-image-and-text-banner',
--- libs/pages/elewa/venture-lab-detail/src/lib/components/venture-detail-hero/venture-detail-hero.component.ts
+++ libs/pages/elewa/venture-lab-detail/src/lib/components/venture-detail-hero/venture-detail-hero.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './venture-detail-hero.component.html',
   styleUrls: ['./venture-detail-hero.component.scss'],
 })
-export class VentureDetailHeroComponent {}
+export class VentureDetailHeroComponent {
+  paragraphs = [' The secret to successful education sequi nesciunt. Neque poro quisquam dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum']
+  showButton = true;
+}
