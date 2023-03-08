13e6e93dba000bb9bed1b125cd9b7a885fa4d572
Rajab Karume <rkarume@gmail.com>
2023-03-08 09:05:31 +0300

    🐛fixed merge conflicts

--- libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
+++ libs/pages/elewa/venture-labs/src/lib/pages-elewa-venture-labs.module.ts
@@ -10,9 +10,6 @@ import { VentureLabsDiscoverComponent } from './components/venture-labs-discover
 import { VentureLabsPageComponent } from './pages/venture-labs-page/venture-labs-page.component';
 
 import { VentureLabsRoutingModule } from './venture-labs.routing';
-import { LayoutModule } from '@elewa-group/elements/layout';
-import { VentureLabsDiscoverComponent } from './components/venture-labs-discover/venture-labs-discover.component';
-import { BannersModule } from '@elewa-group/features/components/banners';
 import { VentureLabsStartupsComponent } from './components/venture-labs-startups/venture-labs-startups.component';
 
 @NgModule({
@@ -20,7 +17,7 @@ import { VentureLabsStartupsComponent } from './components/venture-labs-startups
     CommonModule,
     VentureLabsRoutingModule,
     LayoutModule,
-    BannersModule,
+    BannersModule
   ],
   declarations: [
     VentureLabsPageComponent,
