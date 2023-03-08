6bf8d0c05be3fbdbab3cc6977f7ff6f2ad611af8
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-08 12:46:45 +0300

    #404:lipstick:style the component

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
@@ -9,7 +9,7 @@ export class VentureLabsInvestmentsComponent {
 isimage = true
 cards = [
   {
-    figures: 'https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHVwd29yayUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+    string: 'https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHVwd29yayUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
   },
   {
     figures: '',
