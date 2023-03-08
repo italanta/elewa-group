a85cd672f7f8b392c8805bbb98c3c2bd073939b7
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-08 12:14:02 +0300

    #404:art:design the html file

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./venture-labs-investments.component.scss'],
 })
 export class VentureLabsInvestmentsComponent {
-
+isimage = true
+cards = [
+  {
+    figures: 'https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHVwd29yayUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  },
+  {
+    figures: '',
+  }
+]
 }
