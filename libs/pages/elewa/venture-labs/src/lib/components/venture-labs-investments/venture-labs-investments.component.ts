c1a56bfd4a7a9f98c238103a3fc231b1e68ffbb6
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-08 14:29:15 +0300

    #404:lipstick:add media queries

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-investments/venture-labs-investments.component.ts
@@ -6,31 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./venture-labs-investments.component.scss'],
 })
 export class VentureLabsInvestmentsComponent {
-isimage = true
 cards = [
   {
-    string: 'https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHVwd29yayUyMGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+    figure: 'https://i.postimg.cc/wxnQLbYY/nextsteps.png',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   },
   {
-    figures: '',
+    figure: '',
   }
 ]
 }
