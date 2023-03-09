b7daba11503a177c6d920b0b675237c5df548223
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-09 11:28:28 +0300

    #443:art:create the article properties

--- libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
+++ libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
@@ -8,5 +8,44 @@ import { Component } from '@angular/core';
 export class ElewaNewsSectionComponent {
   activeTab = 'all';
 
+  articles = [
+    {
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      image: 'https://picsum.photos/id/1018/800/600',
+      srcset: 'https://picsum.photos/id/1018/400/300 400w, https://picsum.photos/id/1018/800/600 800w',
+      category: 'world',
+      timestamp: new Date()
+    },
+    {
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      image: 'https://picsum.photos/id/1018/800/600',
+      srcset: 'https://picsum.photos/id/1018/400/300 400w, https://picsum.photos/id/1018/800/600 800w',
+      category: 'world',
+      timestamp: new Date()
+    },
+    {
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      image: 'https://picsum.photos/id/1018/800/600',
+      srcset: 'https://picsum.photos/id/1018/400/300 400w, https://picsum.photos/id/1018/800/600 800w',
+      category: 'world',
+      timestamp: new Date()
+    },
+    {
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      image: 'https://picsum.photos/id/1018/800/600',
+      srcset: 'https://picsum.photos/id/1018/400/300 400w, https://picsum.photos/id/1018/800/600 800w',
+      category: 'world',
+      timestamp: new Date()
+    },
+  ]
+
+  get filteredArticles() {
+    return this.articles.filter(article => article.category === this.activeTab);
+    }
+    
   
 }
