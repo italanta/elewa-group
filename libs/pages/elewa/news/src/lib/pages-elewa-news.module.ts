12215c6b5f0612375225cadd0aa155351f1c42b7
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-03-09 13:11:47 +0300

    feat: import news related component

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
-
+import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
+  declarations: [NewsPageComponent, NewsRelatedArticlesComponent],
 })
 export class NewsPageModule {}
