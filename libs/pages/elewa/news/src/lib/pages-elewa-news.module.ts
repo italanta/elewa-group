3a50ec12f62862732d089ea71bd66d748250c048
Rajab Karume <rkarume@gmail.com>
2023-03-09 12:19:47 +0300

    ✨moved component to news-details

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
-import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
 
 @NgModule({
   imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent, NewsRelatedArticlesComponent],
+  declarations: [NewsPageComponent],
 })
 export class NewsPageModule {}
