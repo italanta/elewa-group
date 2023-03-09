4a682eb2ad845a80963f32603ccd3f62008fea0b
Kangonga <guchu413@gmail.com>
2023-03-09 10:27:41 +0300

    feat: import necessary modules

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  imports: [CommonModule,UiListsModule, NewsRoutingModule, LayoutModule],
+  declarations: [NewsPageComponent, NewsRelatedArticlesComponent],
 })
 export class NewsPageModule {}
