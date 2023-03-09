6b6d9b994f5a6291ae1427b40724fc6333613b79
Kangonga <guchu413@gmail.com>
2023-03-09 10:26:45 +0300

    feat: add articles data

--- libs/pages/elewa/news/src/lib/components/news-related-articles/news-related-articles.component.ts
+++ libs/pages/elewa/news/src/lib/components/news-related-articles/news-related-articles.component.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './news-related-articles.component.html',
   styleUrls: ['./news-related-articles.component.scss'],
 })
-export class NewsRelatedArticlesComponent {}
+export class NewsRelatedArticlesComponent {
+
+  articles = [
+    {
+      image :"https://plus.unsplash.com/premium_photo-1661843643974-051b435f80ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+      timestamp : "20 0ct 2022",
+      title : "Introducing Conversational learning ipsom dolar",
+      body:""
+    },
+    {
+      image :"https://plus.unsplash.com/premium_photo-1661843643974-051b435f80ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+      timestamp : "20 0ct 2022",
+      title : "Introducing Conversational learning ipsom dolar",
+      body:""
+    },
+    {
+      image :"https://plus.unsplash.com/premium_photo-1661843643974-051b435f80ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+      timestamp : "20 0ct 2022",
+      title : "Introducing Conversational learning ipsom dolar",
+      body:""
+    },
+  ]
+}
