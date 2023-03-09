b4db3cf67fe8585af74b06e556ac865ed6d87349
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-09 11:16:01 +0300

    #443:art:import the UiListModule

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 import { NewsRoutingModule } from './news.routing';
+
 import { ElewaNewsSectionComponent } from './components/elewa-news-section/elewa-news-section.component';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
   declarations: [NewsPageComponent, ElewaNewsSectionComponent],
 })
 export class NewsPageModule {}
