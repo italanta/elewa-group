58672ec3e420aa553d07bf9e472c6f656d38d944
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-09 13:15:41 +0300

    :fire: :feat: added data and rendered the page

--- libs/pages/elewa/news-detail/src/lib/pages/news-detail-page/news-related-articles/news-related-articles.component.ts
+++ libs/pages/elewa/news-detail/src/lib/pages/news-detail-page/news-related-articles/news-related-articles.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'elewa-group-news-related-articles',
   templateUrl: './news-related-articles.component.html',
-  styleUrls: ['./news-related-articles.component.css']
+  styleUrls: ['./news-related-articles.component.scss']
 })
 export class NewsRelatedArticlesComponent {
 
