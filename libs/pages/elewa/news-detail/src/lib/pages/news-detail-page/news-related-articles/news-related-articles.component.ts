8c41fdc38065dbaf77440232b7514ee122b2189c
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-09 12:29:49 +0300

    :zap: :feat: generated component

---
+++ libs/pages/elewa/news-detail/src/lib/pages/news-detail-page/news-related-articles/news-related-articles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-news-related-articles',
+  templateUrl: './news-related-articles.component.html',
+  styleUrls: ['./news-related-articles.component.css']
+})
+export class NewsRelatedArticlesComponent {
+
+}
