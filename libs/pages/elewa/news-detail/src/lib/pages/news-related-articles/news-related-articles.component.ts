1e4164eda128451624e9f4785c757be0c8c44fe2
Phillip <ochiengphillip84@gmail.com>
2023-03-09 14:49:33 +0300

    ✨feat: add the code to the component

--- libs/pages/elewa/news-detail/src/lib/pages/news-related-articles/news-related-articles.component.ts
+++ libs/pages/elewa/news-detail/src/lib/pages/news-related-articles/news-related-articles.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Input, Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-news-related-articles',
   templateUrl: './news-related-articles.component.html',
   styleUrls: ['./news-related-articles.component.scss'],
 })
-export class NewsRelatedArticlesComponent {}
+export class NewsRelatedArticlesComponent {
+@Input() articles= [
+  {
+    image: "https://cdn.pixabay.com/photo/2023/03/02/03/01/bird-7824442__340.jpg",
+    title:"Introducing Conventional learning lorem ipsum dolar",
+    timestamp:"20 Oct 2022 ▪ 5 min read"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2023/03/02/03/01/bird-7824442__340.jpg",
+    title:"Introducing Conventional learning lorem ipsum dolar",
+    timestamp:"20 Oct 2022 ▪ 5 min read"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2023/03/02/03/01/bird-7824442__340.jpg",
+        title:"Introducing Conventional learning lorem ipsum dolar",
+        timestamp:"20 Oct 2022 ▪ 5 min read"
+    }
+]
+
+}
