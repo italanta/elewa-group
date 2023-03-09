62bec51aa06cdf9b5c6d13fc8b77097fe985e8ed
Calvin Ngugi <ncalvin67@gmail.com>
2023-03-09 14:17:09 +0300

    :art: Added data to article list component

--- libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
+++ libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-export interface Article { image: string; timestamp: string; title: string; body: string;} //import this interface in the component you will be reusing article-list in. e.g import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component';
+ //import this interface in the component you will be reusing article-list in. e.g import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component';
 
 @Component({
   selector: 'elewa-group-elewa-group-article-list',
@@ -8,7 +8,32 @@ export interface Article { image: string; timestamp: string; title: string; body
 })
 
 export class ElewaGroupArticleListComponent {  
-@Input() articles: Article[];
+@Input() articles = [
+  {
+    image: "https://media.gettyimages.com/id/sb10069524c-001/photo/south-africa-cape-town-hout-bay-group-portrait-of-boys-jumping-in-field.jpg?s=1024x1024&w=gi&k=20&c=PuvFkl-X2Zg9sv4Hgrgb8m5VdFY68HX0rRYiHgqV9AM=",
+    title: "Intoducing Controversial learning",
+    body: "dummy",
+    timestamp: "12 Oct 2022",
+  },
+  {
+    image: "https://media.gettyimages.com/id/1324869840/photo/agronomist-using-digital-tablet-for-analysis-of-plantation.jpg?s=1024x1024&w=gi&k=20&c=wVDuGrpZDEYfEX2CJl3WwzBPRJ8NYplUCS2H_kcn6Nk=",
+    title: "Re-watch the Italanta Academy Launch event",
+    body: "dummy",
+    timestamp: "08 Oct 2022",
+  },
+  {
+    image: "https://media.gettyimages.com/id/950514452/photo/engineer.jpg?s=1024x1024&w=gi&k=20&c=81AE7KM5HUg4g0xoRvbUWRC-x1s89cm0CTvIFsIeD1c=",
+    title: "Introducing Controversial",
+    body: "7 min read",
+    timestamp: "05 Oct 2022",
+  },
+  {
+    image: "https://media.gettyimages.com/id/sb10069524c-001/photo/south-africa-cape-town-hout-bay-group-portrait-of-boys-jumping-in-field.jpg?s=1024x1024&w=gi&k=20&c=PuvFkl-X2Zg9sv4Hgrgb8m5VdFY68HX0rRYiHgqV9AM=",
+    title: "Watch the Italanta Academy Launch event",
+    body: "dummy",
+    timestamp: "02 Oct 2022",
+  },
+];
 
 currentIndex = 0;
   calculateReadTime(body: string): number {
--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
   declarations: [NewsPageComponent],
 })
 export class NewsPageModule {}
