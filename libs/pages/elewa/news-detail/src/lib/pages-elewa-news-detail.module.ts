b2bed923def999a52f81ebbc14454dd942ffa7c6
Rajab Karume <rkarume@gmail.com>
2023-03-09 11:46:27 +0300

    ✨created news details article component

--- libs/pages/elewa/news-detail/src/lib/pages-elewa-news-detail.module.ts
+++ libs/pages/elewa/news-detail/src/lib/pages-elewa-news-detail.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
 
 import { NewsDetailPageComponent } from './pages/news-detail-page/news-detail-page.component';
+import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
 
 @NgModule({
   imports: [CommonModule, LayoutModule],
-  declarations: [NewsDetailPageComponent],
+  declarations: [NewsDetailPageComponent, NewsRelatedArticlesComponent],
 })
 export class ElewaNewsDetailModule {}
--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
 
 @NgModule({
   imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  declarations: [NewsPageComponent, NewsRelatedArticlesComponent],
 })
 export class NewsPageModule {}
