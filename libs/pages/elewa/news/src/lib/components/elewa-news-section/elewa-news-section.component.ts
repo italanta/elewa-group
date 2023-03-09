7a515cb65577e6a27ee684032f843c001994ed0c
Snow <onyangogeorge954@gmail.com>
2023-03-09 10:11:57 +0300

    create component

---
+++ libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-news-section',
+  templateUrl: './elewa-news-section.component.html',
+  styleUrls: ['./elewa-news-section.component.scss'],
+})
+export class ElewaNewsSectionComponent {}
