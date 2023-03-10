50c9f520047cdb48b66235318323f3366a3d5231
Yego Baroni <yegokipkemoi254@gmail.com>
2023-03-10 23:00:00 +0300

    #443:feat:news-page
    created news page feature

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { ElewaNewsSectionComponent } from './components/elewa-news-section/elewa-news-section.component';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
+  declarations: [NewsPageComponent, ElewaNewsSectionComponent],
 })
 export class NewsPageModule {}
