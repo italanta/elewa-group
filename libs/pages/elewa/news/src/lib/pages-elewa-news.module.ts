c64cbbb2b678e2dd923e6217c053e65ed02745a6
codertinie <martin.munyao@moringaschool.com>
2023-03-10 09:57:37 +0300

    ✨feat: Added the component to news-page component for proper editing

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { NewsRelatedArticlesComponent } from 'libs/pages/elewa/news-detail/src/lib/pages/news-related-articles/news-related-articles.component';
 
 @NgModule({
   imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  declarations: [NewsPageComponent, NewsRelatedArticlesComponent],
 })
 export class NewsPageModule {}
