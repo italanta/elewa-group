daa2ccfeb31d8f7926a00f34bea2a4f1cdb5b72a
+pauleen muthoni <+paugitonga6@gmail.com>
2023-03-11 11:57:47 +0300

    feat:created the news section component

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
+import { ElewaNewsSectionComponent } from './components/elewa-news-section/elewa-news-section.component';
 
 @NgModule({
   imports: [CommonModule, NewsRoutingModule, LayoutModule],
-  declarations: [NewsPageComponent],
+  declarations: [NewsPageComponent, ElewaNewsSectionComponent],
 })
 export class NewsPageModule {}
