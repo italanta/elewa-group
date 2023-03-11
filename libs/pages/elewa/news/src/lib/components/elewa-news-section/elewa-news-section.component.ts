54c4f2d14889f88a6f49840224fcb5c2b1a9e9f9
+pauleen muthoni <+paugitonga6@gmail.com>
2023-03-11 12:51:23 +0300

    docs:added the necessary code to my news section components

--- libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
+++ libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
@@ -1,8 +1,51 @@
 import { Component } from '@angular/core';
+import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component';
 
 @Component({
   selector: 'elewa-group-elewa-news-section',
   templateUrl: './elewa-news-section.component.html',
   styleUrls: ['./elewa-news-section.component.scss'],
 })
-export class ElewaNewsSectionComponent {}
+export class ElewaNewsSectionComponent {
+  news: Article []= [
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: new Date().toString(),
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'world',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: new Date().toString(),
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'world',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: new Date().toString(),
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'world',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: new Date().toString(),
+      title: 'Article 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'world',
+    },
+  ];
+  filteredArticles: Article[];
+  selectedTab: string;
+  constructor() {
+    // Default to "All News" tab
+    this.selectedTab = 'all-news';  //filter through the tabs after creating the filter method for the articles
+    this.filterArticles();
+  }
+  //method to filter articles
+  filterArticles() {
+    this.filteredArticles = this.news.filter(article => article.topic === this.selectedTab || this.selectedTab === 'all-news');
+  }
+}
