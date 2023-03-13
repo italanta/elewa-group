a54c846c2f12f39ec90957513d5b383528a8df1a
Vincent <makokhavomondi@gmail.com>
2023-03-13 18:44:12 +0300

    :sparkles: Generate the elewa-news-page component

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -4,9 +4,10 @@ import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
 import { UiListsModule } from '@elewa-group/features/components/ui-lists';
+import { ElewaNewsSectionComponent } from './components/elewa-news-section/elewa-news-section.component';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule, LayoutModule,UiListsModule],
-  declarations: [NewsPageComponent],
+  imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
+  declarations: [NewsPageComponent, ElewaNewsSectionComponent],
 })
 export class NewsPageModule {}
