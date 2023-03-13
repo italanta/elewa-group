3cfd0f0cb3089282f28f0b77e15e228f0970a436
kahoragachau <joelkahoragachau6@gmail.com>
2023-03-13 23:50:36 +0300

    ✨feat: Add Dummy data

--- libs/pages/elewa/news/src/lib/elewa-news-section/elewa-news-section.component.ts
+++ libs/pages/elewa/news/src/lib/elewa-news-section/elewa-news-section.component.ts
@@ -6,4 +6,91 @@ import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-a
   templateUrl: './elewa-news-section.component.html',
   styleUrls: ['./elewa-news-section.component.scss'],
 })
-export class ElewaNewsSectionComponent {}
+export class ElewaNewsSectionComponent { 
+  article: Article[] = [
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "7",
+      title: "Angular elewa Lifecycle methods 1",
+      topic: "elewa"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "7",
+      title: "Angular elewa Lifecycle methods 2",
+      topic: "elewa"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "7",
+      title: "Elewa Lifecycle methods 3",
+      topic: "elewa"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for press",
+      title: "press Lifecycle methods 1",
+      topic: "press"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for press",
+      title: "press Lifecycle methods 2",
+      topic: "press"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for press",
+      title: "press Lifecycle methods 3",
+      topic: "press"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "iTalanta Lifecycle methods 1",
+      topic: "iTalanta"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "iTalanta Lifecycle methods 2",
+      topic: "iTalanta"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "iTalanta Lifecycle methods 3",
+      topic: "iTalanta"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "Venture-labs Lifecycle methods 1",
+      topic: "venture-labs"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "Venture-labs Lifecycle methods 2",
+      topic: "venture-labs"
+    },
+    {
+      image: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png",
+      timestamp: "27 Nov 2022",
+      body: "this for iTalanta",
+      title: "Venture-labs Lifecycle methods 3",
+      topic: "venture-labs"
+    },
+  ]
+}
