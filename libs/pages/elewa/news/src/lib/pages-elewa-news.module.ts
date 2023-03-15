04c7d8896ef6322727852c3acae2d8ca80df5dfd
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2023-03-15 05:02:15 -0400

    🔧 fix: resolving conflict in news module ts

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { LayoutModule } from '@elewa-group/elements/layout';
 import { NewsRoutingModule } from './news.routing';
-import { NewsSectionComponent } from './components/news-section/news-section.component';
-import { ButtonPlainComponent } from './components/button-plain/button-plain.component';
 import { UiListsModule } from '@elewa-group/features/components/ui-lists';
+import { ButtonPlainComponent } from './components/button-plain/button-plain.component';
+import { NewsSectionComponent } from './components/news-section/news-section.component';
 
 @NgModule({
   imports: [CommonModule, NewsRoutingModule, LayoutModule, UiListsModule],
