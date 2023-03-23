60237a55bf84ed526cc2997118650157c2a7e083
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-03-23 09:58:28 +0300

    :art: news detail article

--- libs/pages/elewa/news-detail/src/lib/pages-elewa-news-detail.module.ts
+++ libs/pages/elewa/news-detail/src/lib/pages-elewa-news-detail.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@elewa-group/elements/layout';
 
-import { NewsDetailPageComponent } from './pages/news-detail-page/news-detail-page.component';
 import { NewsRelatedArticlesComponent } from './components/news-related-articles/news-related-articles.component';
-import { NewsArticleComponent } from './news-article/news-article.component';
+import { NewsArticleComponent } from './components/news-article/news-article.component';
+
+import { NewsDetailPageComponent } from './pages/news-detail-page/news-detail-page.component';
+
 import { NgxContentfulRichTextModule } from 'ngx-contentful-rich-text';
 
 @NgModule({
   imports: [CommonModule, LayoutModule, NgxContentfulRichTextModule ],
-  declarations: [NewsDetailPageComponent, NewsArticleComponent],
+  declarations: [NewsDetailPageComponent, NewsArticleComponent, NewsRelatedArticlesComponent],
 })
 
 export class ElewaNewsDetailModule {}
