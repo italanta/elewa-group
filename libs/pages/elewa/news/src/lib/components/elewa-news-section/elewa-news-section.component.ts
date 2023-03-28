2f98944610a51161922769bef173e7d9bf307f07
Pauline Muthoni <106158118+pauleen123@users.noreply.github.com>
2023-03-28 12:03:27 +0300

    feat:created an interface component and a loop for the tabs

--- libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
+++ libs/pages/elewa/news/src/lib/components/elewa-news-section/elewa-news-section.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-article-list/elewa-group-article-list.component';
+import {Article } from 'libs/features/components/ui-lists/src/lib/models/article.interface'
+// import {Article}
 
 @Component({
   selector: 'elewa-group-elewa-news-section',
@@ -7,36 +8,63 @@ import { Article } from 'libs/features/components/ui-lists/src/lib/elewa-group-a
   styleUrls: ['./elewa-news-section.component.scss'],
 })
 export class ElewaNewsSectionComponent {
-  news: Article []= [
+
+tabs = ["all-news", "elewa", "iTalanta", "venture-labs", "press"]
+
+  news: Article[]= [
     {
       image: 'https://picsum.photos/id/1018/800/600',
-      timestamp: new Date().toString(),
+      timestamp: "2017",
       title: 'Article 1',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
-      topic: 'world',
+      topic: 'elewa',
     },
     {
       image: 'https://picsum.photos/id/1018/800/600',
-      timestamp: new Date().toString(),
+      timestamp: "2017",
       title: 'Article 1',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
-      topic: 'world',
+      topic: 'elewa',
     },
     {
       image: 'https://picsum.photos/id/1018/800/600',
-      timestamp: new Date().toString(),
+      timestamp: "2017",
       title: 'Article 1',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
-      topic: 'world',
+      topic: 'elewa',
     },
     {
       image: 'https://picsum.photos/id/1018/800/600',
-      timestamp: new Date().toString(),
+      timestamp: "2017",
       title: 'Article 1',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
-      topic: 'world',
+      topic: 'press',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: "2017",
+      title: 'Article press',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'press',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: "2017",
+      title: 'Article press',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'press',
+    },
+    {
+      image: 'https://picsum.photos/id/1018/800/600',
+      timestamp: "2017",
+      title: 'Article press',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis tempor risus. Maecenas gravida felis a semper bibendum.',
+      topic: 'press',
     },
   ];
+  
+  
+
   filteredArticles: Article[];
   selectedTab: string;
   constructor() {
