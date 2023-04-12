5c8190388e409cac1388ec919a95274d4e98a617
Jente Rosseel <rosseeljente@gmail.com>
2023-04-12 11:30:23 +0300

    :pencil: Refine copy on about-us page

--- libs/pages/elewa/about-us/src/lib/components/about-us-we-care/about-us-we-care.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/about-us-we-care/about-us-we-care.component.ts
@@ -9,8 +9,8 @@ import * as AOS from 'aos';
 })
 export class AboutUsWeCareComponent implements OnInit {
   title = "We Care!"
-  paragraph1 = "Elewa is a mission-driven organization. We make use of our cooperative and shared culture to drive the needle for the development of people and our environment.We care for our own, but also care deeply about the context surrounding us."
-  paragraph2 = " Our investments are therefore not limited to internal ones but contribure heavily to our community and environment.From training the next scout leaders on sustainable practices, to bridging the employment gap for junior software developers."
+  paragraph1 = "Elewa is a mission-driven organization. We make use of our cooperative and shared culture to drive the needle for the development of people and environment. We care about our growth, but also care deeply about the context surrounding us."
+  paragraph2 = " Our investments are therefore not limited to internal ones, but contribute heavily to our community and environment. From training the next scout leaders on sustainable practices, to bridging the employment gap for junior software developers."
   columns = [
     {
       icon: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1680180166/elewa-group-website/Icons/PNG/Holistic_118_ylipr0.png",
@@ -25,7 +25,7 @@ export class AboutUsWeCareComponent implements OnInit {
     {
       icon: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690299/elewa-group-website/Icons/PNG/Opendata_fe7h3j.png",
       title: "Open data",
-      description: "Sharing is caring. We share what we learn. We've made all our internal projects open source."
+      description: "Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects."
     },
   ]
 
--- libs/pages/elewa/about-us/src/lib/components/team-members-carousel/team-members-carousel.component.ts
+++ libs/pages/elewa/about-us/src/lib/components/team-members-carousel/team-members-carousel.component.ts
@@ -26,9 +26,9 @@ export class TeamMembersCarouselComponent implements OnInit {
         `I believe education can be meaningful and enjoyable. Coming from a formal education background in Belgium, 
         I started Elewa with a mission to strengthen formal and traditional education systems. 
         I realized that education, work, and life are indistinguishably linked.
-        <br>
-        Having diversified to many different sectors and activities since, we are still an education company to date. 
-        For we have only one measurement of success; The continuous growth of others and self.`,
+        <br/><br/>
+        Having diversified to many different sectors and activities since, we are still an education company at core. 
+        For we have only one measurement of success i.e. the continuous growth of others and self.`,
     },
     {
       name: 'Kennedy Adhola',
@@ -49,13 +49,15 @@ export class TeamMembersCarouselComponent implements OnInit {
         'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
     },
     {
-      name: 'Zippie Ntabo',
+      name: 'Noella Mwanzia',
       management: false,
-      role: 'Office Administrator & Finance',
-      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656822/elewa-group-website/elewa-team-members/zip-elewa_jksbfg.jpg',
-      LinkedInProfileLink: 'https://www.linkedin.com/in/zippie-ntabo-310693134/',
+      role: 'Software Engineer',
+      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679660055/elewa-group-website/elewa-team-members/noella-elewa_g6tquq.jpg',
+      LinkedInProfileLink: 'https://www.linkedin.com/in/noella-mwanzia-633803173/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `Elewa has helped me broaden my perspective which has been a game changer that enables me to work on 
+              meaningful and innovative solutions that impact society. Certainly gotten used to 
+              channeling my potential to solve everyday problems. Couldn't have it any other way!`   
     },
     {
       name: 'Ian Odhiambo',
@@ -64,16 +66,9 @@ export class TeamMembersCarouselComponent implements OnInit {
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/ian-elewa_bejey9.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/feed/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
-    },
-    {
-      name: 'Shawn Chiama',
-      management: false,
-      role: 'Software Engineer',
-      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679657743/elewa-group-website/elewa-team-members/shawn-elewa_tt3iit.jpg',
-      LinkedInProfileLink: 'https://www.linkedin.com/feed/',
-      summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `As pioneer of the Elewa's first Venture Lab, my ultimate goal is to leverage my expertise, acquired over the years, 
+         to create a successful venture that will revolutionise an industry and make a 
+         meaningful impact to the people around the world.`,
     },
     {
       name: 'Jane Njoroge',
@@ -82,25 +77,35 @@ export class TeamMembersCarouselComponent implements OnInit {
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/jane-elewa_yyybka.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/jane-w-njoroge/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `As a Software Engineer, benefiting from the apprenticeship training program at Elewa,
+           my passion for learning and collaboration is enabling me deliver high-quality software solutions 
+           to our clients while ensuring my continuous professional growth.`    
     },
     {
-      name: 'Reagan Cherana',
+      name: 'Shawn Chiama',
+      management: false,
+      role: 'Software Engineer',
+      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679657743/elewa-group-website/elewa-team-members/shawn-elewa_tt3iit.jpg',
+      LinkedInProfileLink: 'https://www.linkedin.com/feed/',
+      summary:
+        `As a Senior Software Engineer, I am committed to instilling a sense of pride and ownership in my mentees, 
+          while fostering a growth mindset and a dedication to continuous learning. 
+          By building a community of engineers who prioritize impact and quality, 
+          I believe we can shape the future of software engineering and create positive change with everything we create.`,
+    },
+    {
+      name: 'Reagan Charana',
       management: false,
       role: 'Software Engineer',
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/reagan-elewa_bn8xkl.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/reagan-charana/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
-    },
-    {
-      name: 'Noella Mwanzia',
-      management: false,
-      role: 'Software Engineer',
-      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679660055/elewa-group-website/elewa-team-members/noella-elewa_g6tquq.jpg',
-      LinkedInProfileLink: 'https://www.linkedin.com/in/noella-mwanzia-633803173/',
-      summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `I like to get technical 😎. From systems requiring nano-second accuracy on speed, 
+          to developing no-code chatbot editors for non-technical users. DevOps is my second name.
+          Currently leading the development of our Elewa Conversational Learning manager. 
+          <br/> <br/>
+          Outside of work, find me at the chess table, networking, or staying up-to-date 
+            with the latest astronomy discoveries.`
     },
     {
       name: 'Paul Chesa',
@@ -109,25 +114,39 @@ export class TeamMembersCarouselComponent implements OnInit {
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679666201/elewa-group-website/elewa-team-members/chesa-elewa_soeb8f.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/paulchesa/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `Passionate about how frameworks such as JavaScript, Angular, React, 
+          Google Cloud and Firebase may aid to drive innovation that will ensure sustainability of a 
+          digital world that harness human interaction.`   
     },
     {
-      name: 'Joy-Linda Wawira',
+      name: 'Joy-Lidah Wawira',
       management: false,
       role: 'Software Engineer',
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679984133/elewa-group-website/elewa-team-members/joy_gvbweu.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/joy-wawira/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `I am language agnostic. Developing innovative, robust and scalable software products with an aim to 
+            solve social and enterprise challenges. I believe in the power of sharing knowledge and ideas`        
     },
     {
-      name: 'Jeff Kingori',
+      name: `Jeff King'ori`,
       management: false,
       role: 'Software Engineer',
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679665800/elewa-group-website/elewa-team-members/jeff-elewa_hdupp7.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/jeffrey-king-ori-165333164/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `I am thrilled to be part of Elewa, a company that invests in talent development while making 
+            meaningful impact in the world.`        
+    },
+    {
+      name: 'Zippie Ntabo',
+      management: false,
+      role: 'Office Administrator & Finance',
+      imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656822/elewa-group-website/elewa-team-members/zip-elewa_jksbfg.jpg',
+      LinkedInProfileLink: 'https://www.linkedin.com/in/zippie-ntabo-310693134/',
+      summary:
+        `I run Elewa's administration, ultimately driven by the satisfaction of contributing to growth and success of Elewa in both the Finance and 
+        Admin Department.`    
     },
     // {
     //   name: 'Lynn Anastasia',
@@ -145,7 +164,9 @@ export class TeamMembersCarouselComponent implements OnInit {
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656820/elewa-group-website/elewa-team-members/frank-elewa_kihn9e.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/frankline-sable/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `I am passionate about developing app and web projects, gami(ng)fication projects as well as handling bits and pieces of computer hardware engineering. 
+          <br/><br/>  
+          I believe that continous learning and development helps any one to stay ahead of the curve.`        
     },
     {
       name: 'Stephen Mwaura',
@@ -154,7 +175,8 @@ export class TeamMembersCarouselComponent implements OnInit {
       imageLink: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679985792/elewa-group-website/elewa-team-members/lemmy_oosngt.jpg',
       LinkedInProfileLink: 'https://www.linkedin.com/in/stephenlemmymwaura/',
       summary:
-        'This free Paragraph Generator will generate complete paragraphs according to the instructions provided by you, so that you can quickly build unique content.',
+        `I have a strong desire to build software solutions that make a positive impact on people's lives 
+            taking into account user experience and the excellence desired.`        
     },
   ];
 
