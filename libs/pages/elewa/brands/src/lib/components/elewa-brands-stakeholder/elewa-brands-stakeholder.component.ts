23ac8b020a3fb631db15ae8e440f6095829c241f
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-04-12 09:21:48 +0300

    :art: UI animations updates for brands

--- libs/pages/elewa/brands/src/lib/components/elewa-brands-stakeholder/elewa-brands-stakeholder.component.ts
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-stakeholder/elewa-brands-stakeholder.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'elewa-group-elewa-brands-stakeholder',
   templateUrl: './elewa-brands-stakeholder.component.html',
   styleUrls: ['./elewa-brands-stakeholder.component.scss'],
 })
-export class ElewaBrandsStakeholderComponent {}
+export class ElewaBrandsStakeholderComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+}
