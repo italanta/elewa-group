03b093e8a24c98a3f3d209095102960b2d2e31dd
Jente Rosseel <rosseeljente@gmail.com>
2023-04-12 10:27:19 +0300

    :pencil2: fix typo on home page lander

--- libs/pages/elewa/home/src/lib/components/home-page-lander/home-page-lander.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-page-lander/home-page-lander.component.ts
@@ -9,5 +9,5 @@ export class HomePageLanderComponent {
   @Input() subtitle = 'THE WAY IN WHICH WE TRAIN IS OUTDATED';
   @Input() title = 'We are a group of companies which unlock true human potential.';
   @Input() backgroundImg = 'https://images.pexels.com/photos/13348192/pexels-photo-13348192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
-  @Input() description = 'Irrespective of origin,nationality, gender, and belief.';
+  @Input() description = 'Irrespective of origin, nationality, gender and belief.';
 }
