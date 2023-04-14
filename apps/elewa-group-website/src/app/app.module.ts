9929eedd0100db8f68ded275e724847b03fa628a
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-04-14 10:27:33 +0300

    :horse: init angularfire

--- apps/elewa-group-website/src/app/app.module.ts
+++ apps/elewa-group-website/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from '@iote/bricks-angular';
 
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app.routing';
 
@@ -17,10 +23,15 @@ import { AppRoutingModule } from './app.routing';
     BrowserModule,
     BrowserAnimationsModule,
 
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+
     MaterialDesignModule,
 
     RouterModule,
-    ScullyLibModule,
+    ScullyLibModule.forRoot({
+      alwaysMonitor: true,
+    }),
 
     AppRoutingModule
   ],
