7561b8298b8654200494281cf7c047e5a2c38a8b
Jente Rosseel <rosseeljente@gmail.com>
2023-04-15 11:09:57 +0300

    :wrench: Scale down too large images

--- libs/pages/elewa/venture-labs/src/lib/components/venture-labs-hero/venture-labs-hero.component.ts
+++ libs/pages/elewa/venture-labs/src/lib/components/venture-labs-hero/venture-labs-hero.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./venture-labs-hero.component.scss'],
 })
 export class VentureLabsHeroComponent {
-  backgroundImage="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690293/elewa-group-website/hero-Images/Venture_labs_y4tnjx.jpg"
+  backgroundImage="https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1500/v1675690293/elewa-group-website/hero-Images/Venture_labs_y4tnjx.jpg"
   subtitle= "Venture Labs"
   title="Elewa's very own moonshot factory"
   footer=""}
